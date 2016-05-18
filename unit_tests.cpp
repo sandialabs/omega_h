068fd283345d2ecf6972991ff1ae2f84d55d4634
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-05-18 17:00:59 -0400

    simple tests for reflect_down (2)

--- unit_tests.cpp
+++ unit_tests.cpp
@@ -397,6 +397,17 @@ static void test_reflect_down2() {
   a = reflect_down(LOs(),LOs(),3,2);
   CHECK(a.ab2b == LOs({}));
   CHECK(a.codes == Read<I8>({}));
+  a = reflect_down(LOs({0,1,2}),LOs({0,1,1,2,2,0}),2,1);
+  CHECK(a.ab2b == LOs({0,1,2}));
+  CHECK(a.codes == Read<I8>({0,0,0}));
+  a = reflect_down(LOs({0,1,2,3}),
+      LOs({0,1,1,2,2,0,0,3,1,3,2,3}),3,1);
+  CHECK(a.ab2b == LOs({0,1,2,3,4,5}));
+  CHECK(a.codes == Read<I8>({0,0,0,0,0,0}));
+  a = reflect_down(LOs({0,1,2,3}),
+      LOs({0,2,1,0,1,3,1,2,3,2,0,3}),3,2);
+  CHECK(a.ab2b == LOs({0,1,2,3}));
+  CHECK(a.codes == Read<I8>({0,0,0,0}));
 }
 
 int main(int argc, char** argv) {
