d293a537b4ceb47cf743ab02a2950775040070df
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-05-18 15:33:06 -0400

    comment

--- adjacency.hpp
+++ adjacency.hpp
@@ -13,14 +13,17 @@ Adj invert(Adj down, I8 nlows_per_high, LO nlows,
 
 /* given entity uses and unique entities,
    both defined by vertex lists, match
-   uses to unique entities including their
+   uses to unique entities and derive their
    respective alignment codes.
 
    even though this is a downward adjacency, we'll
    define the code as describing how to transform
    the boundary entity into the entity use,
    since typically data is being pulled into an element
-   from its boundary */
+   from its boundary
+
+   this function requires that all entities have
+   at least one corresponding use ! */
 template <Int deg>
 void reflect_down(LOs euv2v, LOs ev2v,
     LOs& eu2e, Read<I8>& eu2e_codes_);
