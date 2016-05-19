18fe91da8e9f0881d08829f253e2a7e8c8cac586
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-05-19 10:36:41 -0400

    drafting upward template tables

--- simplices.hpp
+++ simplices.hpp
@@ -32,7 +32,34 @@ CONSTANT static I8 const* const rfv_[] = {
   rfv0,rfv1,rfv2,rfv3};
 CONSTANT static I8 const* const* const r_v_[] = {
   0,rev_,rfv_};
-CONSTANT static I8 const* const* const* const simplices[] = {
+CONSTANT static I8 const* const* const* const down_templates[] = {
   0,0,f_v_,r_v_};
+
+struct UpTemplate { I8 up; I8 which_down; };
+CONSTANT static UpTemplate const fve0[] = {{0,0},{2,1}};
+CONSTANT static UpTemplate const fve1[] = {{1,0},{0,1}};
+CONSTANT static UpTemplate const fve2[] = {{2,0},{1,1}};
+CONSTANT static UpTemplate const* const fve_[] = {
+  fve0,fve1,fve2};
+CONSTANT static UpTemplate const* const* const f_u_[] = {
+  fve_};
+CONSTANT static UpTemplate const rve0[] = {{0,0},{2,1},{3,0}};
+CONSTANT static UpTemplate const rve1[] = {{1,0},{0,1},{4,0}};
+CONSTANT static UpTemplate const rve2[] = {{2,0},{1,1},{5,0}};
+CONSTANT static UpTemplate const rve3[] = {{3,1},{4,1},{5,1}};
+CONSTANT static UpTemplate const* const rve_[] = {
+  rve0,rve1,rve2,rve3};
+CONSTANT static UpTemplate const ref0[] = {{0,2},{1,0}};
+CONSTANT static UpTemplate const ref1[] = {{0,1},{2,0}};
+CONSTANT static UpTemplate const ref2[] = {{0,0},{3,0}};
+CONSTANT static UpTemplate const ref3[] = {{1,2},{3,1}};
+CONSTANT static UpTemplate const ref4[] = {{2,2},{1,1}};
+CONSTANT static UpTemplate const ref5[] = {{3,2},{2,1}};
+CONSTANT static UpTemplate const* const ref_[] = {
+  ref0,ref1,ref2,ref3,ref4,ref5};
+CONSTANT static UpTemplate const* const* const r_u_[] = {
+  rve_,ref_};
+CONSTANT static UpTemplate const* const* const* const UpTemplates[] = {
+  0,0,f_u_,r_u_};
 
 extern I8 const degrees[DIMS][DIMS];
