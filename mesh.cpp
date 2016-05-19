3cc6289d98b5969285bfcd5191f8f1337a9ba697
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-05-19 19:51:02 -0400

    another revision of Mesh

--- mesh.cpp
+++ mesh.cpp
@@ -4,13 +4,26 @@ Mesh::Mesh():
     nents_[i] = -1;
 }
 
-Mesh::Mesh(I8 dim, LO nverts):
-  dim_(dim) {
-  CHECK(dim > 0);
+void Mesh::set_dim(I8 dim) {
+  CHECK(dim_ == -1);
+  CHECK(dim >= 2);
   CHECK(dim <= 3);
+  dim_ = dim;
+}
+
+void Mesh::set_verts(LO nverts) {
   nents_[VERT] = nverts;
-  for (I8 i = 1; i <= dim; ++i)
-    nents_[i] = -1;
+}
+
+void Mesh::set_ents(I8 dim, Adj down) {
+  check_dim(dim);
+  CHECK(!has_ents(dim));
+  LOs hl2l = down.ab2b;
+  CHECK(hl2l.size() % degrees[dim][dim - 1] == 0);
+  nents_[dim] = hl2l.size() / degrees[dim][dim - 1];
+  add_adj(dim, dim - 1, down);
+  /* todo: replace with hilbert curve on set_coords() */
+  add_tag_priv<GO>(dim, "global", 1, Read<GO>(nents(dim), 0, 1));
 }
 
 I8 Mesh::dim() const {
@@ -24,16 +37,13 @@ LO Mesh::nents(I8 dim) const {
 
 template <typename T>
 void Mesh::add_tag(I8 dim, std::string const& name, I8 ncomps, Read<T> data) {
-  check_dim2(dim);
-  CHECK(!has_tag(dim, name));
-  CHECK(tags_[dim].size() < static_cast<std::size_t>(INT8_MAX));
-  tags_[dim].push_back(TagPtr(new Tag<T>(name, ncomps, data)));
+  CHECK(!(dim == 0 && name == "coordinates"));
+  add_tag_priv(dim, name, ncomps, data);
 }
 
 void Mesh::remove_tag(I8 dim, std::string const& name) {
-  check_dim2(dim);
-  CHECK(has_tag(dim, name));
-  tags_[dim].erase(tag_iter(dim, name));
+  CHECK(!(dim == 0 && name == "coordinates"));
+  remove_tag_priv(dim, name);
 }
 
 bool Mesh::has_tag(I8 dim, std::string const& name) const {
@@ -60,18 +70,9 @@ TagBase const* Mesh::get_tag(I8 dim, I8 i) const {
   return tags_[dim][static_cast<std::size_t>(i)].get();
 }
 
-bool Mesh::has_dim(I8 dim) const {
+bool Mesh::has_ents(I8 dim) const {
   check_dim(dim);
-  return nents_[dim] != -1;
-}
-
-void Mesh::add_adj(I8 from, I8 to, Adj adj) {
-  check_dim(from);
-  check_dim2(to);
-  CHECK(!has_dim(from));
-  CHECK(from > 0);
-  CHECK((to == VERT) || (to == (from - 1)));
-  adjs_[from][to] = AdjPtr(new Adj(adj));
+  return nents_[dim] >= 0;
 }
 
 bool Mesh::has_adj(I8 from, I8 to) const {
@@ -97,6 +98,15 @@ Adj Mesh::ask_adj(I8 from, I8 to) {
   return derived;
 }
 
+Read<GO> Mesh::ask_globals(I8 dim) {
+  check_dim2(dim);
+  if (!has_tag(dim, "global")) {
+    /* todo: replace with hilbert curve */
+    add_tag_priv<GO>(VERT, "global", 1, Read<GO>(nents(dim), 0, 1));
+  }
+  return get_tag<GO>(dim, "global").data();
+}
+
 struct HasName {
   std::string const& name_;
   HasName(std::string const& name):name_(name) {}
@@ -104,6 +114,22 @@ struct HasName {
     return a->name() == name_;
   }
 };
+
+template <typename T>
+void Mesh::add_tag_priv(I8 dim, std::string const& name, I8 ncomps,
+    Read<T> data) {
+  check_dim2(dim);
+  CHECK(data.size() == nents(dim) * ncomps);
+  CHECK(!has_tag(dim, name));
+  CHECK(tags_[dim].size() < static_cast<std::size_t>(INT8_MAX));
+  tags_[dim].push_back(TagPtr(new Tag<T>(name, ncomps, data)));
+}
+
+void Mesh::remove_tag_priv(I8 dim, std::string const& name) {
+  check_dim2(dim);
+  CHECK(has_tag(dim, name));
+  tags_[dim].erase(tag_iter(dim, name));
+}
 
 Mesh::TagIter Mesh::tag_iter(I8 dim, std::string const& name) {
   return std::find_if(begin(tags_[dim]), end(tags_[dim]), HasName(name));
@@ -120,14 +146,46 @@ void Mesh::check_dim(I8 dim) const {
 
 void Mesh::check_dim2(I8 dim) const {
   check_dim(dim);
-  CHECK(has_dim(dim));
+  CHECK(has_ents(dim));
+}
+
+void Mesh::add_adj(I8 from, I8 to, Adj adj) {
+  check_dim2(from);
+  check_dim2(to);
+  if (to < from) {
+    CHECK(adj.a2ab.size() == 0);
+    if (to == VERT)
+      CHECK(adj.codes.size() == 0);
+    CHECK(adj.ab2b.size() == nents(from) * degrees[from][to]);
+  } else {
+    if (from < to) {
+      CHECK(adj.ab2b.size() == nents(to) * degrees[to][from]);
+    }
+    CHECK(adj.a2ab.size() == nents(from) + 1);
+  }
+  adjs_[from][to] = AdjPtr(new Adj(adj));
 }
 
 Adj Mesh::derive_adj(I8 from, I8 to) {
   check_dim(from);
   check_dim2(to);
-  /* todo... */
-  return Adj();
+  if (from < to) {
+    Adj down = ask_adj(to, from);
+    I8 nlows_per_high = degrees[to][from];
+    LO nlows = nents(from);
+    Read<GO> high_globals = ask_globals(to);
+    Adj up = invert(down, nlows_per_high, nlows, high_globals);
+    return up;
+  } else if (to < from) {
+    CHECK(to + 1 < from);
+    Adj h2m = ask_adj(from, to + 1);
+    Adj m2l = ask_adj(to + 1, to);
+    Adj h2l = transit(h2m, m2l, from, to);
+    return h2l;
+  } else {
+    /* todo: star code */
+    NORETURN(Adj());
+  }
 }
 
 #define INST_T(T) \
--- mesh.hpp
+++ mesh.hpp
@@ -1,7 +1,8 @@
 class Mesh {
   public:
     Mesh();
-    Mesh(I8 dim, LO nverts);
+    void set_dim(I8 dim);
+    void set_verts(LO nverts);
     I8 dim() const;
     LO nents(I8 dim) const;
     template <typename T>
@@ -12,21 +13,26 @@ class Mesh {
     Tag<T> const& get_tag(I8 dim, std::string const& name) const;
     I8 count_tags(I8 dim) const;
     TagBase const* get_tag(I8 dim, I8 i) const;
-    bool has_dim(I8 dim) const;
-    void add_adj(I8 from, I8 to, Adj adj);
+    bool has_ents(I8 dim) const;
+    void set_ents(I8 dim, Adj down);
     bool has_adj(I8 from, I8 to) const;
     Adj get_adj(I8 from, I8 to) const;
     Adj ask_adj(I8 from, I8 to);
+    Read<GO> ask_globals(I8 dim);
   private:
     typedef std::shared_ptr<TagBase> TagPtr;
     typedef std::shared_ptr<Adj> AdjPtr;
     typedef std::vector<TagPtr> TagVector;
     typedef TagVector::iterator TagIter;
     typedef TagVector::const_iterator TagCIter;
+    template <typename T>
+    void add_tag_priv(I8 dim, std::string const& name, I8 ncomps, Read<T> data);
+    void remove_tag_priv(I8 dim, std::string const& name);
     TagIter tag_iter(I8 dim, std::string const& name);
     TagCIter tag_iter(I8 dim, std::string const& name) const;
     void check_dim(I8 dim) const;
     void check_dim2(I8 dim) const;
+    void add_adj(I8 from, I8 to, Adj adj);
     Adj derive_adj(I8 from, I8 to);
     I8 dim_;
     LO nents_[DIMS];
