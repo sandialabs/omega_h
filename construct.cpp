e2796116121b8ddf3cda5d9b8bae3f51d6ae1483
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-05-20 16:34:20 -0400

    draft box builder

--- construct.cpp
+++ construct.cpp
@@ -19,3 +19,32 @@ void build_from_elems2verts(Mesh& mesh, Int edim, LOs ev2v, LO nverts) {
   }
   add_ents2verts(mesh, edim, ev2v);
 }
+
+void build_box(Mesh& mesh, Int dim) {
+  CHECK(dim == 2 || dim == 3);
+  if (dim == 2) {
+    build_from_elems2verts(mesh, dim,
+        LOs({0,1,2,2,3,0}), 4);
+    mesh.add_tag<Real>(0, "coordinates", 2);
+    mesh.set_tag<Real>(0, "coordinates",
+        Reals({0,0,1,0,1,1,0,1}));
+  } else {
+    build_from_elems2verts(mesh, dim,
+        LOs({0, 1, 2, 6,
+             2, 3, 0, 6,
+             0, 3, 7, 6,
+             7, 4, 0, 6,
+             0, 4, 5, 6,
+             5, 1, 0, 6 }), 8);
+    mesh.add_tag<Real>(0, "coordinates", 3);
+    mesh.set_tag<Real>(0, "coordinates",
+        Reals({0, 0, 0,
+               1, 0, 0,
+               1, 1, 0,
+               0, 1, 0,
+               0, 0, 1,
+               1, 0, 1,
+               1, 1, 1,
+               0, 1, 1 }));
+  }
+}
--- mesh.hpp
+++ mesh.hpp
@@ -40,3 +40,4 @@ class Mesh {
 };
 
 void build_from_elems2verts(Mesh& mesh, Int edim, LOs ev2v, LO nverts);
+void build_box(Mesh& mesh, Int dim);
