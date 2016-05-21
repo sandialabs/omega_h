d64b9d49bb91178cdab0d27dfcb6ecbba71e25de
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-05-21 15:49:36 -0400

    was using the wrong permutation for numbering

--- manual_test.cpp
+++ manual_test.cpp
@@ -6,9 +6,10 @@ int main(int argc, char** argv) {
   init(argc, argv);
   {
   Mesh mesh;
-  build_box(mesh, 1, 1, 1, 1, 1, 0);
+  build_box(mesh, 3, 3, 0, 3, 3, 0);
   Reals coords = mesh.coords();
-  LOs nums = hilbert::sort_coords(coords, mesh.dim());
+  LOs sorted2input = hilbert::sort_coords(coords, mesh.dim());
+  LOs nums = invert_permutation(sorted2input);
   mesh.add_tag<LO>(VERT, "hilbert", 1);
   mesh.set_tag<LO>(VERT, "hilbert", nums);
   std::ofstream file("out.vtu");
