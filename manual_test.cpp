b568773be712c993d1b4dcfb2bf1312c560fc71e
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-05-21 14:41:44 -0400

    even with prime numbers

--- manual_test.cpp
+++ manual_test.cpp
@@ -6,7 +6,7 @@ int main(int argc, char** argv) {
   init(argc, argv);
   {
   Mesh mesh;
-  build_box(mesh, 1, 1, 1, 2, 2, 2);
+  build_box(mesh, 1, 1, 1, 3, 5, 13);
   std::ofstream file("out.vtu");
   vtk::write_vtu(file, mesh);
   }
