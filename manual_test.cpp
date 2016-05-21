3d1d4d6e7e9b2ffd3367a0488b7fadeb79171e72
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-05-21 14:31:48 -0400

    2D larger grid also okay

--- manual_test.cpp
+++ manual_test.cpp
@@ -6,7 +6,7 @@ int main(int argc, char** argv) {
   init(argc, argv);
   {
   Mesh mesh;
-  build_box(mesh, 1, 1, 0, 1, 1, 0);
+  build_box(mesh, 1, 1, 0, 5, 13, 0);
   std::ofstream file("out.vtu");
   vtk::write_vtu(file, mesh, 2);
   }
