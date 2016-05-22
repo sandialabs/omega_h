a0d07e0d97edbe36d2f45a3d94b17a7230954076
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-05-22 12:10:22 -0400

    3D classification by angles also looks decent

--- manual_test.cpp
+++ manual_test.cpp
@@ -6,9 +6,13 @@ int main(int argc, char** argv) {
   init(argc, argv);
   {
   Mesh mesh;
-  build_box(mesh, 1, 1, 1, 3, 3, 0);
+  build_box(mesh, 1, 1, 1, 2, 2, 2);
   Reals coords = mesh.coords();
   classify_by_angles(mesh, PI / 4);
+  {
+  std::ofstream file("tets.vtu");
+  vtk::write_vtu(file, mesh, 3);
+  }
   {
   std::ofstream file("tris.vtu");
   vtk::write_vtu(file, mesh, 2);
