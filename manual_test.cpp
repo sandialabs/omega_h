c9d069515562ee204087fe368e832c0b7cdf4931
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-05-22 11:58:05 -0400

    2D classification by angles looking okay

--- manual_test.cpp
+++ manual_test.cpp
@@ -6,7 +6,7 @@ int main(int argc, char** argv) {
   init(argc, argv);
   {
   Mesh mesh;
-  build_box(mesh, 1, 1, 1, 1, 1, 0);
+  build_box(mesh, 1, 1, 1, 3, 3, 0);
   Reals coords = mesh.coords();
   classify_by_angles(mesh, PI / 4);
   {
