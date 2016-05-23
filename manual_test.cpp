1a0284849ad8f467ad3c1cdb5b801e246c250a1b
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-05-23 08:55:44 -0400

    reordering by hilbert seems to work

--- manual_test.cpp
+++ manual_test.cpp
@@ -6,14 +6,16 @@ int main(int argc, char** argv) {
   init(argc, argv);
   {
   Mesh mesh;
-  build_box(mesh, 1, 1, 1, 1, 1, 1);
+  build_box(mesh, 1, 1, 1, 4, 4, 4);
   Reals coords = mesh.coords();
   LOs new_verts2old_verts = hilbert::sort_coords(coords, mesh.dim());
   reorder_mesh(mesh, new_verts2old_verts);
-//{
-//std::ofstream file("tets.vtu");
-//vtk::write_vtu(file, mesh, 3);
-//}
+  mesh.add_tag<GO>(VERT, "global", 1, Read<GO>(mesh.nverts(), 0, 1));
+  mesh.add_tag<GO>(mesh.dim(), "global", 1, Read<GO>(mesh.nelems(), 0, 1));
+  {
+  std::ofstream file("tets.vtu");
+  vtk::write_vtu(file, mesh, 3);
+  }
 //{
 //std::ofstream file("tris.vtu");
 //vtk::write_vtu(file, mesh, 2);
