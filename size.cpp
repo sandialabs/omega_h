7ee253d114571a8be20742dd5e711cf35c1af420
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-05-23 12:31:13 -0400

    forgot a file in e1facc6

---
+++ size.cpp
@@ -0,0 +1,36 @@
+template <typename EdgeLengths>
+Reals measure_edges_tmpl(Mesh& mesh, LOs ev2v) {
+  EdgeLengths measurer(mesh);
+  LO nedges = ev2v.size() / 2;
+  Write<Real> lengths(nedges);
+  auto f = LAMBDA(LO e) {
+    auto v = gather_verts<2>(ev2v, e);
+    lengths[e] = measurer.measure(v);
+  };
+  parallel_for(nedges, f);
+  return lengths;
+}
+
+Reals measure_edges(Mesh& mesh, LOs ev2v) {
+  if (mesh.dim() == 3) {
+    if (mesh.has_tag(VERT, "size")) {
+      return measure_edges_tmpl<IsoEdgeLengths<3>>(mesh, ev2v);
+    }
+    if (mesh.has_tag(VERT, "metric")) {
+      return measure_edges_tmpl<MetricEdgeLengths<3>>(mesh, ev2v);
+    }
+  } else {
+    CHECK(mesh.dim() == 2);
+    if (mesh.has_tag(VERT, "size")) {
+      return measure_edges_tmpl<IsoEdgeLengths<2>>(mesh, ev2v);
+    }
+    if (mesh.has_tag(VERT, "metric")) {
+      return measure_edges_tmpl<MetricEdgeLengths<2>>(mesh, ev2v);
+    }
+  }
+  fail("measure_edges(): no size field exists!\n");
+}
+
+Reals measure_edges(Mesh& mesh) {
+  return measure_edges(mesh, mesh.ask_down(EDGE,VERT).ab2b);
+}
