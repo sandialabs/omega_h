8081023278577cd325ece54112b66a6f71d8e29f
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-05-25 10:04:13 -0400

    linpart unit test from malla

--- unit_tests.cpp
+++ unit_tests.cpp
@@ -585,6 +585,17 @@ static void test_file() {
   CHECK(s == s2);
 }
 
+static void test_linpart() {
+  GO total = 7;
+  I32 comm_size = 2;
+  CHECK(linear_partition_size(total, comm_size, 0) == 4);
+  CHECK(linear_partition_size(total, comm_size, 1) == 3);
+  Read<GO> globals({6,5,4,3,2,1,0});
+  auto remotes = globals_to_linear_owners(globals, total, comm_size);
+  CHECK(remotes.ranks == Read<I32>({1,1,1,0,0,0,0}));
+  CHECK(remotes.idxs == Read<I32>({2,1,0,3,2,1,0}));
+}
+
 int main(int argc, char** argv) {
   init(argc, argv);
   test_cubic();
@@ -613,5 +624,6 @@ int main(int argc, char** argv) {
   test_dual();
   test_quality();
   test_file();
+  test_linpart();
   fini();
 }
