f56f33bfc3e7e712b8d2fafd48b67669af4dbe8a
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-05-25 09:05:17 -0400

    lots of 'auto' in transit code

--- transit.cpp
+++ transit.cpp
@@ -1,33 +1,33 @@
 Adj transit(Adj h2m, Adj m2l, Int high_dim, Int low_dim) {
   CHECK(3 >= high_dim);
-  Int mid_dim = low_dim + 1;
+  auto mid_dim = low_dim + 1;
   CHECK(high_dim > mid_dim);
   CHECK(low_dim == 1 || low_dim == 0);
-  LOs hm2m = h2m.ab2b;
-  Read<I8> m2hm_codes = h2m.codes;
-  LOs ml2l = m2l.ab2b;
-  Read<I8> ml_codes = m2l.codes;
-  LO nmids_per_high = simplex_degrees[high_dim][mid_dim];
-  LO nlows_per_mid = simplex_degrees[mid_dim][low_dim];
-  LO nlows_per_high = simplex_degrees[high_dim][low_dim];
-  LO nhighs = hm2m.size() / nmids_per_high;
+  auto hm2m = h2m.ab2b;
+  auto m2hm_codes = h2m.codes;
+  auto ml2l = m2l.ab2b;
+  auto ml_codes = m2l.codes;
+  auto nmids_per_high = simplex_degrees[high_dim][mid_dim];
+  auto nlows_per_mid = simplex_degrees[mid_dim][low_dim];
+  auto nlows_per_high = simplex_degrees[high_dim][low_dim];
+  auto nhighs = hm2m.size() / nmids_per_high;
   Write<LO> hl2l(nhighs * nlows_per_high);
   Write<I8> codes;
   if (low_dim == 1)
     codes = Write<I8>(hl2l.size());
   auto f = LAMBDA(LO h) {
-    LO hl_begin = h * nlows_per_high;
-    LO hm_begin = h * nmids_per_high;
+    auto hl_begin = h * nlows_per_high;
+    auto hm_begin = h * nmids_per_high;
     for (Int hl = 0; hl < nlows_per_high; ++hl) {
-      UpTemplate ut = up_templates[high_dim][low_dim][hl][0];
-      Int hm = ut.up;
-      Int hml = ut.which_down;
-      LO m = hm2m[hm_begin + hm];
+      auto ut = up_templates[high_dim][low_dim][hl][0];
+      auto hm = ut.up;
+      auto hml = ut.which_down;
+      auto m = hm2m[hm_begin + hm];
       auto m2hm_code = m2hm_codes[hm_begin + hm];
       auto hm2m_code = invert_alignment(nlows_per_mid, m2hm_code);
-      Int ml = align_index(nlows_per_mid, low_dim, hml, hm2m_code);
-      LO ml_begin = m * nlows_per_mid;
-      LO l = ml2l[ml_begin + ml];
+      auto ml = align_index(nlows_per_mid, low_dim, hml, hm2m_code);
+      auto ml_begin = m * nlows_per_mid;
+      auto l = ml2l[ml_begin + ml];
       //safety check for duplicates.
       //remove after this code is heavily exercised (or don't)
       for (Int hhl2 = 0; hhl2 < hl; ++hhl2)
@@ -36,9 +36,9 @@ Adj transit(Adj h2m, Adj m2l, Int high_dim, Int low_dim) {
       if (low_dim == 1) {
         auto tet_tri_code = hm2m_code;
         auto tri_edge_code = ml_codes[ml_begin + ml];
-        bool tet_tri_flipped = code_is_flipped(tet_tri_code);
-        bool tri_edge_flipped = code_rotation(tri_edge_code) == 1;
-        bool canon_flipped = ut.is_flipped;
+        auto tet_tri_flipped = code_is_flipped(tet_tri_code);
+        auto tri_edge_flipped = bool(code_rotation(tri_edge_code) == 1);
+        auto canon_flipped = ut.is_flipped;
         bool tet_edge_flipped = tet_tri_flipped ^
                                 tri_edge_flipped ^
                                 canon_flipped;
