59c0531e9237b1b2242bf861a7e54ce7b47cad24
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-05-26 09:47:15 -0600

    use Kokkos::deep_copy in our deep_copy
    the dead-simple lambda from before was
    causing major CUDA+Kokkos template errors.

--- classify.cpp
+++ classify.cpp
@@ -91,8 +91,8 @@ void project_classification(Mesh& mesh) {
     auto lh2h = l2h.ab2b;
     auto high_class_dim = mesh.get_tag<I8>(d + 1, "class_dim").array();
     auto high_class_id = mesh.get_tag<LO>(d + 1, "class_id").array();
-    Write<I8> class_dim = deep_copy(mesh.get_tag<I8>(d, "class_dim").array());
-    Write<LO> class_id = deep_copy(mesh.get_tag<LO>(d, "class_id").array());
+    Write<I8> class_dim = deep_copy<I8>(mesh.get_tag<I8>(d, "class_dim").array());
+    Write<LO> class_id = deep_copy<LO>(mesh.get_tag<LO>(d, "class_id").array());
     auto f = LAMBDA(LO l) {
       if (class_dim[l] >= 0)
         return;
