0d0a1ea36c60d019bed339c0d79279290cdd14cb
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-05-27 09:52:21 -0400

    describe old set to bcast

--- bcast.cpp
+++ bcast.cpp
@@ -1,40 +1,40 @@
-void bcast_mesh(Mesh& mesh, CommPtr new_comm) {
+void bcast_mesh(Mesh& mesh, CommPtr new_comm, bool is_source) {
   CommPtr old_comm = mesh.comm();
-  bool is_root = ((old_comm) && (old_comm->rank() == 0));
-  if (is_root)
-    CHECK(new_comm->rank() == 0);
+  if (new_comm->rank() == 0) {
+    CHECK(is_source);
+  }
   I32 dim;
-  if (is_root)
+  if (is_source)
     dim = mesh.dim();
   new_comm->bcast(dim);
-  if (!is_root) {
+  if (!is_source) {
     mesh.set_dim(dim);
     mesh.set_verts(0);
   }
   for (Int d = 0; d <= dim; ++d) {
-    if (d > VERT && !is_root)
+    if (d > VERT && !is_source)
       mesh.set_ents(d, Adj(LOs({}), Read<I8>({})));
     I32 ntags;
-    if (is_root)
+    if (is_source)
       ntags = mesh.ntags(d);
     new_comm->bcast(ntags);
     for (Int i = 0; i < ntags; ++i) {
       TagBase const* tag = nullptr;
-      if (is_root)
+      if (is_source)
         tag = mesh.get_tag(d, i);
       std::string name;
-      if (is_root)
+      if (is_source)
         name = tag->name();
       new_comm->bcast_string(name);
       I32 ncomps;
-      if (is_root)
+      if (is_source)
         ncomps = tag->ncomps();
       new_comm->bcast(ncomps);
       I32 tag_type;
-      if (is_root)
+      if (is_source)
         tag_type = tag->type();
       new_comm->bcast(tag_type);
-      if (!is_root) {
+      if (!is_source) {
         switch (tag_type) {
           case OSH_I8: mesh.add_tag<I8>(d, name, ncomps); break;
           case OSH_I32: mesh.add_tag<I32>(d, name, ncomps); break;
@@ -43,7 +43,7 @@ void bcast_mesh(Mesh& mesh, CommPtr new_comm) {
         }
       }
     }
-    if (!is_root && mesh.has_tag(d, "owner"))
+    if (!is_source && mesh.has_tag(d, "owner"))
       mesh.set_own_idxs(d, LOs({}));
   }
 }
--- bcast.hpp
+++ bcast.hpp
@@ -1,1 +1,1 @@
-void bcast_mesh(Mesh& mesh, CommPtr new_comm);
+void bcast_mesh(Mesh& mesh, CommPtr new_comm, bool is_source);
