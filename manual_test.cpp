737968ef1fb85ed848575a6f3d1c7b98e06a55d5
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-05-29 16:30:31 -0400

    testing 3D ghosting

--- manual_test.cpp
+++ manual_test.cpp
@@ -4,7 +4,7 @@
 
 static void serial_test(Mesh& mesh) {
   static Int const nx = 1;
-  static Int const dim = 2;
+  static Int const dim = 3;
   build_box(mesh, 1, 1, 1, nx, nx, (dim == 3) ? nx : 0);
   classify_by_angles(mesh, PI / 4);
   reorder_by_hilbert(mesh);
@@ -33,11 +33,11 @@ int main(int argc, char** argv) {
   bcast_mesh(mesh, world, world->rank() == 0);
   mesh.set_comm(world);
   if (world->rank() == 0) {
-    migrate_mesh(mesh, Remotes(Read<I32>(1, 0), LOs(1, 0, 1)));
+    migrate_mesh(mesh, Remotes(Read<I32>(3, 0), LOs(3, 0, 1)));
   } else {
-    migrate_mesh(mesh, Remotes(Read<I32>(1, 0), LOs(1, 1, 1)));
+    migrate_mesh(mesh, Remotes(Read<I32>(3, 0), LOs(3, 3, 1)));
   }
-  mesh.ask_partition(VERTEX_BASED);
+  mesh.ask_partition(GHOSTED);
   if (mesh.dim() == 3) {
   vtk::write_parallel_vtk("tets", mesh, 3);
   }
