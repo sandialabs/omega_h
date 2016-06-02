ed8078f12c22958ba31d539e85cdccf82d01403c
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-06-02 17:24:50 -0400

    test early refinement code

--- ex2.cpp
+++ ex2.cpp
@@ -8,6 +8,10 @@ int main(int argc, char** argv) {
   static Int const dim = 2;
   build_box(mesh, 1, 1, 1, nx, nx, (dim == 3) ? nx : 0);
   classify_by_angles(mesh, PI / 4);
+  mesh.add_tag(EDGE, "candidate", 1, OSH_DONT_TRANSFER,
+      Read<I8>(mesh.nents(EDGE), 1));
+  bool did = refine(mesh, 0.0);
+  CHECK(did);
   if (mesh.dim() == 3) {
   vtk::write_parallel("tets", mesh, 3);
   }
