17a3cf86524f492086941aa097d701adc9523920
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-06-02 17:56:20 -0400

    actually do 777b563

--- omega_h_in.hpp
+++ omega_h_in.hpp
@@ -8,6 +8,36 @@
 #cmakedefine OSH_USE_ZLIB
 #cmakedefine OSH_CHECK_BOUNDS
 
+#ifdef OSH_USE_MPI
+
+/* on BlueGene/Q the default install
+ * defines MPICH2_CONST to empty if we
+ * don't define it first, causing tons
+ * of compile errors.
+ *
+ * in addition, the mpicxx.h header is full
+ * of "const MPICH2_CONST", probably as a workaround
+ * for the first mistake above.
+ * as a result, properly defining MPICH2_CONST to const
+ * also causes compile errors.
+ * luckily, we can avoid including mpicxx.h with
+ * MPICH_SKIP_MPICXX.
+ */
+#ifdef __bgq__
+#define MPICH2_CONST const
+#define MPICH_SKIP_MPICXX
+#endif
+
+/* have Clang diagnostics ignore everything
+ * inside mpi.h
+ */
+#ifdef __clang__
+#pragma clang system_header
+#endif
+#include <mpi.h>
+
+#endif //OSH_USE_MPI
+
 #ifdef OSH_USE_KOKKOS
 #ifdef __clang__
 #pragma clang system_header
@@ -20,7 +50,7 @@
 #ifdef __GNUC__
 #pragma GCC diagnostic pop
 #endif
-#endif
+#endif //OSH_USE_KOKKOS
 
 #ifdef OSH_USE_KOKKOS
 #define OSH_INLINE KOKKOS_INLINE_FUNCTION
@@ -28,12 +58,12 @@
 #else
 #define OSH_INLINE inline
 #define OSH_LAMBDA [=]
-#endif
+#endif //OSH_USE_KOKKOS
 
 #ifdef OSH_USE_CUDA
 #define OSH_DEVICE __device__ inline
 #else
 #define OSH_DEVICE inline
-#endif
+#endif //OSH_USE_CUDA
 
 #endif
