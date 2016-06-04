e6b882f5d51030903cf1e84a35a6f2af7f8132ee
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-06-04 14:33:56 -0400

    separate cuts and pairs generation

--- refine_topology.cpp
+++ refine_topology.cpp
@@ -44,13 +44,12 @@ void refine_domains_to_pairs(
     LOs keys2edges,
     LOs keys2midverts,
     LOs old_verts2new_verts,
-    LOs& keys2cuts,
     LOs& keys2pairs,
-    LOs& cut_verts2verts_r,
-    LOs& pair_verts2verts_r) {
-  if (dim == 1) {
-    refine_edge_interiors(mesh, keys2edges, keys2midverts, old_verts2new_verts,
-        keys2cuts, keys2pairs, cut_verts2verts_r, pair_verts2verts_r);
+    LOs& pair_verts2verts) {
+  CHECK(dim > VERT);
+  if (dim == EDGE) {
+    refine_edges_to_pairs(mesh, keys2edges, keys2midverts, old_verts2new_verts,
+        keys2pairs, pair_verts2verts);
     return;
   }
   auto nkeys = keys2edges.size();
@@ -64,17 +63,13 @@ void refine_domains_to_pairs(
   auto key_dom_degrees = unmap(keys2edges, edge_dom_degrees, 1);
   auto keys2key_doms = offset_scan(key_dom_degrees);
   auto ndoms = keys2key_doms.last();
-  auto ncuts = ndoms;
   auto npairs = ndoms * 2;
-  keys2cuts = keys2key_doms;
   keys2pairs = multiply_each_by(2, keys2key_doms);
-  Write<LO> cut_verts2verts(ncuts * (dim));
-  Write<LO> pair_verts2verts(npairs * (dim + 1));
+  Write<LO> pair_verts2verts_w(npairs * (dim + 1));
   auto f = OSH_LAMBDA(LO key) {
     auto edge = keys2edges[key];
     auto midvert = keys2midverts[key];
     auto pair = keys2pairs[key];
-    auto cut = keys2cuts[key];
     for (auto edge_dom = edges2edge_doms[edge];
          edge_dom < edges2edge_doms[edge + 1];
          ++edge_dom) {
@@ -90,7 +85,7 @@ void refine_domains_to_pairs(
         auto dev = eev ^ rot;
         auto ddv = down_templates[dim][EDGE][dde][dev];
         auto dds = opposite_templates[dim][VERT][ddv];
-        auto ppv2v = &pair_verts2verts[pair * (dim + 1)];
+        auto ppv2v = &pair_verts2verts_w[pair * (dim + 1)];
         for (Int dsv = 0; dsv < dim; ++dsv) {
           auto ddv2 = down_templates[dim][dim - 1][dds][dsv];
           auto ov = ddv2v[ddv2];
@@ -100,43 +95,21 @@ void refine_domains_to_pairs(
         ppv2v[dim] = midvert;
         ++pair;
       }
-      /* a "cut" is formed by connecting its opposite
-         "tip" (running out of words) to the new midpoint
-         vertex. for triangle domains, the tip is the vertex
-         not adjacent to the key edge. for tet domains, the tip
-         is the edge not adjacent to the key edge. */
-      auto ccv2v = &cut_verts2verts[cut * dim];
-      auto ddt = opposite_templates[dim][EDGE][dde];
-      for (Int dtv = 0; dtv < dim - 1; ++dtv) {
-        auto ddv2 = down_templates[dim][dim - 2][ddt][dtv];
-        auto ov = ddv2v[ddv2];
-        auto nv = old_verts2new_verts[ov];
-        ccv2v[dtv] = nv;
-      }
-      ccv2v[dim - 1] = midvert;
-      ++cut;
     }
   };
   parallel_for(nkeys, f);
-  cut_verts2verts_r = cut_verts2verts;
-  pair_verts2verts_r = pair_verts2verts;
+  pair_verts2verts = pair_verts2verts_w;
 }
 
-void refine_domain_interiors(
+void refine_domains_to_cuts(
     Mesh& mesh,
     Int dim,
     LOs keys2edges,
     LOs keys2midverts,
     LOs old_verts2new_verts,
     LOs& keys2cuts,
-    LOs& keys2pairs,
-    LOs& cut_verts2verts_r,
-    LOs& pair_verts2verts_r) {
-  if (dim == 1) {
-    refine_edge_interiors(mesh, keys2edges, keys2midverts, old_verts2new_verts,
-        keys2cuts, keys2pairs, cut_verts2verts_r, pair_verts2verts_r);
-    return;
-  }
+    LOs& cut_verts2verts) {
+  CHECK(dim > EDGE);
   auto nkeys = keys2edges.size();
   auto edge_verts2verts = mesh.ask_verts_of(EDGE);
   auto dom_verts2verts = mesh.ask_verts_of(dim);
@@ -149,15 +122,11 @@ void refine_domain_interiors(
   auto keys2key_doms = offset_scan(key_dom_degrees);
   auto ndoms = keys2key_doms.last();
   auto ncuts = ndoms;
-  auto npairs = ndoms * 2;
   keys2cuts = keys2key_doms;
-  keys2pairs = multiply_each_by(2, keys2key_doms);
-  Write<LO> cut_verts2verts(ncuts * (dim));
-  Write<LO> pair_verts2verts(npairs * (dim + 1));
+  Write<LO> cut_verts2verts_w(ncuts * (dim));
   auto f = OSH_LAMBDA(LO key) {
     auto edge = keys2edges[key];
     auto midvert = keys2midverts[key];
-    auto pair = keys2pairs[key];
     auto cut = keys2cuts[key];
     for (auto edge_dom = edges2edge_doms[edge];
          edge_dom < edges2edge_doms[edge + 1];
@@ -165,31 +134,13 @@ void refine_domain_interiors(
       auto dom = edge_doms2doms[edge_dom];
       auto code = edge_dom_codes[edge_dom];
       auto dde = code_which_down(code);
-      auto rot = code_rotation(code);
       auto ddv2v = &dom_verts2verts[dom * (dim + 1)];
-      for (Int eev = 0; eev < 2; ++eev) {
-        /* a new cell is formed from an old cell by finding
-           its side that is opposite to one of the edge endpoints
-           and connecting it to the midpoint to form the new cell */
-        auto dev = eev ^ rot;
-        auto ddv = down_templates[dim][EDGE][dde][dev];
-        auto dds = opposite_templates[dim][VERT][ddv];
-        auto ppv2v = &pair_verts2verts[pair * (dim + 1)];
-        for (Int dsv = 0; dsv < dim; ++dsv) {
-          auto ddv2 = down_templates[dim][dim - 1][dds][dsv];
-          auto ov = ddv2v[ddv2];
-          auto nv = old_verts2new_verts[ov];
-          ppv2v[dsv] = nv;
-        }
-        ppv2v[dim] = midvert;
-        ++pair;
-      }
       /* a "cut" is formed by connecting its opposite
          "tip" (running out of words) to the new midpoint
          vertex. for triangle domains, the tip is the vertex
          not adjacent to the key edge. for tet domains, the tip
          is the edge not adjacent to the key edge. */
-      auto ccv2v = &cut_verts2verts[cut * dim];
+      auto ccv2v = &cut_verts2verts_w[cut * dim];
       auto ddt = opposite_templates[dim][EDGE][dde];
       for (Int dtv = 0; dtv < dim - 1; ++dtv) {
         auto ddv2 = down_templates[dim][dim - 2][ddt][dtv];
@@ -202,33 +153,32 @@ void refine_domain_interiors(
     }
   };
   parallel_for(nkeys, f);
-  cut_verts2verts_r = cut_verts2verts;
-  pair_verts2verts_r = pair_verts2verts;
+  cut_verts2verts = cut_verts2verts_w;
 }
 
 void combine_pairs_and_cuts(
     Int ent_dim,
     LOs keys2cuts,
-    LOs keys2pairs_last,
+    LOs keys2pairs,
     LOs cut_verts2verts,
-    LOs pair_verts2verts_last,
+    LOs pair_verts2verts,
     LOs& keys2prods,
     LOs& prod_verts2verts) {
   auto nkeys = keys2cuts.size() - 1;
-  CHECK(nkeys == keys2pairs_last.size() - 1);
+  CHECK(nkeys == keys2pairs.size() - 1);
   auto keys2ncuts = get_degrees(keys2cuts);
-  auto keys2npairs_last = get_degrees(keys2pairs_last);
-  auto keys2nprods = add_each(keys2ncuts, keys2npairs_last);
+  auto keys2npairs = get_degrees(keys2pairs);
+  auto keys2nprods = add_each(keys2ncuts, keys2npairs);
   keys2prods = offset_scan(keys2nprods);
   auto nprods = keys2prods.last();
   auto nppv = simplex_degrees[ent_dim][VERT];
-  auto prod_verts2verts_w(nprods * nppv);
-  auto f = LAMBDA(LO key) {
+  auto prod_verts2verts_w = Write<LO>(nprods * nppv);
+  auto f = OSH_LAMBDA(LO key) {
     auto prod = keys2prods[key];
     for (auto pair = keys2pairs[key]; pair < keys2pairs[key]; ++pair) {
       for (Int ppv = 0; ppv < nppv; ++ppv) {
         prod_verts2verts_w[prod * nppv + ppv] =
-          pair_verts2verts_last[pair * nppv + ppv];
+          pair_verts2verts[pair * nppv + ppv];
       }
       ++prod;
     }
