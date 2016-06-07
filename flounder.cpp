f680b24bd4ef597548cccf954da0087c15ed9708
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-06-07 11:35:47 -0400

    flounder test in 1M final mode
    same result as old omega_h,
    but twice as slow...

--- flounder.cpp
+++ flounder.cpp
@@ -8,6 +8,7 @@ int main(int argc, char** argv) {
   Mesh mesh;
   build_box(mesh, 1, 1, 0, 1, 1, 0);
   classify_by_angles(mesh, PI / 4);
+  Now t0 = now();
   do {
     Write<Real> size(mesh.nverts());
     auto coords = mesh.coords();
@@ -15,16 +16,16 @@ int main(int argc, char** argv) {
       auto x = get_vec<2>(coords, v);
       auto s = cos(x[0] * 8.0 * M_PI) / 4.0 + 1.0 / 2.0;
       auto d = fabs(x[1] - s);
-      auto area = 1e-6 + d * 1e-4;
-      auto length = sqrt(area);
-      auto fudge = 1.4;
-      size[v] = length * fudge;
+      auto area = 1e-7 + d * 1e-5;
+      auto length = sqrt(2 * area);
+      size[v] = length;
     };
     parallel_for(mesh.nverts(), f);
     mesh.add_tag(VERT, "size", 1, OSH_DONT_TRANSFER, Reals(size));
-    std::cerr << mesh.nelems() << '\n';
   } while(refine_by_size(mesh, 0.3));
-  vtk::write_vtu("out.vtu", mesh, mesh.dim());
+  Now t1 = now();
+  std::cout << "refinement time " << (t1-t0) << " seconds ";
+  std::cout << mesh.nelems() << " final triangles\n";
   }
   fini();
 }
