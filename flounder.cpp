e7ec843f615862180b467a90e2171d301a293bbf
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-06-07 10:03:27 -0400

    flounder test with linear size field

--- flounder.cpp
+++ flounder.cpp
@@ -11,17 +11,16 @@ int main(int argc, char** argv) {
   vtk::FullWriter writer(mesh, "out");
   do {
     Write<Real> size(mesh.nverts());
-  //auto coords = mesh.coords();
+    auto coords = mesh.coords();
     auto f = LAMBDA(LO v) {
-    //auto x = get_vec<2>(coords, v);
-    //double s = cos(x[0] * 8.0 * PI) / 4.0 + 1.0 / 2.0;
-    //double d = fabs(x[1] - s);
-    //double fudge = 10;
-    //size[v] = sqrt(2 * (1e-7 + d * 1e-5)) * fudge;
-      size[v] = 0.3;
+      auto x = get_vec<2>(coords, v);
+      auto area = 1e-5 + (1e-3) * x[0];
+      auto length = sqrt(area);
+      size[v] = length;
     };
     parallel_for(mesh.nverts(), f);
     mesh.add_tag(VERT, "size", 1, OSH_DONT_TRANSFER, Reals(size));
+    std::cerr << mesh.nelems() << '\n';
     writer.write();
   } while(refine_by_size(mesh, 0.3));
   }
