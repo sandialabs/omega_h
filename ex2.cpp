da3e23944d03f48724033c1a64981108e28b344b
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-06-07 14:24:31 -0400

    3D parallel refine looks okay

--- ex2.cpp
+++ ex2.cpp
@@ -7,16 +7,16 @@ int main(int argc, char** argv) {
   {
   auto world = Comm::world();
   Mesh mesh;
-  static Int const dim = 2;
+  static Int const dim = 3;
   if (world->rank() == 0) {
-    build_box(mesh, 3, 1, 1, 3, 1, (dim == 3) ? 1 : 0);
+    build_box(mesh, 1, 1, 1, 4, 4, (dim == 3) ? 4 : 0);
     classify_by_angles(mesh, PI / 4);
   }
   mesh.set_comm(world);
   mesh.balance();
   vtk::FullWriter writer(mesh, "out");
   writer.write();
-  for (Int i = 0; i < 1; ++i) {
+  for (Int i = 0; i < 2; ++i) {
     mesh.add_tag(EDGE, "candidate", 1, OSH_DONT_TRANSFER,
         Read<I8>(mesh.nents(EDGE), 1));
     bool did = refine(mesh, 0.0);
