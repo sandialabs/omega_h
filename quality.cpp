d50e017e793ee6986540d20bb18ba25099c41659
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-06-07 19:19:22 -0400

    change measure_qualities to take subset

--- quality.cpp
+++ quality.cpp
@@ -1,34 +1,35 @@
 template <typename ElementQualities, Int dim>
-Reals measure_qualities_tmpl(Mesh const& mesh, LOs ev2v) {
+Reals measure_qualities_tmpl(Mesh& mesh, LOs a2e) {
   ElementQualities measurer(mesh);
-  auto neev = dim + 1;
-  auto nelems = ev2v.size() / neev;
-  Write<Real> qualities(nelems);
-  auto f = LAMBDA(LO e) {
+  auto ev2v = mesh.ask_verts_of(mesh.dim());
+  auto na = a2e.size();
+  Write<Real> qualities(na);
+  auto f = LAMBDA(LO a) {
+    auto e = a2e[a];
     auto v = gather_verts<dim + 1>(ev2v, e);
-    qualities[e] = measurer.measure(v);
+    qualities[a] = measurer.measure(v);
   };
-  parallel_for(nelems, f);
+  parallel_for(na, f);
   return qualities;
 }
 
-Reals measure_qualities(Mesh const& mesh, LOs ev2v) {
+Reals measure_qualities(Mesh& mesh, LOs a2e) {
   if (mesh.dim() == 3) {
     if (mesh.has_tag(VERT, "metric")) {
-      return measure_qualities_tmpl<MetricElementQualities,3>(mesh, ev2v);
+      return measure_qualities_tmpl<MetricElementQualities,3>(mesh, a2e);
     } else {
-      return measure_qualities_tmpl<RealElementQualities,3>(mesh, ev2v);
+      return measure_qualities_tmpl<RealElementQualities,3>(mesh, a2e);
     }
   } else {
     CHECK(mesh.dim() == 2);
     if (mesh.has_tag(VERT, "metric")) {
-      return measure_qualities_tmpl<MetricElementQualities,2>(mesh, ev2v);
+      return measure_qualities_tmpl<MetricElementQualities,2>(mesh, a2e);
     } else {
-      return measure_qualities_tmpl<RealElementQualities,2>(mesh, ev2v);
+      return measure_qualities_tmpl<RealElementQualities,2>(mesh, a2e);
     }
   }
 }
 
 Reals measure_qualities(Mesh& mesh) {
-  return measure_qualities(mesh, mesh.ask_verts_of(mesh.dim()));
+  return measure_qualities(mesh, LOs(mesh.nelems(), 0, 1));
 }
