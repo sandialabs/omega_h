7b842e6934512226fcc9aad4c3a29fd0d381cb24
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-06-07 19:05:22 -0400

    use ex2 to test length and size transfer

--- ex2.cpp
+++ ex2.cpp
@@ -7,22 +7,27 @@ int main(int argc, char** argv) {
   {
   auto world = Comm::world();
   Mesh mesh;
-  static Int const dim = 3;
+  static Int const dim = 2;
   if (world->rank() == 0) {
-    build_box(mesh, 1, 1, 1, 4, 4, (dim == 3) ? 4 : 0);
+    build_box(mesh, 1, 1, 1, 1, 1, (dim == 3) ? 1 : 0);
     classify_by_angles(mesh, PI / 4);
   }
   mesh.set_comm(world);
   mesh.balance();
   vtk::FullWriter writer(mesh, "out");
-  writer.write();
-  for (Int i = 0; i < 2; ++i) {
-    mesh.add_tag(EDGE, "candidate", 1, OSH_DONT_TRANSFER,
-        Read<I8>(mesh.nents(EDGE), 1));
-    bool did = refine(mesh, 0.0);
-    CHECK(did);
+  mesh.add_tag<Real>(VERT, "size", 1, OSH_LINEAR_INTERP);
+  Write<Real> size(mesh.nverts());
+  auto f = LAMBDA(LO v) {
+    size[v] = 0.2;
+  };
+  parallel_for(mesh.nverts(), f);
+  mesh.set_tag(VERT, "size", Reals(size));
+  mesh.ask_edge_lengths();
+  do {
+    std::cerr << mesh.nglobal_ents(mesh.dim()) << " elements\n";
+    std::cerr << mesh.nglobal_ents(EDGE) << " edges\n";
     writer.write();
-  }
+  } while(refine_by_size(mesh, 0.3));
   }
   fini();
 }
