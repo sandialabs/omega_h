ff8c5787b2554e89db203133fafa129966b25491
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-06-09 12:23:56 -0400

    draft code to select collapse per vertex

--- coarsen.hpp
+++ coarsen.hpp
@@ -1,13 +1,23 @@
 Few<Read<I8>, 4> mark_dead_ents(Mesh& mesh,
     LOs rails2edges,
     Read<I8> rail_col_verts);
+
 Adj find_coarsen_domains(Mesh& mesh,
     LOs keys2verts,
     Int ent_dim,
     Read<I8> ents_are_dead);
+
 Reals coarsen_qualities(Mesh& mesh,
     LOs cands2edges,
     Read<I8> cand_codes);
+
+void choose_vertex_collapses(Mesh& mesh,
+    LOs cands2edges,
+    Read<I8> cand_edge_codes,
+    Reals cand_edge_quals,
+    Read<I8>& verts_are_cands,
+    Reals& vert_quals);
+
 LOs coarsen_topology(Mesh& mesh,
     LOs keys2verts_onto,
     Int dom_dim,
--- coarsen_qualities.cpp
+++ coarsen_qualities.cpp
@@ -48,22 +48,68 @@ Reals coarsen_qualities(Mesh& mesh,
     LOs cands2edges,
     Read<I8> cand_codes) {
   CHECK(mesh.partition() == GHOSTED);
+  auto cand_quals = Reals();
   if (mesh.dim() == 3) {
     if (mesh.has_tag(VERT, "metric")) {
-      return coarsen_qualities_tmpl<MetricElementQualities,3>(
+      cand_quals = coarsen_qualities_tmpl<MetricElementQualities,3>(
           mesh, cands2edges, cand_codes);
     } else {
-      return coarsen_qualities_tmpl<RealElementQualities,3>(
+      cand_quals = coarsen_qualities_tmpl<RealElementQualities,3>(
           mesh, cands2edges, cand_codes);
     }
   } else {
     CHECK(mesh.dim() == 2);
     if (mesh.has_tag(VERT, "metric")) {
-      return coarsen_qualities_tmpl<MetricElementQualities,2>(
+      cand_quals = coarsen_qualities_tmpl<MetricElementQualities,2>(
           mesh, cands2edges, cand_codes);
     } else {
-      return coarsen_qualities_tmpl<RealElementQualities,2>(
+      cand_quals = coarsen_qualities_tmpl<RealElementQualities,2>(
           mesh, cands2edges, cand_codes);
     }
   }
+  auto edge_quals_w = Write<Real>(mesh.nedges() * 2, -1);
+  map_into(cand_quals, cands2edges, edge_quals_w, 2);
+  auto edge_quals = Reals(edge_quals_w);
+  edge_quals = mesh.sync_array(EDGE, edge_quals, 2);
+  return unmap(cands2edges, edge_quals, 2);
+}
+
+void choose_vertex_collapses(Mesh& mesh,
+    LOs cands2edges,
+    Read<I8> cand_edge_codes,
+    Reals cand_edge_quals,
+    Read<I8>& verts_are_cands,
+    Reals& vert_quals) {
+  CHECK(mesh.partition() == GHOSTED);
+  auto edges2cands = invert_injective_map(cands2edges, mesh.nedges());
+  auto v2e = mesh.ask_up(VERT, EDGE);
+  auto v2ve = v2e.a2ab;
+  auto ve2e = v2e.ab2b;
+  auto ve_codes = v2e.codes;
+  auto ev2v = mesh.ask_verts_of(EDGE);
+  auto verts_are_cands_w = Write<I8>(mesh.nverts());
+  auto vert_quals_w = Write<Real>(mesh.nverts());
+  auto f = LAMBDA(LO v) {
+    bool vert_is_cand = false;
+    Real best_qual = -1.0;
+    for (auto ve = v2ve[v]; ve < v2ve[v + 1]; ++ve) {
+      auto e = ve2e[ve];
+      auto cand = edges2cands[e];
+      if (cand == -1) continue;
+      auto ve_code = ve_codes[ve];
+      auto eev = code_which_down(ve_code);
+      auto cand_code = cand_edge_codes[cand];
+      if (!collapses(cand_code, eev)) continue;
+      auto qual = cand_edge_quals[cand * 2 + eev];
+      if (qual > best_qual) {
+        vert_is_cand = true;
+        best_qual = qual;
+      }
+    }
+    verts_are_cands_w[v] = vert_is_cand;
+    vert_quals_w[v] = best_qual;
+  };
+  parallel_for(mesh.nverts(), f);
+  verts_are_cands = verts_are_cands_w;
+  vert_quals = vert_quals_w;
 }
