21f5224a715c4aecd085fdc5983fed0ef6437523
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-06-10 10:17:54 -0400

    coarsening one 3D edge works

--- ex3.cpp
+++ ex3.cpp
@@ -7,20 +7,20 @@ int main(int argc, char** argv) {
   {
   auto world = Comm::world();
   Mesh mesh;
-  static Int const dim = 2;
+  static Int const dim = 3;
   if (world->rank() == 0) {
     build_box(mesh, 1, 1, 1, 1, 1, (dim == 3) ? 1 : 0);
     classify_by_angles(mesh, PI / 4);
   }
   vtk::FullWriter writer(mesh, "out");
   writer.write();
-  for (Int i = 0; i < 3; ++i) {
+  for (Int i = 0; i < 1; ++i) {
     mesh.add_tag(EDGE, "candidate", 1, OSH_DONT_TRANSFER,
         Read<I8>(mesh.nedges(), 1));
     refine(mesh, 0.3);
     writer.write();
   }
-  for (Int i = 0; i < 2; ++i) {
+  for (Int i = 0; i < 1; ++i) {
     coarsen_verts(mesh, Read<I8>(mesh.nverts(), 1), 0.3, false);
     writer.write();
   }
