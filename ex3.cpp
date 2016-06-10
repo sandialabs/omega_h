0bebb3b9e769fce3fb693ffce41f225b584a238f
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-06-10 10:03:17 -0400

    simplest coarsen test looks good

--- ex3.cpp
+++ ex3.cpp
@@ -13,13 +13,17 @@ int main(int argc, char** argv) {
     classify_by_angles(mesh, PI / 4);
   }
   vtk::FullWriter writer(mesh, "out");
-  mesh.add_tag(EDGE, "candidate", 1, OSH_DONT_TRANSFER,
-      Read<I8>(mesh.nedges(), 1));
   writer.write();
-  refine(mesh, 0.3);
-  writer.write();
-  coarsen_verts(mesh, Read<I8>(mesh.nverts(), 1), 0.3, false);
-  writer.write();
+  for (Int i = 0; i < 3; ++i) {
+    mesh.add_tag(EDGE, "candidate", 1, OSH_DONT_TRANSFER,
+        Read<I8>(mesh.nedges(), 1));
+    refine(mesh, 0.3);
+    writer.write();
+  }
+  for (Int i = 0; i < 2; ++i) {
+    coarsen_verts(mesh, Read<I8>(mesh.nverts(), 1), 0.3, false);
+    writer.write();
+  }
   }
   fini();
 }
