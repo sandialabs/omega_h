6e4c488765bc1b593d4c81e05c277eafd7eefec1
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-06-13 17:59:19 -0400

    can coarsen all the way out of a 4x4x4

--- corner.cpp
+++ corner.cpp
@@ -14,21 +14,25 @@ int main(int argc, char** argv) {
   mesh.balance();
   mesh.add_tag<Real>(VERT, "size", 1, OSH_LINEAR_INTERP);
   vtk::FullWriter writer(&mesh, "out");
-  do {
-    Write<Real> size(mesh.nverts());
-    auto coords = mesh.coords();
-    auto f = LAMBDA(LO v) {
-      auto x = get_vec<3>(coords, v);
-      auto coarse = 0.3;
-      auto fine = 0.05;
-      auto radius = norm(x);
-      auto diagonal = sqrt(3) - 0.5;
-      auto distance = fabs(radius - 0.5) / diagonal;
-      size[v] = coarse * distance + fine * (1.0 - distance);
-    };
-    parallel_for(mesh.nverts(), f);
-    mesh.set_tag(VERT, "size", Reals(size));
-    mesh.ask_edge_lengths();
+//do {
+//  Write<Real> size(mesh.nverts());
+//  auto coords = mesh.coords();
+//  auto f = LAMBDA(LO v) {
+//    auto x = get_vec<3>(coords, v);
+//    auto coarse = 0.4;
+//    auto fine = 0.1;
+//    auto radius = norm(x);
+//    auto diagonal = sqrt(3) - 0.5;
+//    auto distance = fabs(radius - 0.5) / diagonal;
+//    size[v] = coarse * distance + fine * (1.0 - distance);
+//  };
+//  parallel_for(mesh.nverts(), f);
+//  mesh.set_tag(VERT, "size", Reals(size));
+//  mesh.ask_edge_lengths();
+//  writer.write();
+//} while(refine_by_size(&mesh, 4.0 / 3.0, 0.47));
+  mesh.set_tag(VERT, "size", Reals(mesh.nverts(), 1.0));
+  while (coarsen_by_size(&mesh, 2.0 / 3.0, 0.47, false)) {
     writer.write();
-  } while(refine_by_size(&mesh, 4.0 / 3.0, 0.47));
+  }
 }
