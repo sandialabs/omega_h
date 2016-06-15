a4ab022989bc1c8fc21ef019feed327cb76e0f28
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-06-15 12:58:18 -0400

    swap2d_topology returns edges as well

--- swap2d.hpp
+++ swap2d.hpp
@@ -1,4 +1,5 @@
 Reals swap2d_qualities(Mesh* mesh, LOs cands2edges);
 void filter_swap2d_improve(Mesh* mesh,
     LOs* cands2edges, Reals* cand_quals);
-LOs swap2d_topology(Mesh* mesh, LOs keys2edges);
+void swap2d_topology(Mesh* mesh, LOs keys2edges,
+    LOs* prod_tv2v, LOs* prod_ev2v);
--- swap2d_topology.cpp
+++ swap2d_topology.cpp
@@ -1,4 +1,5 @@
-LOs swap2d_topology(Mesh* mesh, LOs keys2edges) {
+void swap2d_topology(Mesh* mesh, LOs keys2edges,
+    LOs* prod_tv2v, LOs* prod_ev2v) {
   auto ev2v = mesh->ask_verts_of(EDGE);
   auto tv2v = mesh->ask_verts_of(TRI);
   auto e2t = mesh->ask_up(EDGE, TRI);
@@ -6,12 +7,13 @@ LOs swap2d_topology(Mesh* mesh, LOs keys2edges) {
   auto et2t = e2t.ab2b;
   auto et_codes = e2t.codes;
   auto nkeys = keys2edges.size();
-  auto prod_verts2verts = Write<LO>(nkeys * 2 * 3);
+  auto tri_verts2verts_w = Write<LO>(nkeys * 2 * 3);
+  auto edge_verts2verts_w = Write<LO>(nkeys * 1 * 2);
   auto f = LAMBDA(LO key) {
     auto e = keys2edges[key];
     CHECK(e2et[e + 1] == 2 + e2et[e]);
     LO t[2];
-    LO ov[2];
+    auto ov = &edge_verts2verts_w[key * 2];
     for (Int i = 0; i < 2; ++i) {
       auto et = e2et[e] + i;
       auto code = et_codes[et];
@@ -24,12 +26,13 @@ LOs swap2d_topology(Mesh* mesh, LOs keys2edges) {
     auto ev = gather_verts<2>(ev2v, e);
     for (Int i = 0; i < 2; ++i) {
       auto prod = key * 2 + i;
-      auto ntv = &prod_verts2verts[prod * 3];
+      auto ntv = &tri_verts2verts_w[prod * 3];
       ntv[0] = ev[1 - i];
       ntv[1] = ov[i];
       ntv[2] = ov[1 - i];
     }
   };
   parallel_for(nkeys, f);
-  return prod_verts2verts;
+  *prod_tv2v = tri_verts2verts_w;
+  *prod_ev2v = edge_verts2verts_w;
 }
