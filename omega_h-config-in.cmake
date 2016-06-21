79fae256b6c2c65a5de1b958f6edaa6702e451a5
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-06-21 14:00:19 -0400

    fix path derivation in exported CMake file

--- omega_h-config-in.cmake
+++ omega_h-config-in.cmake
@@ -1,5 +1,5 @@
 get_filename_component(OSH_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-get_filename_component(OSH_LIB_DIR "${SELF_DIR}" PATH)
+get_filename_component(OSH_LIB_DIR "${OSH_CMAKE_DIR}" PATH)
 include(${OSH_LIB_DIR}/omega_h-targets.cmake)
 get_filename_component(Omega_h_INCLUDE_DIRS "${OSH_LIB_DIR}/../include" ABSOLUTE)
 set(Omega_h_COMPILER "@CMAKE_CXX_COMPILER@")
