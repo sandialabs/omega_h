573e8f70905c172590a2b8d472d012ea88306751
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-06-21 14:08:52 -0400

    add include dirs of dependencies to exported CMake

--- omega_h-config-in.cmake
+++ omega_h-config-in.cmake
@@ -1,6 +1,7 @@
 get_filename_component(OSH_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 get_filename_component(OSH_LIB_DIR "${OSH_CMAKE_DIR}" PATH)
 include(${OSH_LIB_DIR}/omega_h-targets.cmake)
-get_filename_component(Omega_h_INCLUDE_DIRS "${OSH_LIB_DIR}/../include" ABSOLUTE)
+get_filename_component(Omega_h_INCLUDE_DIR "${OSH_LIB_DIR}/../include" ABSOLUTE)
+set(Omega_h_INCLUDE_DIRS ${Omega_h_INCLUDE_DIR} @DEP_INCLUDE_DIRS@)
 set(Omega_h_COMPILER "@CMAKE_CXX_COMPILER@")
 set(Omega_h_FLAGS "@FLAGS@")
