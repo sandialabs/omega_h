d18bbbbfb68b64b3bff45c376a35787778dd772e
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-06-25 14:24:10 -0400

    confirm that derived mesh is correct
    only elements and vertices will be the
    same, so we cannot directly use
    Mesh::operator==(), but comparing the
    key arrays succeeds.

--- unit_tests.cpp
+++ unit_tests.cpp
@@ -772,6 +772,11 @@ static void test_read_vtu(Library const& lib) {
   vtk::write_vtu(stream, &mesh0, mesh0.dim());
   Mesh mesh1;
   vtk::read_vtu(stream, mesh0.comm(), &mesh1);
+  CHECK(mesh0.dim() == mesh1.dim());
+  CHECK(mesh0.ask_verts_of(mesh0.dim()) ==
+        mesh1.ask_verts_of(mesh1.dim()));
+  CHECK(mesh0.coords() == mesh1.coords());
+  CHECK(mesh0.ask_globals(VERT) == mesh1.ask_globals(VERT));
 }
 
 int main(int argc, char** argv) {
