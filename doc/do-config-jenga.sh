6a53cbb0a17cb5589e5918c9a5058db2947feb64
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-06-28 10:40:02 -0400

    change another Jenga dir

--- doc/do-config-jenga.sh
+++ doc/do-config-jenga.sh
@@ -1,4 +1,4 @@
-cmake /lore/dibanez/trilinos/omega_h \
+cmake /lore/dibanez/trilinos/omega_h2 \
 -DCMAKE_CXX_COMPILER=mpicxx \
 -DCMAKE_INSTALL_PREFIX=/lore/dibanez/trilinos/omega_h2/install \
 -DOSH_USE_KOKKOS=True
