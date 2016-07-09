ab5f2a59b63d59f76cc492afefeba847093227d9
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-07-09 19:02:43 -0400

    cool new automated testing with Gmodel and Gmsh

---
+++ gen_balls_in_box.cpp
@@ -0,0 +1,26 @@
+#include <gmodel.hpp>
+
+int main()
+{
+  auto g = gmod::new_group();
+  auto fluid = gmod::new_cube(
+      gmod::Vector{0,0,0},
+      gmod::Vector{1,0,0},
+      gmod::Vector{0,1,0},
+      gmod::Vector{0,0,2});
+  gmod::add_to_group(g, fluid);
+  auto solid1 = gmod::new_ball(
+      gmod::Vector{0.5,0.5,0.5},
+      gmod::Vector{0,0,1},
+      gmod::Vector{0.2,0,0});
+  gmod::add_to_group(g, solid1);
+  gmod::insert_into(fluid, solid1);
+  auto solid2 = gmod::new_ball(
+      gmod::Vector{0.5,0.5,1.5},
+      gmod::Vector{0,0,1},
+      gmod::Vector{0.2,0,0});
+  gmod::add_to_group(g, solid2);
+  gmod::insert_into(fluid, solid2);
+  gmod::write_closure_to_geo(g, "balls_in_box.geo");
+  printf("solids are %u and %u\n", solid1->id, solid2->id);
+}
