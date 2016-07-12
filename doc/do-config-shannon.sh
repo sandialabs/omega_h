6a8faa30cbaaa55b00bf215d38014bbeb69f4fcd
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-07-12 07:33:05 -0600

    new Shannon configuration

--- doc/do-config-shannon.sh
+++ doc/do-config-shannon.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-cmake . \
--DCMAKE_CXX_COMPILER=$HOME/kokkos-install/bin/nvcc_wrapper \
+cmake .. \
+-DCMAKE_CXX_COMPILER=/home/daibane/minicontact/doc/dashboards/shannon.sandia.gov/shannon_local/nvcc_wrapper \
 -DCMAKE_INSTALL_PREFIX=$HOME/omega_h2-install \
--DKokkos_PREFIX=$HOME/kokkos-install \
+-DKokkos_PREFIX=/home/gahanse/nightly/build/TrilinosInstall \
 -DOSH_USE_KOKKOS=True \
--DOSH_USE_CUDA=True
+-DOSH_USE_CUDA=True \
+-DBUILD_TESTING=ON \
+-DOSH_ONE_FILE=ON
