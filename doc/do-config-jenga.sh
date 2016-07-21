0feb6796ba8c0199f5c68632a6310fde9e0f7fb7
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-07-21 18:55:40 -0400

    add find_dependency system

--- doc/do-config-jenga.sh
+++ doc/do-config-jenga.sh
@@ -1,4 +1,7 @@
 cmake /lore/dibanez/trilinos/omega_h2 \
 -DCMAKE_CXX_COMPILER=mpicxx \
 -DCMAKE_INSTALL_PREFIX=/lore/dibanez/trilinos/omega_h2/install \
--DOSH_USE_KOKKOS=True
+-DKokkos_PREFIX=/lore/dibanez/trilinos/trilinos/install \
+-DOSH_USE_KOKKOS=ON \
+-DOSH_ONE_FILE=OFF \
+-DBUILD_SHARED_LIBS=ON
