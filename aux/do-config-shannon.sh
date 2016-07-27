ec5ac9ac1fb598a47930b3d16157f07fbfc52939
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-07-27 07:59:49 -0600

    use $HOME in shannon configure

--- aux/do-config-shannon.sh
+++ aux/do-config-shannon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cmake .. \
--DCMAKE_CXX_COMPILER=/home/daibane/minicontact/doc/dashboards/shannon.sandia.gov/shannon_local/nvcc_wrapper \
+-DCMAKE_CXX_COMPILER=$HOME/minicontact/doc/dashboards/shannon.sandia.gov/shannon_local/nvcc_wrapper \
 -DCMAKE_INSTALL_PREFIX=$HOME/omega_h2-install \
 -DKokkos_PREFIX=$HOME/Trilinos-install \
 -DOSH_USE_KOKKOS=True \
