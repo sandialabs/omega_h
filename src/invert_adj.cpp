1557362e97d8d3b568b1542a954a155a4897bb2d
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-07-29 15:42:11 -0400

    try using local merge sort

--- src/invert_adj.cpp
+++ src/invert_adj.cpp
@@ -2,29 +2,45 @@
 
 #include "algorithm.hpp"
 #include "align.hpp"
+#include "local_sort.hpp"
 #include "loop.hpp"
 #include "map.hpp"
+
+#define MAX_UPWARD 100
 
 namespace osh {
 
 static void order_by_globals(LOs l2lh, Write<LO> lh2h, Write<I8> codes,
                              Read<GO> hg) {
+  struct Item {
+    GO high_global;
+    LO high;
+    I8 code;
+    DEVICE bool operator<(Item const& other) {
+      return high_global < other.high_global;
+    }
+  };
   LO nl = l2lh.size() - 1;
   auto f = LAMBDA(LO l) {
     LO begin = l2lh[l];
     LO end = l2lh[l + 1];
-    for (LO j = begin; j < end; ++j) {
-      LO k_min = j;
-      GO min_g = hg[lh2h[j]];
-      for (LO k = j + 1; k < end; ++k) {
-        GO g = hg[lh2h[k]];
-        if (g < min_g) {
-          k_min = k;
-          min_g = g;
-        }
-      }
-      swap2(lh2h[j], lh2h[k_min]);
-      swap2(codes[j], codes[k_min]);
+    CHECK(end - begin <= MAX_UPWARD);
+    LocalMergeSort<Item, MAX_UPWARD> sorter;
+    sorter.n = end - begin;
+    for (Int i = 0; i < sorter.n; ++i) {
+      auto lh = begin + i;
+      Item item;
+      item.high = lh2h[lh];
+      item.code = codes[lh];
+      item.high_global = hg[item.high];
+      sorter.array[i] = item;
+    }
+    sorter.run();
+    for (Int i = 0; i < sorter.n; ++i) {
+      auto lh = begin + i;
+      auto item = sorter.array[i];
+      lh2h[lh] = item.high;
+      codes[lh] = item.code;
     }
   };
   parallel_for(nl, f);
