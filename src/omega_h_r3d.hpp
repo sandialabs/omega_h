de508a6c86166615441893ce4ff9b222c2c83cfc
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-08-03 18:22:59 -0400

    clang-format

--- src/omega_h_r3d.hpp
+++ src/omega_h_r3d.hpp
@@ -102,12 +102,12 @@ struct ClipHelper<3> {
 template <>
 struct ClipHelper<2> {
   static void relink(Int onv, Int* nverts, Vertex<2>* vertbuffer) {
-    for(auto vstart = onv; vstart < *nverts; ++vstart) {
-      if(vertbuffer[vstart].pnbrs[1] >= 0) continue;
+    for (auto vstart = onv; vstart < *nverts; ++vstart) {
+      if (vertbuffer[vstart].pnbrs[1] >= 0) continue;
       auto vcur = vertbuffer[vstart].pnbrs[0];
       do {
         vcur = vertbuffer[vcur].pnbrs[0];
-      } while(vcur < onv);
+      } while (vcur < onv);
       vertbuffer[vstart].pnbrs[1] = vcur;
       vertbuffer[vcur].pnbrs[0] = vstart;
     }
@@ -115,7 +115,8 @@ struct ClipHelper<2> {
 };
 
 /**
- * \brief Clip a polytope against an arbitrary number of clip planes (find its intersection with a set of half-spaces).
+ * \brief Clip a polytope against an arbitrary number of clip planes (find its
+ * intersection with a set of half-spaces).
  *
  * \param [in, out] poly
  * The polygon to be clipped.
@@ -130,7 +131,7 @@ struct ClipHelper<2> {
 template <Int dim>
 OSH_INLINE void clip(Polytope<dim>* poly, Plane<dim>* planes, Int nplanes) {
   Int* nverts = &poly->nverts;
-  if(*nverts <= 0) return;
+  if (*nverts <= 0) return;
 
   // direct access to vertex buffer
   Vertex<dim>* vertbuffer = poly->verts;
@@ -143,7 +144,7 @@ OSH_INLINE void clip(Polytope<dim>* poly, Plane<dim>* planes, Int nplanes) {
   Real smin, smax;
 
   // loop over each clip plane
-  for(p = 0; p < nplanes; ++p) {
+  for (p = 0; p < nplanes; ++p) {
     // calculate signed distances to the clip plane
     onv = *nverts;
     smin = ArithTraits<Real>::max();
@@ -151,27 +152,27 @@ OSH_INLINE void clip(Polytope<dim>* poly, Plane<dim>* planes, Int nplanes) {
 
     // for marking clipped vertices
     Int clipped[decltype(poly)::max_verts] = {};  // all initialized to zero
-    for(v = 0; v < onv; ++v) {
+    for (v = 0; v < onv; ++v) {
       sdists[v] = planes[p].d + (vertbuffer[v].pos * planes[p].n);
-      if(sdists[v] < smin) smin = sdists[v];
-      if(sdists[v] > smax) smax = sdists[v];
-      if(sdists[v] < 0.0) clipped[v] = 1;
+      if (sdists[v] < smin) smin = sdists[v];
+      if (sdists[v] > smax) smax = sdists[v];
+      if (sdists[v] < 0.0) clipped[v] = 1;
     }
 
     // skip this face if the poly lies entirely on one side of it
-    if(smin >= 0.0) continue;
-    if(smax <= 0.0) {
+    if (smin >= 0.0) continue;
+    if (smax <= 0.0) {
       *nverts = 0;
       return;
     }
 
     // check all edges and insert new vertices on the bisected edges
-    for(vcur = 0; vcur < onv; ++vcur) {
-      if(clipped[vcur]) continue;
-      for(np = 0; np < dim; ++np) {
+    for (vcur = 0; vcur < onv; ++vcur) {
+      if (clipped[vcur]) continue;
+      for (np = 0; np < dim; ++np) {
         vnext = vertbuffer[vcur].pnbrs[np];
-        if(!clipped[vnext]) continue;
-        vertbuffer[*nverts].pnbrs[1-np] = vcur;
+        if (!clipped[vnext]) continue;
+        vertbuffer[*nverts].pnbrs[1 - np] = vcur;
         vertbuffer[*nverts].pnbrs[np] = -1;
         vertbuffer[vcur].pnbrs[np] = *nverts;
         vertbuffer[*nverts].pos = wav(vertbuffer[vcur].pos, -sdists[vnext],
@@ -187,14 +188,14 @@ OSH_INLINE void clip(Polytope<dim>* poly, Plane<dim>* planes, Int nplanes) {
     // go through and compress the vertex list, removing clipped verts
     // and re-indexing accordingly (reusing `clipped` to re-index everything)
     numunclipped = 0;
-    for(v = 0; v < *nverts; ++v) {
-      if(!clipped[v]) {
+    for (v = 0; v < *nverts; ++v) {
+      if (!clipped[v]) {
         vertbuffer[numunclipped] = vertbuffer[v];
         clipped[v] = numunclipped++;
       }
     }
     *nverts = numunclipped;
-    for(v = 0; v < *nverts; ++v)
+    for (v = 0; v < *nverts; ++v)
       for (np = 0; np < dim; ++np)
         vertbuffer[v].pnbrs[np] = clipped[vertbuffer[v].pnbrs[np]];
   }
@@ -263,11 +264,11 @@ OSH_INLINE void tet_faces_from_verts(Plane<3> faces[], Vector<3> verts[]) {
 }
 
 constexpr Int num_moments_3d(Int order) {
-  return ((order+1)*(order+2)*(order+3)/6);
+  return ((order + 1) * (order + 2) * (order + 3) / 6);
 }
 
 constexpr Int num_moments_2d(Int order) {
-  return ((order+1)*(order+2)/2);
+  return ((order + 1) * (order + 2) / 2);
 }
 
 template <Int dim, Int order>
@@ -284,20 +285,25 @@ struct NumMoments<2, order> {
 };
 
 /**
- * \brief Integrate a polynomial density over a polyhedron using simplicial decomposition.
- * Uses the fast recursive method of Koehl (2012) to carry out the integration.
+ * \brief Integrate a polynomial density over a polyhedron using simplicial
+ * decomposition.
+ *
+ * \details Uses the fast recursive method of Koehl (2012) to carry out the
+ * integration.
  *
  * \param [in] poly
  * The polyhedron over which to integrate.
  *
  * \param [in] polyorder
- * Order of the polynomial density field. 0 for constant (1 moment), 1 for linear
- * (4 moments), 2 for quadratic (10 moments), etc.
+ * Order of the polynomial density field. 0 for constant (1 moment), 1 for
+ * linear (4 moments), 2 for quadratic (10 moments), etc.
  *
  * \param [in, out] moments
- * Array to be filled with the integration results, up to the specified `polyorder`. Must be at
- * least `(polyorder+1)*(polyorder+2)*(polyorder+3)/6` long. A conventient macro,
- * `num_moments_3d()` is provided to compute the number of moments for a given order.
+ * Array to be filled with the integration results, up to the specified
+ * `polyorder`. Must be at least
+ * `(polyorder+1)*(polyorder+2)*(polyorder+3)/6` long.
+ * A conventient macro, `num_moments_3d()` is provided to compute the number
+ * of moments for a given order.
  * Order of moments is row-major, i.e. `1`, `x`, `y`, `z`, `x^2`,
  * `x*y`, `x*z`, `y^2`, `y*z`, `z^2`, `x^3`, `x^2*y`...
  *
@@ -306,7 +312,7 @@ struct NumMoments<2, order> {
 template <Int polyorder>
 OSH_INLINE void reduce_3d(Polytope<3>* poly, Real* moments) {
   Int* nverts = &poly->nverts;
-  if(*nverts <= 0) return;
+  if (*nverts <= 0) return;
 
   // var declarations
   Real sixv;
@@ -318,141 +324,147 @@ OSH_INLINE void reduce_3d(Polytope<3>* poly, Real* moments) {
   Vertex<3>* vertbuffer = poly->verts;
 
   // zero the moments
-  for(m = 0; m < num_moments_3d(polyorder); ++m)
-    moments[m] = 0.0;
+  for (m = 0; m < num_moments_3d(polyorder); ++m) moments[m] = 0.0;
 
   // for keeping track of which edges have been visited
-  Int emarks[Polytope<3>::max_verts][3] = {{}}; // initialized to zero
+  Int emarks[Polytope<3>::max_verts][3] = {{}};  // initialized to zero
 
   // Storage for coefficients
   // keep two layers of the pyramid of coefficients
   // Note: Uses twice as much space as needed, but indexing is faster this way
   Int prevlayer = 0;
   Int curlayer = 1;
-  Real S[polyorder+1][polyorder+1][2];
-  Real D[polyorder+1][polyorder+1][2];
-  Real C[polyorder+1][polyorder+1][2];
+  Real S[polyorder + 1][polyorder + 1][2];
+  Real D[polyorder + 1][polyorder + 1][2];
+  Real C[polyorder + 1][polyorder + 1][2];
 
   // loop over all vertices to find the starting point for each face
-  for(vstart = 0; vstart < *nverts; ++vstart)
-  for(pstart = 0; pstart < 3; ++pstart) {
+  for (vstart = 0; vstart < *nverts; ++vstart)
+    for (pstart = 0; pstart < 3; ++pstart) {
+      // skip this face if we have marked it
+      if (emarks[vstart][pstart]) continue;
 
-    // skip this face if we have marked it
-    if(emarks[vstart][pstart]) continue;
-
-    // initialize face looping
-    pnext = pstart;
-    vcur = vstart;
-    emarks[vcur][pnext] = 1;
-    vnext = vertbuffer[vcur].pnbrs[pnext];
-    v0 = vertbuffer[vcur].pos;
-
-    // move to the second edge
-    for(np = 0; np < 3; ++np) if(vertbuffer[vnext].pnbrs[np] == vcur) break;
-    vcur = vnext;
-    pnext = (np+1)%3;
-    emarks[vcur][pnext] = 1;
-    vnext = vertbuffer[vcur].pnbrs[pnext];
-
-    // make a triangle fan using edges
-    // and first vertex
-    while(vnext != vstart) {
-
-      v2 = vertbuffer[vcur].pos;
-      v1 = vertbuffer[vnext].pos;
-
-      sixv = (-v2[0]*v1[1]*v0[2] + v1[0]*v2[1]*v0[2] + v2[0]*v0[1]*v1[2]
-             - v0[0]*v2[1]*v1[2] - v1[0]*v0[1]*v2[2] + v0[0]*v1[1]*v2[2]);
-
-      // calculate the moments
-      // using the fast recursive method of Koehl (2012)
-      // essentially building a set of trinomial pyramids, one layer at a time
-
-      // base case
-      S[0][0][prevlayer] = 1.0;
-      D[0][0][prevlayer] = 1.0;
-      C[0][0][prevlayer] = 1.0;
-      moments[0] += ONE_SIXTH*sixv;
-
-      // build up successive polynomial orders
-      for(corder = 1, m = 1; corder <= polyorder; ++corder) {
-        for(i = corder; i >= 0; --i)
-        for(j = corder - i; j >= 0; --j, ++m) {
-          k = corder - i - j;
-          C[i][j][curlayer] = 0;
-          D[i][j][curlayer] = 0;
-          S[i][j][curlayer] = 0;
-          if(i > 0) {
-            C[i][j][curlayer] += v2[0]*C[i-1][j][prevlayer];
-            D[i][j][curlayer] += v1[0]*D[i-1][j][prevlayer];
-            S[i][j][curlayer] += v0[0]*S[i-1][j][prevlayer];
-          }
-          if(j > 0) {
-            C[i][j][curlayer] += v2[1]*C[i][j-1][prevlayer];
-            D[i][j][curlayer] += v1[1]*D[i][j-1][prevlayer];
-            S[i][j][curlayer] += v0[1]*S[i][j-1][prevlayer];
-          }
-          if(k > 0) {
-            C[i][j][curlayer] += v2[2]*C[i][j][prevlayer];
-            D[i][j][curlayer] += v1[2]*D[i][j][prevlayer];
-            S[i][j][curlayer] += v0[2]*S[i][j][prevlayer];
-          }
-          D[i][j][curlayer] += C[i][j][curlayer];
-          S[i][j][curlayer] += D[i][j][curlayer];
-          moments[m] += sixv*S[i][j][curlayer];
-        }
-        curlayer = 1 - curlayer;
-        prevlayer = 1 - prevlayer;
-      }
-
-      // move to the next edge
-      for(np = 0; np < 3; ++np) if(vertbuffer[vnext].pnbrs[np] == vcur) break;
-      vcur = vnext;
-      pnext = (np+1)%3;
+      // initialize face looping
+      pnext = pstart;
+      vcur = vstart;
       emarks[vcur][pnext] = 1;
       vnext = vertbuffer[vcur].pnbrs[pnext];
+      v0 = vertbuffer[vcur].pos;
+
+      // move to the second edge
+      for (np = 0; np < 3; ++np)
+        if (vertbuffer[vnext].pnbrs[np] == vcur) break;
+      vcur = vnext;
+      pnext = (np + 1) % 3;
+      emarks[vcur][pnext] = 1;
+      vnext = vertbuffer[vcur].pnbrs[pnext];
+
+      // make a triangle fan using edges
+      // and first vertex
+      while (vnext != vstart) {
+        v2 = vertbuffer[vcur].pos;
+        v1 = vertbuffer[vnext].pos;
+
+        sixv = (-v2[0] * v1[1] * v0[2] + v1[0] * v2[1] * v0[2] +
+                v2[0] * v0[1] * v1[2] - v0[0] * v2[1] * v1[2] -
+                v1[0] * v0[1] * v2[2] + v0[0] * v1[1] * v2[2]);
+
+        // calculate the moments
+        // using the fast recursive method of Koehl (2012)
+        // essentially building a set of trinomial pyramids, one layer at a time
+
+        // base case
+        S[0][0][prevlayer] = 1.0;
+        D[0][0][prevlayer] = 1.0;
+        C[0][0][prevlayer] = 1.0;
+        moments[0] += ONE_SIXTH * sixv;
+
+        // build up successive polynomial orders
+        for (corder = 1, m = 1; corder <= polyorder; ++corder) {
+          for (i = corder; i >= 0; --i)
+            for (j = corder - i; j >= 0; --j, ++m) {
+              k = corder - i - j;
+              C[i][j][curlayer] = 0;
+              D[i][j][curlayer] = 0;
+              S[i][j][curlayer] = 0;
+              if (i > 0) {
+                C[i][j][curlayer] += v2[0] * C[i - 1][j][prevlayer];
+                D[i][j][curlayer] += v1[0] * D[i - 1][j][prevlayer];
+                S[i][j][curlayer] += v0[0] * S[i - 1][j][prevlayer];
+              }
+              if (j > 0) {
+                C[i][j][curlayer] += v2[1] * C[i][j - 1][prevlayer];
+                D[i][j][curlayer] += v1[1] * D[i][j - 1][prevlayer];
+                S[i][j][curlayer] += v0[1] * S[i][j - 1][prevlayer];
+              }
+              if (k > 0) {
+                C[i][j][curlayer] += v2[2] * C[i][j][prevlayer];
+                D[i][j][curlayer] += v1[2] * D[i][j][prevlayer];
+                S[i][j][curlayer] += v0[2] * S[i][j][prevlayer];
+              }
+              D[i][j][curlayer] += C[i][j][curlayer];
+              S[i][j][curlayer] += D[i][j][curlayer];
+              moments[m] += sixv * S[i][j][curlayer];
+            }
+          curlayer = 1 - curlayer;
+          prevlayer = 1 - prevlayer;
+        }
+
+        // move to the next edge
+        for (np = 0; np < 3; ++np)
+          if (vertbuffer[vnext].pnbrs[np] == vcur) break;
+        vcur = vnext;
+        pnext = (np + 1) % 3;
+        emarks[vcur][pnext] = 1;
+        vnext = vertbuffer[vcur].pnbrs[pnext];
+      }
     }
-  }
 
   // reuse C to recursively compute the leading multinomial coefficients
   C[0][0][prevlayer] = 1.0;
-  for(corder = 1, m = 1; corder <= polyorder; ++corder) {
-    for(i = corder; i >= 0; --i)
-    for(j = corder - i; j >= 0; --j, ++m) {
-      k = corder - i - j;
-      C[i][j][curlayer] = 0.0;
-      if(i > 0) C[i][j][curlayer] += C[i-1][j][prevlayer];
-      if(j > 0) C[i][j][curlayer] += C[i][j-1][prevlayer];
-      if(k > 0) C[i][j][curlayer] += C[i][j][prevlayer];
-      moments[m] /= C[i][j][curlayer]*(corder+1)*(corder+2)*(corder+3);
-    }
+  for (corder = 1, m = 1; corder <= polyorder; ++corder) {
+    for (i = corder; i >= 0; --i)
+      for (j = corder - i; j >= 0; --j, ++m) {
+        k = corder - i - j;
+        C[i][j][curlayer] = 0.0;
+        if (i > 0) C[i][j][curlayer] += C[i - 1][j][prevlayer];
+        if (j > 0) C[i][j][curlayer] += C[i][j - 1][prevlayer];
+        if (k > 0) C[i][j][curlayer] += C[i][j][prevlayer];
+        moments[m] /=
+            C[i][j][curlayer] * (corder + 1) * (corder + 2) * (corder + 3);
+      }
     curlayer = 1 - curlayer;
     prevlayer = 1 - prevlayer;
   }
 }
 
 /**
- * \brief Integrate a polynomial density over a polygon using simplicial decomposition.
- * Uses the fast recursive method of Koehl (2012) to carry out the integration.
+ * \brief Integrate a polynomial density over a polygon using simplicial
+ * decomposition.
+ *
+ * \details Uses the fast recursive method of Koehl (2012) to carry out the
+ * integration.
  *
  * \param [in] poly
  * The polygon over which to integrate.
  *
  * \param [in] polyorder
- * Order of the polynomial density field. 0 for constant (1 moment), 1 for linear
- * (4 moments), 2 for quadratic (10 moments), etc.
+ * Order of the polynomial density field. 0 for constant (1 moment), 1 for
+ * linear (4 moments), 2 for quadratic (10 moments), etc.
  *
  * \param [in, out] moments
- * Array to be filled with the integration results, up to the specified `polyorder`. Must be at
- * least `(polyorder+1)*(polyorder+2)/2` long. A conventient macro,
- * `R2D_NUM_MOMENTS()` is provided to compute the number of moments for a given order.
- * Order of moments is row-major, i.e. `1`, `x`, `y`, `x^2`, `x*y`, `y^2`, `x^3`, `x^2*y`...
+ * Array to be filled with the integration results, up to the specified
+ * `polyorder`. Must be at least `(polyorder+1)*(polyorder+2)/2` long.
+ * A conventient macro, `R2D_NUM_MOMENTS()` is provided to compute the
+ * number of moments for a given order.
+ * Order of moments is row-major, i.e. `1`, `x`, `y`, `x^2`, `x*y`, `y^2`,
+ * `x^3`, `x^2*y`...
  *
  */
 template <Int polyorder>
 OSH_INLINE void reduce_2d(Polytope<2>* poly, Real* moments) {
   Int* nverts = &poly->nverts;
-  if(*nverts <= 0) return;
+  if (*nverts <= 0) return;
 
   // var declarations
   Int vcur, vnext, m, i, j, corder;
@@ -463,23 +475,21 @@ OSH_INLINE void reduce_2d(Polytope<2>* poly, Real* moments) {
   Vertex<2>* vertbuffer = poly->verts;
 
   // zero the moments
-  for(m = 0; m < num_moments_2d(polyorder); ++m)
-    moments[m] = 0.0;
+  for (m = 0; m < num_moments_2d(polyorder); ++m) moments[m] = 0.0;
 
   // Storage for coefficients
   // keep two layers of the triangle of coefficients
   Int prevlayer = 0;
   Int curlayer = 1;
-  Real D[polyorder+1][2];
-  Real C[polyorder+1][2];
+  Real D[polyorder + 1][2];
+  Real C[polyorder + 1][2];
 
   // iterate over edges and compute a sum over simplices
-  for(vcur = 0; vcur < *nverts; ++vcur) {
-
+  for (vcur = 0; vcur < *nverts; ++vcur) {
     vnext = vertbuffer[vcur].pnbrs[0];
     v0 = vertbuffer[vcur].pos;
     v1 = vertbuffer[vnext].pos;
-    twoa = (v0[0]*v1[1] - v0[1]*v1[0]);
+    twoa = (v0[0] * v1[1] - v0[1] * v1[0]);
 
     // calculate the moments
     // using the fast recursive method of Koehl (2012)
@@ -488,24 +498,24 @@ OSH_INLINE void reduce_2d(Polytope<2>* poly, Real* moments) {
     // base case
     D[0][prevlayer] = 1.0;
     C[0][prevlayer] = 1.0;
-    moments[0] += 0.5*twoa;
+    moments[0] += 0.5 * twoa;
 
     // build up successive polynomial orders
-    for(corder = 1, m = 1; corder <= polyorder; ++corder) {
-      for(i = corder; i >= 0; --i, ++m) {
+    for (corder = 1, m = 1; corder <= polyorder; ++corder) {
+      for (i = corder; i >= 0; --i, ++m) {
         j = corder - i;
         C[i][curlayer] = 0;
         D[i][curlayer] = 0;
-        if(i > 0) {
-          C[i][curlayer] += v1[0]*C[i-1][prevlayer];
-          D[i][curlayer] += v0[0]*D[i-1][prevlayer];
+        if (i > 0) {
+          C[i][curlayer] += v1[0] * C[i - 1][prevlayer];
+          D[i][curlayer] += v0[0] * D[i - 1][prevlayer];
         }
-        if(j > 0) {
-          C[i][curlayer] += v1[1]*C[i][prevlayer];
-          D[i][curlayer] += v0[1]*D[i][prevlayer];
+        if (j > 0) {
+          C[i][curlayer] += v1[1] * C[i][prevlayer];
+          D[i][curlayer] += v0[1] * D[i][prevlayer];
         }
         D[i][curlayer] += C[i][curlayer];
-        moments[m] += twoa*D[i][curlayer];
+        moments[m] += twoa * D[i][curlayer];
       }
       curlayer = 1 - curlayer;
       prevlayer = 1 - prevlayer;
@@ -514,13 +524,13 @@ OSH_INLINE void reduce_2d(Polytope<2>* poly, Real* moments) {
 
   // reuse C to recursively compute the leading multinomial coefficients
   C[0][prevlayer] = 1.0;
-  for(corder = 1, m = 1; corder <= polyorder; ++corder) {
-    for(i = corder; i >= 0; --i, ++m) {
+  for (corder = 1, m = 1; corder <= polyorder; ++corder) {
+    for (i = corder; i >= 0; --i, ++m) {
       j = corder - i;
       C[i][curlayer] = 0.0;
-      if(i > 0) C[i][curlayer] += C[i-1][prevlayer];
-      if(j > 0) C[i][curlayer] += C[i][prevlayer];
-      moments[m] /= C[i][curlayer]*(corder+1)*(corder+2);
+      if (i > 0) C[i][curlayer] += C[i - 1][prevlayer];
+      if (j > 0) C[i][curlayer] += C[i][prevlayer];
+      moments[m] /= C[i][curlayer] * (corder + 1) * (corder + 2);
     }
     curlayer = 1 - curlayer;
     prevlayer = 1 - prevlayer;
@@ -551,7 +561,8 @@ struct Integration<3, order> {
 };
 
 template <Int dim, Int order>
-OSH_INLINE Real integrate(Polytope<dim>* polytope, Polynomial<dim, order> polynomial) {
+OSH_INLINE Real integrate(Polytope<dim>* polytope,
+                          Polynomial<dim, order> polynomial) {
   Real moments[decltype(polynomial)::nterms];
   Integration<dim, order>::get_moments(polytope, moments);
   Real result = 0;
