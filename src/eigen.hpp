sandialabs/omega_h

commit 1941e676ec340d414c8ffe7b386f6eeef5c21ebb

pure functional eigendecompositions

src/eigen.hpp
@@ -28,9 +28,9 @@ INLINE void characteristic_quadratic(Matrix<2, 2> A, Real& a, Real& b) {
 
 /* in the case that the null space is 1D and space is 3D,
    take the largest cross product of any pair of columns */
-INLINE void single_eigenvector(Matrix<3, 3> m, Real l, Vector<3>& v) {
+INLINE Vector<3> single_eigenvector(Matrix<3, 3> m, Real l) {
   subtract_from_diag(m, l);
-  v = cross(m[0], m[1]);
+  auto v = cross(m[0], m[1]);
   Real v_norm = norm(v);
   Vector<3> c = cross(m[1], m[2]);
   Real c_norm = norm(c);
@@ -46,6 +46,7 @@ INLINE void single_eigenvector(Matrix<3, 3> m, Real l, Vector<3>& v) {
   }
   CHECK(v_norm > EPSILON);
   v = v / v_norm;
+  return v;
 }
 
 template <Int m>
@@ -66,56 +67,66 @@ INLINE Vector<m> get_1d_column_space(Matrix<m, m> a) {
 /* in the case that the null space is 2D, find the
    largest-norm column and get a couple vectors
    orthogonal to that */
-INLINE void double_eigenvector(
-    Matrix<3, 3> m, Real l, Vector<3>& u, Vector<3>& v) {
+INLINE Few<Vector<3>, 2> double_eigenvector(Matrix<3, 3> m, Real l) {
   subtract_from_diag(m, l);
   Vector<3> n = get_1d_column_space(m);
   Matrix<3, 3> b = form_ortho_basis(n);
-  u = b[1];
-  v = b[2];
+  return {b[1], b[2]};
 }
 
-INLINE void decompose_eigen2(Matrix<3, 3> m, Matrix<3, 3>& q, Vector<3>& l) {
+template <Int dim>
+struct EigenDecomposition {
+  Matrix<dim, dim> q;
+  Vector<dim> l;
+};
+
+INLINE EigenDecomposition<3> decompose_eigen_dim(Matrix<3, 3> m) {
   Real a, b, c;
   characteristic_cubic(m, a, b, c);
   Few<Real, 3> roots;
   Few<Int, 3> mults;
   Int nroots = solve_cubic(a, b, c, roots, mults);
   /* there are only a few output cases, see solve_cubic() */
+  Matrix<3, 3> q;
+  Vector<3> l;
   if (nroots == 3) {
     for (Int i = 0; i < 3; ++i) {
-      single_eigenvector(m, roots[i], q[i]);
+      q[i] = single_eigenvector(m, roots[i]);
       l[i] = roots[i];
     }
   } else if (nroots == 2 && mults[1] == 2) {
-    single_eigenvector(m, roots[0], q[0]);
+    q[0] = single_eigenvector(m, roots[0]);
     l[0] = roots[0];
-    double_eigenvector(m, roots[1], q[1], q[2]);
+    auto dev = double_eigenvector(m, roots[1]);
+    q[1] = dev[0]; q[2] = dev[1];
     l[1] = l[2] = roots[1];
   } else {
     CHECK(nroots == 1 && mults[0] == 3);
     l[0] = l[1] = l[2] = roots[0];
     q = identity_matrix<3, 3>();
   }
+  return {q, l};
 }
 
 /* in the case that the null space is 1D and space is 2D,
    get the largest column and rotate it 90 deg */
-INLINE void single_eigenvector(Matrix<2, 2> m, Real l, Vector<2>& v) {
+INLINE Vector<2> single_eigenvector(Matrix<2, 2> m, Real l) {
   Matrix<2, 2> s = (m - (l * identity_matrix<2, 2>()));
-  v = perp(get_1d_column_space(s));
+  return perp(get_1d_column_space(s));
 }
 
-INLINE void decompose_eigen2(Matrix<2, 2> m, Matrix<2, 2>& q, Vector<2>& l) {
+INLINE EigenDecomposition<2> decompose_eigen_dim(Matrix<2, 2> m) {
   Real a, b;
   characteristic_quadratic(m, a, b);
   Few<Real, 2> roots;
   Few<Int, 2> mults;
   Int nroots = solve_quadratic(a, b, roots, mults);
   /* there are only a few output cases, see solve_quadratic() */
+  Matrix<2, 2> q;
+  Vector<2> l;
   if (nroots == 2) {
     for (Int i = 0; i < 2; ++i) {
-      single_eigenvector(m, roots[i], q[i]);
+      q[i] = single_eigenvector(m, roots[i]);
       l[i] = roots[i];
     }
   } else {
@@ -123,6 +134,7 @@ INLINE void decompose_eigen2(Matrix<2, 2> m, Matrix<2, 2>& q, Vector<2>& l) {
     l[0] = l[1] = roots[0];
     q = identity_matrix<2, 2>();
   }
+  return {q, l};
 }
 
 /* decompose an m x m matrix (where m <= 3) into
@@ -137,21 +149,18 @@ INLINE void decompose_eigen2(Matrix<2, 2> m, Matrix<2, 2>& q, Vector<2>& l) {
    the output should satisfy
      m ~= transpose(q * diagonal(l) * invert(q)) */
 template <Int dim>
-INLINE void decompose_eigen(
-    Matrix<dim, dim> m, Matrix<dim, dim>& q, Vector<dim>& l) {
+INLINE EigenDecomposition<dim> decompose_eigen(Matrix<dim, dim> m) {
   /* the cubic solver is especially sensitive to dynamic
      range. what we can do is to normalize the input matrix
      and then re-apply that norm to the resulting roots */
   Real nm = max_norm(m);
-  if (nm > EPSILON) {
-    m = m / nm;
-    decompose_eigen2(m, q, l);
-    l = l * nm;
-  } else {
+  if (nm <= EPSILON) {
     /* this is the zero matrix... */
-    q = identity_matrix<dim, dim>();
-    l = zero_vector<dim>();
+    return {identity_matrix<dim, dim>(), zero_vector<dim>()};
   }
+  m = m / nm;
+  auto decomp = decompose_eigen_dim(m);
+  return {decomp.q, decomp.l * nm};
 }
 
 /* Q, again, being the matrix whose columns

src/inertia.cpp
@@ -48,9 +48,9 @@ Matrix<3, 3> get_matrix(
 
 Vector<3> get_axis(CommPtr comm, Reals coords, Reals masses, Vector<3> center) {
   auto m = get_matrix(comm, coords, masses, center);
-  Matrix<3, 3> q;
-  Vector<3> l;
-  decompose_eigen(m, q, l);
+  auto ed = decompose_eigen(m);
+  auto l = ed.l;
+  auto q = ed.q;
   Int min_i = 0;
   for (Int i = 1; i < 3; ++i) {
     if (l[i] < l[min_i]) {

src/metric.hpp
@@ -55,10 +55,7 @@ template <Int dim>
 INLINE Matrix<dim, dim> common_metric_basis(
     Matrix<dim, dim> a, Matrix<dim, dim> b) {
   auto c = invert(a) * b;
-  Matrix<dim, dim> p;
-  Vector<dim> l;
-  decompose_eigen(c, p, l);
-  return p;
+  return decompose_eigen(c).q;
 }
 
 template <Int dim>

src/perf_tests.cpp
@@ -58,9 +58,7 @@ static void test_metric_decompose(Reals metrics) {
   Write<Real> write_eigenvs(nelems);
   auto f1 = LAMBDA(Int i) {
     auto m = get_symm<3>(metrics, i);
-    Matrix<3, 3> r;
-    Vector<3> l;
-    decompose_eigen(m, r, l);
+    auto l = decompose_eigen(m).l;
     auto eigenv = max2(max2(l[0], l[1]), l[2]);
     write_eigenvs[i] = eigenv;
   };

src/size.cpp
@@ -290,4 +290,11 @@ Reals scale_metric_for_nelems(Mesh* mesh, Reals v2m, Real target_nelems) {
   return multiply_each_by(m_scal, v2m);
 }
 
+#if 0
+template <Int dim>
+INLINE Matrix<dim, dim> metric_from_hessian(Matrix<dim, dim> hessian,
+    Real error_bound, Real hmin, Real hmax) {
+}
+#endif
+
 }  // end namespace Omega_h

src/unit_tests.cpp
@@ -69,25 +69,25 @@ static void test_cubic() {
 
 static void test_eigen_cubic(
     Matrix<3, 3> m, Matrix<3, 3> q_expect, Vector<3> l_expect) {
-  Matrix<3, 3> q;
-  Vector<3> l;
-  decompose_eigen(m, q, l);
+  auto ed = decompose_eigen(m);
+  auto q = ed.q;
+  auto l = ed.l;
   CHECK(are_close(q, q_expect));
   CHECK(are_close(l, l_expect));
 }
 
 static void test_eigen_cubic(Matrix<3, 3> m, Vector<3> l_expect) {
-  Matrix<3, 3> q;
-  Vector<3> l;
-  decompose_eigen(m, q, l);
+  auto ed = decompose_eigen(m);
+  auto q = ed.q;
+  auto l = ed.l;
   CHECK(are_close(l, l_expect, 1e-8, 1e-8));
   CHECK(are_close(m, compose_eigen(q, l)));
 }
 
 static void test_eigen_cubic_ortho(Matrix<3, 3> m, Vector<3> l_expect) {
-  Matrix<3, 3> q;
-  Vector<3> l;
-  decompose_eigen(m, q, l);
+  auto ed = decompose_eigen(m);
+  auto q = ed.q;
+  auto l = ed.l;
   CHECK(are_close(transpose(q) * q, identity_matrix<3, 3>(), 1e-8, 1e-8));
   CHECK(are_close(l, l_expect, 1e-8, 1e-8));
   CHECK(are_close(m, compose_ortho(q, l), 1e-8, 1e-8));

commit 974babcd8b1b385fbbc6d52de63ea2a4ab627110

fix CUDA compile regression in 1941e67
the initializer-list ctor for Few is
a __host__ function, can't use it
in device code.

src/eigen.hpp
@@ -71,7 +71,9 @@ INLINE Few<Vector<3>, 2> double_eigenvector(Matrix<3, 3> m, Real l) {
   subtract_from_diag(m, l);
   Vector<3> n = get_1d_column_space(m);
   Matrix<3, 3> b = form_ortho_basis(n);
-  return {b[1], b[2]};
+  Few<Vector<3>, 2> out;
+  out[0] = b[1]; out[1] = b[2];
+  return out;
 }
 
 template <Int dim>