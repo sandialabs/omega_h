96ff9af8cfa87751486e9aab74183a86c9344ce7
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-09-05 20:08:17 -0400

    turn up verbosity settings to debug

--- src/coarsen.cpp
+++ src/coarsen.cpp
@@ -152,12 +152,12 @@ static void coarsen_element_based2(Mesh* mesh, bool verbose) {
 bool coarsen(Mesh* mesh, Real min_qual, bool improve, bool verbose) {
   if (!coarsen_element_based1(mesh)) return false;
   Int nghost_layers = needs_buffer_layers(mesh) ? 3 : 1;
-  mesh->set_parting(OMEGA_H_GHOSTED, nghost_layers);
+  mesh->set_parting(OMEGA_H_GHOSTED, nghost_layers, true);
   if (!coarsen_ghosted(mesh, min_qual, improve)) return false;
   if (needs_buffer_layers(mesh)) {
-    mesh->set_parting(OMEGA_H_GHOSTED, 1);
+    mesh->set_parting(OMEGA_H_GHOSTED, 1, true);
   } else {
-    mesh->set_parting(OMEGA_H_ELEM_BASED);
+    mesh->set_parting(OMEGA_H_ELEM_BASED, true);
   }
   coarsen_element_based2(mesh, verbose);
   return true;
