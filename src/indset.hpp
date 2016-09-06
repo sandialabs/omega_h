1b776f797979e870c4e35ba63366429510b28f9c
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-09-05 20:08:42 -0400

    find_indset variant that accepts a Graph

--- src/indset.hpp
+++ src/indset.hpp
@@ -6,6 +6,9 @@
 namespace Omega_h {
 
 Read<I8> find_indset(
+    Mesh* mesh, Int ent_dim, Graph graph,
+    Reals quality, Read<I8> candidates);
+Read<I8> find_indset(
     Mesh* mesh, Int ent_dim, Reals quality, Read<I8> candidates);
 
 }  // end namespace Omega_h
