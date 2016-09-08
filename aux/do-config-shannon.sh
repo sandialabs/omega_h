b5c602d6d3037cca71a63e7391a025246afc8f4b
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-09-08 12:15:40 -0600

    update Shannon config

--- aux/do-config-shannon.sh
+++ aux/do-config-shannon.sh
@@ -3,7 +3,7 @@ cmake .. \
 -DCMAKE_CXX_COMPILER=$HOME/minicontact/doc/dashboards/shannon.sandia.gov/shannon_local/nvcc_wrapper \
 -DCMAKE_INSTALL_PREFIX=$HOME/omega_h2-install \
 -DKokkos_PREFIX=$HOME/Trilinos-install \
--DOSH_USE_KOKKOS=True \
--DOSH_USE_CUDA=True \
+-DOmega_h_USE_Kokkos=True \
+-DOmega_h_USE_CUDA=True \
 -DBUILD_TESTING=ON \
--DOSH_ONE_FILE=ON
+-DOmega_h_ONE_FILE=ON
