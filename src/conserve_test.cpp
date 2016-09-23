c39b4b9cb9ffdb0f9fb9d0c0f82c592fbfc9778c
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-09-23 14:14:25 -0400

    add beginnings of momentum conservation test

---
+++ src/conserve_test.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+#include "access.hpp"
+#include "array.hpp"
+#include "internal.hpp"
+#include "loop.hpp"
+#include "size.hpp"
+#include "space.hpp"
+#include "timer.hpp"
+
+using namespace Omega_h;
+
+static void postprocess_conserve(Mesh* mesh) {
+  auto volume = measure_elements_real(mesh);
+  auto mass = mesh->get_array<Real>(mesh->dim(), "mass");
+  CHECK(are_close(1.0, sum(mesh->comm(), mass)));
+  auto density = divide_each(mass, volume);
+  mesh->add_tag(mesh->dim(), "density", 1, OMEGA_H_DONT_TRANSFER,
+      OMEGA_H_DO_OUTPUT, density);
+}
+
+int main(int argc, char** argv) {
+  auto lib = Library(&argc, &argv);
+  auto world = lib.world();
+  Mesh mesh;
+  if (world->rank() == 0) {
+    auto nx = 10;
+    build_box(&mesh, lib, 1, 1, 0, nx, nx, 0);
+    classify_by_angles(&mesh, PI / 4);
+    mesh.reorder();
+    mesh.reset_globals();
+  }
+  mesh.set_comm(world);
+  mesh.balance();
+  mesh.set_parting(OMEGA_H_GHOSTED);
+  auto size = find_identity_size(&mesh);
+  size = multiply_each_by(2.0, size);
+  mesh.add_tag(VERT, "size", 1, OMEGA_H_LINEAR_INTERP, OMEGA_H_DO_OUTPUT, size);
+  mesh.add_tag(mesh.dim(), "mass", 1, OMEGA_H_CONSERVE, OMEGA_H_DO_OUTPUT,
+      measure_elements_real(&mesh));
+  mesh.add_tag(VERT, "velocity", mesh.dim(), OMEGA_H_MOMENTUM_VELOCITY,
+    OMEGA_H_DO_OUTPUT, Reals(mesh.nverts() * mesh.dim(), 1));
+  Now t0 = now();
+  adapt(&mesh, 0.30, 0.30, 2.0 / 3.0, 4.0 / 3.0, 4, 3);
+  Now t1 = now();
+  mesh.set_parting(OMEGA_H_ELEM_BASED);
+  if (mesh.comm()->rank() == 0) {
+    std::cout << "test took " << (t1 - t0) << " seconds\n";
+  }
+  postprocess_conserve(&mesh);
+  bool ok = check_regression("gold_conserve", &mesh, 0.0, 0.0);
+  if (!ok) return 2;
+  return 0;
+}
+
--- src/transfer_conserve.cpp
+++ src/transfer_conserve.cpp
@@ -266,16 +266,11 @@ class MomentumVelocity {
       Int key_dim, TagBase const* tagbase,
       LOs keys2kds, LOs keys2prods, LOs prods2new_elems,
       LOs same_verts2old_verts, LOs same_verts2new_verts) {
-    auto velocity_name = tagbase->name();
-    if (velocity_name != "velocity") {
-      Omega_h_fail(
-          "%s tranferred as momentum-conserving velocity,"
-          " but its name is not \"velocity\"\n",
-          velocity_name.c_str());
-    }
+    CHECK(tagbase->name() == "velocity");
+    CHECK(tagbase->ncomps() == dim);
     this->target_masses = target_mesh->get_array<Real>(dim, "mass");
     this->donor_masses = donor_mesh->get_array<Real>(dim, "mass");
-    this->donor_velocities = donor_mesh->get_array<Real>(VERT, velocity_name);
+    this->donor_velocities = donor_mesh->get_array<Real>(VERT, "velocity");
     this->target_velocities = Write<Real>(target_mesh->nverts() * dim);
     auto same_velocities = unmap(same_verts2old_verts, donor_velocities, dim);
     map_into(same_velocities, same_verts2new_verts, target_velocities, dim);
