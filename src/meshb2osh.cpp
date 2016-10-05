2137ec4a79d8e2a26000dedb3bc2c79888a5db55
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-10-04 21:40:38 -0400

    got basic elements/verts coming from libMeshb

--- src/meshb2osh.cpp
+++ src/meshb2osh.cpp
@@ -36,40 +36,71 @@ struct VersionTypes<4> {
   using Line = std::int64_t;
 };
 
+static void safe_goto(long long file, GmfKwdCod key) {
+  auto ret = GmfGotoKwd(file, key);
+  OMEGA_H_CHECK(ret);
+}
+
 template <int version>
-static void read_meshb_version(osh::Mesh* m, long long file, int ver, int dim) {
+static void read_meshb_version(osh::Library const& lib,
+    osh::Mesh* mesh, long long file, int ver, int dim) {
   using Index = typename VersionTypes<version>::Index;
   using Real = typename VersionTypes<version>::Real;
   using Line = typename VersionTypes<version>::Line;
   OMEGA_H_CHECK(ver == version);
   Index nverts = GmfStatKwd(file, GmfVertices);
-  auto coords = osh::HostWrite<osh::Real>(nverts * dim);
-  static GmfKwdCod const elem_kwds[4] = {
+  static GmfKwdCod const simplex_kwds[4] = {
     GmfVertices,
     GmfEdges,
     GmfTriangles,
     GmfTetrahedra
   };
-  auto elem_kwd = elem_kwds[dim];
-  Index nelems = GmfStatKwd(file, elem_kwd);
-  GmfGotoKwd(file, GmfVertices);
+  safe_goto(file, GmfVertices);
+  auto coords = osh::HostWrite<osh::Real>(nverts * dim);
   for (Line i = 0; i < nverts; ++i) {
     Index ref;
-    /* Omega_h doesn't instantiate its templates for `float`,
-     * so `tmp` is here to temporarily store floats from version
-     * 1 so they can be copied to doubles in Omega_h
-     */
     osh::Few<Real, 3> tmp;
     if (dim == 2) GmfGetLin(file, GmfVertices, &tmp[0], &tmp[1], &ref);
     if (dim == 3) GmfGetLin(file, GmfVertices, &tmp[0], &tmp[1], &tmp[2], &ref);
-    std::cout << "vert " << i << " ref " << ref << '\n';
+    OMEGA_H_CHECK(ref == i + 1);
     for (int j = 0; j < dim; ++j) coords[i * dim + j] = tmp[j];
   }
+  auto side_kwd = simplex_kwds[dim - 1];
+  Index nsides = GmfStatKwd(file, side_kwd);
+  auto sides2verts = osh::HostWrite<osh::LO>(nsides * dim);
+  auto sides2class_id = osh::HostWrite<osh::I32>(nsides);
+  if (nsides) {
+    safe_goto(file, side_kwd);
+    for (Line i = 0; i < nsides; ++i) {
+      Index class_id;
+      osh::Few<Index, 3> tmp;
+      if (dim == 2) GmfGetLin(file, side_kwd, &tmp[0], &tmp[1], &class_id);
+      if (dim == 3) GmfGetLin(file, side_kwd, &tmp[0], &tmp[1], &tmp[2], &class_id);
+      for (int j = 0; j < dim; ++j) sides2verts[i * dim + j] = tmp[j] - 1;
+      sides2class_id[i] = class_id;
+    }
+  }
+  auto elem_kwd = simplex_kwds[dim];
+  Index nelems = GmfStatKwd(file, elem_kwd);
+  safe_goto(file, elem_kwd);
+  auto elems2verts = osh::HostWrite<osh::LO>(nelems * (dim + 1));
+  for (Line i = 0; i < nelems; ++i) {
+    Index ref;
+    osh::Few<Index, 4> tmp;
+    if (dim == 2) GmfGetLin(file, elem_kwd, &tmp[0], &tmp[1], &tmp[2], &ref);
+    if (dim == 3) GmfGetLin(file, elem_kwd, &tmp[0], &tmp[1], &tmp[2], &tmp[3], &ref);
+    OMEGA_H_CHECK(ref == i + 1);
+    for (int j = 0; j <= dim; ++j) elems2verts[i * (dim + 1) + j] = tmp[j] - 1;
+  }
+  osh::build_from_elems2verts(mesh, lib, dim,
+      osh::LOs(elems2verts.write()), nverts);
+  mesh->add_tag(osh::VERT, "coordinates", dim, OMEGA_H_LINEAR_INTERP, OMEGA_H_DO_OUTPUT,
+      osh::Reals(coords.write()));
   GmfCloseMesh(file);
-  (void)m;
+  (void)mesh;
 }
 
-static void read_meshb(osh::Mesh* m, const char* filepath) {
+static void read_meshb(osh::Library const& lib, osh::Mesh* mesh, const char* filepath) {
   int ver, dim;
   auto file = GmfOpenMesh(filepath, GmfRead, &ver, &dim);
   if (!file) {
@@ -77,10 +108,10 @@ static void read_meshb(osh::Mesh* m, const char* filepath) {
   }
   OMEGA_H_CHECK(dim == 2 || dim == 3);
   switch (ver) {
-    case 1: read_meshb_version<1>(m, file, ver, dim); return;
-    case 2: read_meshb_version<2>(m, file, ver, dim); return;
-    case 3: read_meshb_version<3>(m, file, ver, dim); return;
-    case 4: read_meshb_version<4>(m, file, ver, dim); return;
+    case 1: read_meshb_version<1>(lib, mesh, file, ver, dim); return;
+    case 2: read_meshb_version<2>(lib, mesh, file, ver, dim); return;
+    case 3: read_meshb_version<3>(lib, mesh, file, ver, dim); return;
+    case 4: read_meshb_version<4>(lib, mesh, file, ver, dim); return;
   }
   Omega_h_fail("unknown libMeshb version %d\n", ver);
 }
@@ -91,7 +122,8 @@ int main(int argc, char** argv) {
     std::cout << "usage: " << argv[0] << " input.meshb output.osh\n";
     return -1;
   }
-  osh::Mesh m;
-  read_meshb(&m, argv[1]);
+  osh::Mesh mesh;
+  read_meshb(lib, &mesh, argv[1]);
+  osh::binary::write(argv[2], &mesh);
   return 0;
 }
