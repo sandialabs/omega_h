2e335fe7ae710d508e265d37081aee4a906b20a6
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-10-08 15:08:31 -0400

    update the conserve test

--- src/conserve_test.cpp
+++ src/conserve_test.cpp
@@ -55,7 +55,7 @@ int main(int argc, char** argv) {
   mesh.add_tag(VERT, "velocity", mesh.dim(), OMEGA_H_MOMENTUM_VELOCITY,
       OMEGA_H_DO_OUTPUT, Reals(velocity));
   auto momentum_before = get_total_momentum(&mesh);
-  adapt(&mesh, 0.30, 0.30, 2.0 / 3.0, 4.0 / 3.0, -1.0, 4, 3);
+  adapt(&mesh, 0.30, 0.30, 1.0 / sqrt(2.), sqrt(2.), 1.0, 4, 0);
   postprocess_conserve(&mesh);
   auto momentum_after = get_total_momentum(&mesh);
   std::cout << "before"
