f8b41486dbc25b682cc63fe82e5628d085236167
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-10-10 19:16:53 -0400

    use defined() for version string macro
    addresses issue #60

--- src/Omega_h_macros.h
+++ src/Omega_h_macros.h
@@ -3,50 +3,25 @@
 
 #include <Omega_h_config.h>
 
+#define OMEGA_H_STRINGIFY(s) #s
+/* apparently you need two macros to make a string */
+#define OMEGA_H_TOSTRING(s) OMEGA_H_STRINGIFY(s)
+
 /* this block of preprocessor code creates a string
    literal describing the version and compile options
    used to configure this header.
    this is used to ensure that during user application
    compiles, the header included matches the library linked to.
    it is not foolproof, but better than nothing */
-#define OMEGA_H_TOSTR2(s) #s
-#define OMEGA_H_TOSTR(s) OMEGA_H_TOSTR2(s)
-#ifdef OMEGA_H_USE_MPI
-#define OMEGA_H_MPI_STR "1"
-#else
-#define OMEGA_H_MPI_STR "0"
-#endif
-#ifdef OMEGA_H_USE_KOKKOS
-#define OMEGA_H_KOKKOS_STR "1"
-#else
-#define OMEGA_H_KOKKOS_STR "0"
-#endif
-#ifdef OMEGA_H_USE_OPENMP
-#define OMEGA_H_OPENMP_STR "1"
-#else
-#define OMEGA_H_OPENMP_STR "0"
-#endif
-#ifdef OMEGA_H_USE_CUDA
-#define OMEGA_H_CUDA_STR "1"
-#else
-#define OMEGA_H_CUDA_STR "0"
-#endif
-#ifdef OMEGA_H_USE_ZLIB
-#define OMEGA_H_ZLIB_STR "1"
-#else
-#define OMEGA_H_ZLIB_STR "0"
-#endif
-#ifdef OMEGA_H_CHECK_BOUNDS
-#define OMEGA_H_BOUNDS_STR "1"
-#else
-#define OMEGA_H_BOUNDS_STR "0"
-#endif
-#define OMEGA_H_VERSION                                 \
-  OMEGA_H_TOSTR(OMEGA_H_VERSION_MAJOR) "."              \
-  OMEGA_H_TOSTR(OMEGA_H_VERSION_MINOR) "."              \
-  OMEGA_H_TOSTR(OMEGA_H_VERSION_PATCH) "+"              \
-  OMEGA_H_MPI_STR OMEGA_H_KOKKOS_STR OMEGA_H_OPENMP_STR \
-  OMEGA_H_CUDA_STR OMEGA_H_ZLIB_STR OMEGA_H_BOUNDS_STR
+#define OMEGA_H_VERSION                                                        \
+  OMEGA_H_TOSTRING(OMEGA_H_VERSION_MAJOR)                                      \
+  "." OMEGA_H_TOSTRING(OMEGA_H_VERSION_MINOR) "." OMEGA_H_TOSTRING(            \
+      OMEGA_H_VERSION_PATCH) "+" OMEGA_H_TOSTRING(defined(OMEGA_H_USE_MPI))    \
+      OMEGA_H_TOSTRING(defined(OMEGA_H_USE_KOKKOS))                            \
+          OMEGA_H_TOSTRING(defined(OMEGA_H_USE_OPENMP))                        \
+              OMEGA_H_TOSTRING(defined(OMEGA_H_USE_CUDA))                      \
+                  OMEGA_H_TOSTRING(defined(OMEGA_H_USE_ZLIB))                  \
+                      OMEGA_H_TOSTRING(defined(OMEGA_H_CHECK_BOUNDS))
 
 #define OMEGA_H_PRAGMA(x) _Pragma(#x)
 
