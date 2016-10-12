c00c01824d9df41861129cd4d887e08c4feb5568
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-10-12 17:02:29 -0400

    modernize conservation test
    3D geometry with two "materials",
    checks for mass conservation in
    each material and total momentum
    conservation.
    uses the Gmsh generated
    "ball_in_cube" mesh.

--- src/conserve_test.cpp
+++ src/conserve_test.cpp
@@ -7,8 +7,12 @@
 #include "size.hpp"
 #include "space.hpp"
 #include "timer.hpp"
+#include "map.hpp"
 
 using namespace Omega_h;
+
+constexpr Int nobjs = 2;
+constexpr Int obj_ids[nobjs] = {34,72};
 
 static void postprocess_conserve(Mesh* mesh) {
   auto volume = measure_elements_real(mesh);
@@ -19,13 +23,22 @@ static void postprocess_conserve(Mesh* mesh) {
       OMEGA_H_DO_OUTPUT, density);
 }
 
-static Vector<2> get_total_momentum(Mesh* mesh) {
+static Real get_total_mass(Mesh* mesh, Int obj) {
+  auto masses = mesh->get_array<Real>(mesh->dim(), "mass");
+  auto class_ids = mesh->get_array<I32>(mesh->dim(), "class_id");
+  auto elem_in_obj = each_eq_to(class_ids, obj_ids[obj]);
+  auto obj_elems = collect_marked(elem_in_obj);
+  auto obj_masses = unmap(obj_elems, masses, 1);
+  return repro_sum(mesh->comm(), obj_masses);
+}
+
+static Vector<3> get_total_momentum(Mesh* mesh) {
   auto vert_velocities = mesh->get_array<Real>(VERT, "velocity");
   auto elem_velocities = average_field(mesh, mesh->dim(),
       LOs(mesh->nelems(), 0, 1), mesh->dim(), vert_velocities);
   auto masses = mesh->get_array<Real>(mesh->dim(), "mass");
   auto momenta = multiply_each(elem_velocities, masses);
-  Vector<2> total;
+  Vector<3> total;
   repro_sum(mesh->comm(), momenta, mesh->dim(), &total[0]);
   return total;
 }
@@ -35,33 +48,40 @@ int main(int argc, char** argv) {
   auto world = lib.world();
   CHECK(world->size() == 1);
   Mesh mesh(&lib);
-  auto nx = 10;
-  build_box(&mesh, 1, 1, 0, nx, nx, 0);
-  classify_by_angles(&mesh, PI / 4);
-  mesh.reorder();
-  mesh.reset_globals();
+  gmsh::read("ball_in_cube.msh", &mesh);
   auto size = find_implied_size(&mesh);
-  size = multiply_each_by(1.3, size);
+  size = multiply_each_by(1.1, size);
   mesh.add_tag(VERT, "size", 1, OMEGA_H_SIZE, OMEGA_H_DO_OUTPUT, size);
   mesh.add_tag(mesh.dim(), "mass", 1, OMEGA_H_CONSERVE, OMEGA_H_DO_OUTPUT,
       measure_elements_real(&mesh));
   auto velocity = Write<Real>(mesh.nverts() * mesh.dim());
   auto coords = mesh.coords();
   auto f = LAMBDA(LO vert) {
-    auto x = get_vector<2>(coords, vert);
-    set_vector(velocity, vert, vector_2(1, 0) * sqrt(fabs(x[0])));
+    auto x = get_vector<3>(coords, vert);
+    set_vector(velocity, vert, vector_3(1, 0, 0) * sqrt(fabs(x[0])));
   };
   parallel_for(mesh.nverts(), f);
   mesh.add_tag(VERT, "velocity", mesh.dim(), OMEGA_H_MOMENTUM_VELOCITY,
       OMEGA_H_DO_OUTPUT, Reals(velocity));
   auto momentum_before = get_total_momentum(&mesh);
+  Real masses_before[nobjs];
+  for (Int obj = 0; obj < nobjs; ++obj) {
+    masses_before[obj] = get_total_mass(&mesh, obj);
+  }
   adapt(&mesh, AdaptOpts(&mesh));
   postprocess_conserve(&mesh);
+  for (Int obj = 0; obj < nobjs; ++obj) {
+    auto mass_after = get_total_mass(&mesh, obj);
+    std::cout << "model region " << obj_ids[obj]
+      << " mass before " << masses_before[obj] << ", after " << mass_after << '\n';
+    CHECK(are_close(mass_after, masses_before[obj]));
+  }
   auto momentum_after = get_total_momentum(&mesh);
-  std::cout << "before" << ' ' << momentum_before[0] << ' '
-            << momentum_before[1] << '\n';
-  std::cout << "after" << ' ' << momentum_after[0] << ' ' << momentum_after[1]
-            << '\n';
+  std::cout << "momentum before" << ' ' << momentum_before[0] << ' '
+            << momentum_before[1] << ' '
+            << momentum_before[2] << '\n';
+  std::cout << "momentum after" << ' ' << momentum_after[0] << ' ' << momentum_after[1]
+            << ' ' << momentum_after[2] << '\n';
   CHECK(are_close(momentum_before, momentum_after));
   bool ok = check_regression("gold_conserve", &mesh, 0.0, 0.0);
   if (!ok) return 2;
