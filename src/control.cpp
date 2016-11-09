94a3ff66335e75c3107540247f8619c97495741b
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-11-09 14:13:11 -0500

    add flag to control memory logging
    this is the first command-line
    flag we've started using...

--- src/control.cpp
+++ src/control.cpp
@@ -1,19 +1,33 @@
-#include "comm.hpp"
-#include "internal.hpp"
-#include "protect.hpp"
+#include "control.hpp"
 
 #include <cstdarg>
 #include <sstream>
 
-namespace Omega_h {
+#include "comm.hpp"
+#include "internal.hpp"
+#include "protect.hpp"
 
+namespace Omega_h {
+bool should_log_memory = false;
+}
+
+namespace {
 #ifdef OMEGA_H_USE_MPI
 static bool we_called_mpi_init = false;
 #endif
 #ifdef OMEGA_H_USE_KOKKOS
 static bool we_called_kokkos_init = false;
 #endif
-
+static bool remove_flag(int* argc, char*** argv, std::string const& flag) {
+  for (int i = 0; i < *argc; ++i) {
+    if (flag == (*argv)[i]) {
+      --(*argc);
+      for (int j = i; j < *argc; ++j) (*argv)[j] = (*argv)[j + 1];
+      return true;
+    }
+  }
+  return false;
+}
 }
 
 extern "C" void Omega_h_init_internal(
@@ -27,6 +41,7 @@ extern "C" void Omega_h_init_internal(
     std::string msg_str = msg.str();
     Omega_h_fail("%s\n", msg_str.c_str());
   }
+  Omega_h::should_log_memory = remove_flag(argc, argv, "--osh-log-mem");
 #ifdef OMEGA_H_USE_MPI
   int mpi_is_init;
   CHECK(MPI_SUCCESS == MPI_Initialized(&mpi_is_init));
@@ -57,19 +72,21 @@ extern "C" void Omega_h_finalize(void) {
     Omega_h::we_called_kokkos_init = false;
   }
 #endif
-  std::size_t mem_used = Omega_h::get_max_bytes();
-  std::size_t max_mem_used = mem_used;
+  if (Omega_h::should_log_memory) {
+    std::size_t mem_used = Omega_h::get_max_bytes();
+    std::size_t max_mem_used = mem_used;
 #ifdef OMEGA_H_USE_MPI
-  MPI_Reduce(&mem_used, &max_mem_used, 1,
-      Omega_h::MpiTraits<std::size_t>::datatype(), MPI_MAX, 0, MPI_COMM_WORLD);
-  int rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  if (rank == 0) {
+    MPI_Reduce(&mem_used, &max_mem_used, 1,
+        Omega_h::MpiTraits<std::size_t>::datatype(), MPI_MAX, 0, MPI_COMM_WORLD);
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (rank == 0) {
 #endif
-    printf("maximum Omega_h memory usage: %zu bytes\n", max_mem_used);
+      printf("maximum Omega_h memory usage: %zu bytes\n", max_mem_used);
 #ifdef OMEGA_H_USE_MPI
+    }
+#endif
   }
-#endif
 #ifdef OMEGA_H_USE_MPI
   if (Omega_h::we_called_mpi_init) {
     CHECK(MPI_SUCCESS == MPI_Finalize());
---
+++ src/control.hpp
@@ -0,0 +1,8 @@
+#ifndef CONTROL_HPP
+#define CONTROL_HPP
+
+namespace Omega_h {
+extern bool should_log_memory;
+}
+
+#endif
