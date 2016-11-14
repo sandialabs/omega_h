fb3c048b08c61f71ebe7322caa6a01038043e049
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-11-13 20:48:01 -0500

    test face ordering and closest point queries

--- src/egads_test.cpp
+++ src/egads_test.cpp
@@ -12,6 +12,7 @@
 
 #include <cassert>
 #include <array>
+#include <cmath>
 
 #define CALL(f) assert(EGADS_SUCCESS == (f))
 
@@ -41,6 +42,30 @@ static std::array<double, 3> get_point(ego vert) {
   return data;
 }
 
+static std::array<double, 3> get_closest_point(ego face, std::array<double, 3> in) {
+  double ignored[2];
+  std::array<double, 3> out = {{0.0, 0.0, 0.0}};
+  auto ret = EG_invEvaluateGuess(face, in.data(), ignored, out.data());
+  if (ret != EGADS_SUCCESS) {
+    printf("EG_invEvaluateGuess failed with code %d\n", ret);
+    ret = EG_invEvaluate(face, in.data(), ignored, out.data());
+    if (ret != EGADS_SUCCESS) {
+      printf("EG_invEvaluate also failed, with code %d\n", ret);
+    }
+  }
+  return out;
+}
+
+static void print_closest_point(ego* faces, int i, std::array<double, 3> pt) {
+  auto face = faces[i - 1];
+  auto closest = get_closest_point(face, pt);
+  printf("closest point to %f %f %f on face %d is %f %f %f\n",
+      pt[0], pt[1], pt[2], i,
+      closest[0],
+      closest[1],
+      closest[2]);
+}
+
 int main(int argc, char** argv) {
   assert(argc == 2);
   ego context;
@@ -62,7 +87,6 @@ int main(int argc, char** argv) {
   ego* faces;
   CALL(EG_getBodyTopos(body, nullptr, FACE, &nfaces, &faces));
   printf("first body has %d faces\n", nfaces);
-  EG_free(faces);
   int nedges;
   ego* edges;
   CALL(EG_getBodyTopos(body, nullptr, EDGE, &nedges, &edges));
@@ -74,9 +98,17 @@ int main(int argc, char** argv) {
   printf("first body has %d verts\n", nverts);
   for (int i = 0; i < nverts; ++i) {
     auto pt = get_point(verts[i]);
-    printf("point %d at %f %f %f\n", i, pt[0], pt[1], pt[2]);
+    printf("point %d at %f %f %f\n", i + 1, pt[0], pt[1], pt[2]);
   }
   EG_free(verts);
+  print_closest_point(faces, 1, {{ 0.0, 0.75, 0.5 }});
+  print_closest_point(faces, 2, {{ 0.5, 0.5, 1.0 }});
+  print_closest_point(faces, 3, {{ 0.5, 1.0, 0.5 }});
+  print_closest_point(faces, 4, {{ 0.5, 0.5, 0.0 }});
+  print_closest_point(faces, 5, {{ 0.5 / sqrt(2), 0.5 / sqrt(2), 0.5 }});
+  print_closest_point(faces, 6, {{ 1.0, 0.5, 0.5 }});
+  print_closest_point(faces, 7, {{ 0.75, 0.0, 0.5 }});
+  EG_free(faces);
   CALL(EG_deleteObject(model));
   CALL(EG_close(context));
 }
