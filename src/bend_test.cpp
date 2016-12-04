f8430ce7edc702b9d67d7d8b6f78471d8487e759
Dan Ibanez <daibane@sandia.gov>
2016-12-04 16:26:41 -0700

    more work on the bend test

--- src/bend_test.cpp
+++ src/bend_test.cpp
@@ -25,25 +25,37 @@ int main(int argc, char** argv) {
   }
   mesh.set_comm(world);
   mesh.balance();
-  mesh.set_parting(OMEGA_H_GHOSTED);
   auto orig_coords = mesh.coords();
-  auto bend_radius = orig_height / PI;
-  auto full_angle = orig_height / bend_radius;
-  auto new_coords_w = Write<Real>(mesh.nverts() * 3);
-  auto f = LAMBDA(LO v) {
-    auto p = get_vector<3>(orig_coords, v);
-    auto angle = full_angle * (p[2] / orig_height);
-    auto radius = bend_radius - p[1];
-    auto y = bend_radius - radius * cos(angle);
-    auto z = radius * sin(angle);
-    auto p2 = vector_3(p[0], y, z);
-    set_vector(new_coords_w, v, p2);
-  };
-  parallel_for(mesh.nverts(), f);
-  mesh.set_coords(Reals(new_coords_w));
-  auto max_size = 1.0 / Real(orig_resolution);
-  auto segment_angle = PI / 32.0;
-  auto isos = get_curvature_isos(&mesh, segment_angle, max_size);
-  mesh.add_tag(VERT, "size", 1, OMEGA_H_SIZE, OMEGA_H_DO_OUTPUT, isos);
-  vtk::write_vtu("debug.vtu", &mesh, mesh.dim());
+  mesh.add_tag(VERT, "orig_coords", mesh.dim(), OMEGA_H_LINEAR_INTERP,
+      OMEGA_H_DO_OUTPUT, orig_coords);
+  mesh.add_tag<Real>(VERT, "size", 1, OMEGA_H_SIZE, OMEGA_H_DO_OUTPUT);
+  vtk::Writer writer(&mesh, "bend", mesh.dim());
+  auto first_bend_radius = 5.0;
+  auto final_bend_radius = orig_height / PI;
+  auto nsteps = 20;
+  for (Int i = 0; i <= nsteps; ++i) {
+    auto t = Real(i) / Real(nsteps);
+    auto bend_radius = first_bend_radius * (1.0 - t) + final_bend_radius * t;
+    auto full_angle = orig_height / bend_radius;
+    mesh.set_parting(OMEGA_H_GHOSTED);
+    auto orig_coords = mesh.get_array<Real>(VERT, "orig_coords");
+    auto new_coords_w = Write<Real>(mesh.nverts() * 3);
+    auto f = LAMBDA(LO v) {
+      auto p = get_vector<3>(orig_coords, v);
+      auto angle = full_angle * (p[2] / orig_height);
+      auto radius = bend_radius - p[1];
+      auto y = bend_radius - radius * cos(angle);
+      auto z = radius * sin(angle);
+      auto p2 = vector_3(p[0], y, z);
+      set_vector(new_coords_w, v, p2);
+    };
+    parallel_for(mesh.nverts(), f);
+    mesh.set_coords(Reals(new_coords_w));
+    auto max_size = 1.0 / Real(orig_resolution);
+    auto segment_angle = PI / 32.0;
+    auto isos = get_curvature_isos(&mesh, segment_angle, max_size);
+    isos = limit_size_field_gradation(&mesh, isos, 1.05);
+    mesh.set_tag(VERT, "size", isos);
+    writer.write();
+  }
 }
