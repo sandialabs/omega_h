adc358daff0d9d130f2be41991e566c63e973758
Dan Ibanez <daibane@sandia.gov>
2016-12-13 09:51:16 -0700

    draft the actual metrics for UGAWG cube-cylinder

--- src/ugawg_cylinder.cpp
+++ src/ugawg_cylinder.cpp
@@ -3,6 +3,7 @@
 #include "Omega_h_egads.hpp"
 #include "loop.hpp"
 #include "timer.hpp"
+#include "metric.hpp"
 
 #include <iostream>
 
@@ -10,16 +11,52 @@ using namespace Omega_h;
 
 constexpr Int dim = 3;
 
-void run_case(Mesh* mesh, Egads* eg, char const* vtk_path) {
+static Reals get_first_metric(Mesh* mesh) {
+  auto coords = mesh->coords();
+  auto out = Write<Real>(mesh->nverts() * symm_dofs(dim));
+  constexpr Real h0 = 0.001;
+  auto f = LAMBDA(LO v) {
+    auto p = get_vector<dim>(coords, v);
+    auto z = p[2];
+    auto h = vector_3(0.1, 0.1, h0 + 2 * (0.1 - h0) * fabs(z - 0.5));
+    auto m = diagonal(metric_eigenvalues(h));
+    set_symm(out, v, m);
+  };
+  parallel_for(mesh->nverts(), f);
+  return out;
+}
+
+static Reals get_second_metric(Mesh* mesh) {
+  auto coords = mesh->coords();
+  auto out = Write<Real>(mesh->nverts() * symm_dofs(dim));
+  constexpr Real h0 = 0.001;
+  auto f = LAMBDA(LO v) {
+    auto p = get_vector<dim>(coords, v);
+    auto x = p[0];
+    auto y = p[1];
+    auto xy = vector_2(x, y);
+    auto radius = norm(xy);
+    auto t = atan2(y, x);
+    auto h = vector_3(0.1, 0.1, h0 + 2 * (0.1 - h0) * fabs(radius - 0.5));
+    auto rotation = rotate(t, vector_3(0,0,1));
+    auto m = compose_metric(rotation, h);
+    set_symm(out, v, m);
+  };
+  parallel_for(mesh->nverts(), f);
+  return out;
+}
+
+static void run_case(Mesh* mesh, Egads* eg, int which_metric, char const* vtk_path) {
   auto world = mesh->comm();
   mesh->set_parting(OMEGA_H_GHOSTED);
-  auto implied_size = find_implied_size(mesh);
-  mesh->add_tag(VERT, "size", 1, OMEGA_H_SIZE,
-      OMEGA_H_DO_OUTPUT, implied_size);
-  auto target_size = multiply_each_by(0.5, implied_size);
+  auto implied_metrics = find_implied_metric(mesh);
+  mesh->add_tag(VERT, "metric", symm_dofs(dim), OMEGA_H_METRIC,
+      OMEGA_H_DO_OUTPUT, implied_metrics);
+  auto target_metrics = (which_metric == 1) ? get_first_metric(mesh) :
+                                              get_second_metric(mesh);
   mesh->add_tag<Real>(
-      VERT, "target_size", 1, OMEGA_H_SIZE, OMEGA_H_DO_OUTPUT,
-      target_size);
+      VERT, "target_metric", symm_dofs(dim), OMEGA_H_METRIC, OMEGA_H_DO_OUTPUT,
+      target_metrics);
   mesh->set_parting(OMEGA_H_ELEM_BASED);
   mesh->ask_lengths();
   mesh->ask_qualities();
@@ -48,6 +85,7 @@ int main(int argc, char** argv) {
   char const* egads_path = nullptr;
   char const* out_path = nullptr;
   char const* vtk_path = nullptr;
+  int which_metric = 1;
   bool should_help = false;
   for (int i = 1; i < argc; ++i) {
     if (std::string("-a") == argv[i]) {
@@ -56,6 +94,17 @@ int main(int argc, char** argv) {
         should_help = true;
       } else {
         vtk_path = argv[++i];
+      }
+    } else if (std::string("-m") == argv[i]) {
+      if (i == argc - 1) {
+        std::cout << "-m takes an argument\n";
+        should_help = true;
+      } else {
+        which_metric = atoi(argv[++i]);
+        if (!(which_metric == 1 || which_metric == 2)) {
+          std::cout << "-m takes only values 1 or 2\n";
+          should_help = true;
+        }
       }
     } else if (ends_with(argv[i], ".egads")) {
       egads_path = argv[i];
@@ -80,7 +129,7 @@ int main(int argc, char** argv) {
   meshb::read(&mesh, in_path);
   auto eg = egads_load(egads_path);
   egads_reclassify(&mesh, eg);
-  run_case(&mesh, eg, vtk_path);
+  run_case(&mesh, eg, which_metric, vtk_path);
   egads_free(eg);
   meshb::write(&mesh, out_path, 2);
   return 0;
