d77b0018f645ad7c986ef30e348b2347015592a2
Dan Ibanez <daibane@sandia.gov>
2016-12-16 20:34:54 -0700

    curvature refinement test looks good !

--- src/bend_test.cpp
+++ src/bend_test.cpp
@@ -1,8 +1,13 @@
 #include "Omega_h.hpp"
 #include "access.hpp"
 #include "loop.hpp"
+#include "timer.hpp"
+
+#include <iostream>
 
 using namespace Omega_h;
+
+constexpr Int dim = 3;
 
 int main(int argc, char** argv) {
   auto lib = Library(&argc, &argv);
@@ -11,6 +16,8 @@ int main(int argc, char** argv) {
   auto orig_height = 4;
   auto orig_width = 1;
   auto orig_resolution = 3;
+  auto max_size = 1.0 / Real(orig_resolution);
+  auto segment_angle = PI / 32.0;
   if (world->rank() == 0) {
     build_box(&mesh, orig_width, orig_width, orig_height,
         orig_width * orig_resolution, orig_width * orig_resolution,
@@ -28,29 +35,43 @@ int main(int argc, char** argv) {
   auto first_bend_radius = 5.0;
   auto final_bend_radius = orig_height / PI;
   auto nsteps = 20;
+  AdaptOpts opts(&mesh);
+  auto t0 = now();
   for (Int i = 0; i <= nsteps; ++i) {
+    std::cout << "OUTER STEP " << i << '\n';
     auto t = Real(i) / Real(nsteps);
     auto bend_radius = first_bend_radius * (1.0 - t) + final_bend_radius * t;
     auto full_angle = orig_height / bend_radius;
     mesh.set_parting(OMEGA_H_GHOSTED);
     auto orig_coords = mesh.get_array<Real>(VERT, "orig_coords");
-    auto new_coords_w = Write<Real>(mesh.nverts() * 3);
+    auto coords = mesh.coords();
+    auto warp_w = Write<Real>(mesh.nverts() * dim);
     auto f = LAMBDA(LO v) {
-      auto p = get_vector<3>(orig_coords, v);
-      auto angle = full_angle * (p[2] / orig_height);
-      auto radius = bend_radius - p[1];
+      auto op = get_vector<dim>(orig_coords, v);
+      auto angle = full_angle * (op[2] / orig_height);
+      auto radius = bend_radius - op[1];
       auto y = bend_radius - radius * cos(angle);
       auto z = radius * sin(angle);
-      auto p2 = vector_3(p[0], y, z);
-      set_vector(new_coords_w, v, p2);
+      auto p2 = vector_3(op[0], y, z);
+      auto p = get_vector<dim>(coords, v);
+      auto wv = p2 - p;
+      set_vector(warp_w, v, wv);
     };
     parallel_for(mesh.nverts(), f);
-    mesh.set_coords(Reals(new_coords_w));
-    auto max_size = 1.0 / Real(orig_resolution);
-    auto segment_angle = PI / 32.0;
-    auto isos = get_curvature_isos(&mesh, segment_angle, max_size);
-    isos = limit_size_field_gradation(&mesh, isos, 1.05);
-    mesh.set_tag(VERT, "size", isos);
-    writer.write();
+    mesh.add_tag(VERT, "warp", mesh.dim(), OMEGA_H_LINEAR_INTERP,
+        OMEGA_H_DO_OUTPUT, Reals(warp_w));
+    do {
+      std::cout << "WARP STEP\n";
+      auto isos = get_curvature_isos(&mesh, segment_angle, max_size);
+      isos = limit_size_field_gradation(&mesh, isos, 1.015);
+      mesh.set_tag(VERT, "size", isos);
+      adapt(&mesh, opts);
+      writer.write();
+    } while (warp_to_limit(&mesh, opts));
   }
+  auto t1 = now();
+  std::cout << "total time " << t1 - t0 << '\n';
+  //paraview workaround
+  writer.write();
+  writer.write();
 }
