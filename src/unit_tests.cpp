ab5d1dde5854f3cf76decf68f4e7b75f956392e9
Dan Ibanez <daibane@sandia.gov>
2017-01-16 13:18:19 -0700

    tet test, two bridges.

--- src/unit_tests.cpp
+++ src/unit_tests.cpp
@@ -907,6 +907,14 @@ static void test_proximity(Library* lib) {
   auto isos = get_pad_isos(&mesh, 2, 42.0, Read<I8>({1,1,0}));
   CHECK(are_close(isos, Reals({1.0,42.0,42.0})));
   }
+  { // tet with two bridges
+  Mesh mesh(lib);
+  build_from_elems2verts(&mesh, 3, LOs({0,1,2,3}), 4);
+  mesh.add_tag(VERT, "coordinates", 3, OMEGA_H_LINEAR_INTERP,
+      OMEGA_H_DO_OUTPUT, Reals(3*4, 0.0));
+  auto isos = get_pad_isos(&mesh, 3, 42.0, Read<I8>({1,1,0,0,0,0}));
+  CHECK(are_close(isos, Reals(4, 42.0)));
+  }
 }
 
 int main(int argc, char** argv) {
