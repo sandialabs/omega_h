2ef8973c9c7d847aada844230e9999108f943914
Dan Ibanez <daibane@sandia.gov>
2017-03-08 11:21:46 -0700

    Omega_h_library.hpp

--- src/Omega_h.hpp
+++ src/Omega_h.hpp
@@ -15,6 +15,7 @@
 #include <Omega_h_comm.hpp>
 #include <Omega_h_dist.hpp>
 #include <Omega_h_adj.hpp>
+#include <Omega_h_library.hpp>
 
 namespace Omega_h {
 
@@ -25,49 +26,6 @@ enum {
   EDGE = OMEGA_H_EDGE,
   TRI = OMEGA_H_TRI,
   TET = OMEGA_H_TET
-};
-
-class Library {
- public:
-  Library(Library const&);
-  inline Library(int* argc, char*** argv
-#ifdef OMEGA_H_USE_MPI
-      ,
-      MPI_Comm comm_mpi = MPI_COMM_WORLD
-#endif
-      ) {
-    initialize(OMEGA_H_VERSION, argc, argv
-#ifdef OMEGA_H_USE_MPI
-        ,
-        comm_mpi
-#endif
-        );
-  }
-  ~Library();
-  CommPtr world();
-  CommPtr self();
-  void add_to_timer(std::string const& name, double nsecs);
-  LO self_send_threshold() const;
-  bool should_time_;
-  LO self_send_threshold_;
-  bool silent_;
-
- private:
-  void initialize(char const* head_desc, int* argc, char*** argv
-#ifdef OMEGA_H_USE_MPI
-      ,
-      MPI_Comm comm_mpi
-#endif
-      );
-  CommPtr world_;
-  CommPtr self_;
-#ifdef OMEGA_H_USE_MPI
-  bool we_called_mpi_init;
-#endif
-#ifdef OMEGA_H_USE_KOKKOS
-  bool we_called_kokkos_init;
-#endif
-  std::map<std::string, double> timers;
 };
 
 namespace inertia {
