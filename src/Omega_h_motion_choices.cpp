3cd4136af5daccc51e373cc50330ce20549fbbfa
Dan Ibanez <daibane@sandia.gov>
2017-08-03 12:46:42 -0600

    first draft of volume-correcting motion kernel

--- src/Omega_h_motion_choices.cpp
+++ src/Omega_h_motion_choices.cpp
@@ -7,98 +7,13 @@
 namespace Omega_h {
 
 template <Int mesh_dim, Int metric_dim>
-struct MotionChooser {
- private:
-  static constexpr Int metric_ncomps = symm_ncomps(metric_dim);
- private:
-  LinearPack pack;
-  Graph v2k;
-  LOs kv2v;
-  Reals coords;
-  Reals metrics;
-  /* this array contains the element size (volume) errors as they are
-     at the beginning of each step */
-  Write<Real> size_errors_w;
-  /* this array contains the vertex field DOFs as they are
-     at the beginning of each step */
-  Write<Real> new_sol_w;
-  /* this array contains the vertex field DOFs as they are
-     at each candidate point to be evaluated during a step */
-  Write<Real> tmp_sol_w;
- private:
-  OMEGA_H_INLINE Real compute_objective(LO v) const {
-    Real obj = 0.0;
-    auto nx = get_interpolated_position(v);
-    for (auto vk = v2k.a2ab[v]; vk < v2k.a2ab[v + 1]; ++vk) {
-      auto k = v2k.ab2b[vk];
-      auto k_obj = size_errors_w[k];
-      obj += square(k_obj);
-    }  // end loop over elements for quality
-    return obj;
-  }
-  OMEGA_H_INLINE Vector<mesh_dim> compute_objective_gradient(LO v) const {
-    auto obj_grad = zero_vector<mesh_dim>();
-    auto nx = get_tmp_position(v);
-    for (auto vk = v2k.a2ab[v]; vk < v2k.a2ab[v + 1]; ++vk) {
-      auto k = v2k.ab2b[vk];
-      auto vk_code = v2k.codes[vk];
-      auto kvv_c = code_which_down(vk_code);
-      auto kvv2v = gather_verts<mesh_dim + 1>(kv2v, k);
-      auto kvv2nx = gather_vectors<mesh_dim + 1, mesh_dim>(coords, kvv2v);
-      kvv2nx[kvv_c] = nx;
-      auto k_size_grad = get_size_gradient(kvv2nx, kvv_c);
-      auto k_obj = size_errors_w[k];
-      auto k_obj_grad = 2.0 * k_obj * k_size_grad;
-      obj_grad += k_obj_grad;
-    }
-    return obj_grad;
-  }
-  OMEGA_H_INLINE Real compute_quality(LO v) const {
-    Real new_qual = 1.0;
-    auto nx = get_tmp_position(v);
-    auto nm = delinearize_metric(get_tmp_linear_metric(v));
-    for (auto vk = v2k.a2ab[v]; vk < v2k.a2ab[v + 1]; ++vk) {
-      auto k = v2k.ab2b[vk];
-      auto vk_code = v2k.codes[vk];
-      auto kvv_c = code_which_down(vk_code);
-      auto kvv2v = gather_verts<mesh_dim + 1>(kv2v, k);
-      auto kvv2nx = gather_vectors<mesh_dim + 1, mesh_dim>(coords, kvv2v);
-      kvv2nx[kvv_c] = nx;
-      auto kvv2m = gather_symms<mesh_dim + 1, metric_dim>(metrics, kvv2v);
-      kvv2m[kvv_c] = nm;
-      auto km = maxdet_metric(kvv2m);
-      auto k_qual = metric_element_quality(kvv2nx, km);
-      new_qual = min2(new_qual, k_qual);
-    }  // end loop over elements for quality
-    return new_qual;
-  }
-  OMEGA_H_INLINE Vector<mesh_dim> get_tmp_position(LO v) {
-    Vector<mesh_dim> nx;
-    for (Int i = 0; i < mesh_dim; ++i) {
-      nx[i] = tmp_sol_w[v * pack.ncomps + pack.coords_offset + i];
-    }
-    return nx;
-  }
-  OMEGA_H_INLINE Matrix<metric_dim, metric_dim> get_tmp_linear_metric(LO v) {
-    Vector<metric_ncomps> metric_comps;
-    for (Int i = 0; i < metric_ncomps; ++i) {
-      metric_comps[i] = tmp_sol_w[v * pack.ncomps + pack.metric_offset + i];
-    }
-    return vector2symm(metric_comps);
-  }
-};
-
-template <Int mesh_dim, Int metric_dim>
 MotionChoices motion_choices_tmpl(
     Mesh* mesh, AdaptOpts const& opts, LOs cands2verts) {
-  using Metric = Matrix<metric_dim, metric_dim>;
-  auto pack = pack_linearized_fields(mesh, opts.xfer_opts);
-  /* this array contains the vertex field DOFs as they are
-     at the beginning of each step */
-  auto new_sol_w = deep_copy(pack.data);
-  /* this array contains the vertex field DOFs as they are
-     at each candidate point to be evaluated during a step */
-  auto tmp_sol_w = deep_copy(pack.data);
+  auto nsteps = opts.motion_step_count;
+  OMEGA_H_CHECK(nsteps > 0);
+  auto step_size = opts.motion_step_size;
+  OMEGA_H_CHECK(0.0 < step_size);
+  OMEGA_H_CHECK(step_size < 1.0);
   auto coords = mesh->coords();
   auto metrics = mesh->get_array<Real>(VERT, "metric");
   auto size_errors = mesh->get_array<Real>(mesh_dim, "size_error");
@@ -113,106 +28,73 @@ MotionChoices motion_choices_tmpl(
   auto elems2old_qual = mesh->ask_qualities();
   auto cands2old_qual =
       graph_reduce(cands2elems, elems2old_qual, 1, OMEGA_H_MIN);
-  auto max_steps = opts.max_motion_steps;
-  OMEGA_H_CHECK(max_steps >= 0);
-  auto max_backtracks = opts.max_motion_backtracks;
-  auto max_length = opts.max_length_allowed;
-  OMEGA_H_CHECK(0.0 < step_size);
-  OMEGA_H_CHECK(step_size < 1.0);
-  auto did_move_w = Write<I8>(ncands);
-  auto coordinates_w = Write<Real>(ncands * mesh_dim);
-  constexpr auto metric_ncomps = symm_ncomps(metric_dim);
-  auto metrics_w = Write<Real>(ncands * metric_ncomps);
-  auto qualities_w = Write<Real>(ncands);
+  auto new_coords_w = Write<Real>(ncands * mesh_dim);
+  auto new_quals_w = Write<Real>(ncands);
   auto f = OMEGA_H_LAMBDA(LO cand) {
     auto v = cands2verts[cand];
-    auto v_dim = verts2dim[v];
-    auto old_qual = cands2old_qual[cand];
-    auto last_qual = old_qual;
-    // loop over number of steps */
-    for (Int step = 0; step < max_steps; ++step) {
-      bool found_step = false;
-      // loop over edges for possible steps
-      for (auto ve = v2e.a2ab[v]; ve < v2e.a2ab[v + 1]; ++ve) {
-        auto e = v2e.ab2b[ve];
-        auto e_dim = edges2dim[e];
-        if (e_dim != v_dim) continue;
-        auto ve_code = v2e.codes[ve];
-        auto evv_c = code_which_down(ve_code);
-        auto evv_o = 1 - evv_c;
-        auto evv2v = gather_verts<2>(ev2v, e);
-        auto ov = evv2v[evv_o];
-        for (Int i = 0; i < pack.ncomps; ++i) {
-          tmp_sol_w[v * pack.ncomps + i] =
-            (1.0 - step_size) * new_sol_w[v * pack.ncomps + i] +
-                    step_size * pack.data[ov * pack.ncomps + i];
+    Real old_obj = -1.0;
+    auto old_x = get_vector<mesh_dim>(coords, v);
+    auto new_x = old_x;
+    Real new_qual = -1.0;
+    while (true) {
+      new_qual = 1.0;
+      Real new_obj = 0.0;
+      auto obj_grad = zero_vector<mesh_dim>();
+      for (auto vk = v2k.a2ab[v]; vk < v2k.a2ab[v + 1]; ++vk) {
+        auto k = v2k.ab2b[vk];
+        auto vk_code = v2k.codes[vk];
+        auto kvv_c = code_which_down(vk_code);
+        auto kvv2v = gather_verts<mesh_dim + 1>(kv2v, k);
+        auto kvv2nx = gather_vectors<mesh_dim + 1, mesh_dim>(coords, kvv2v);
+        kvv2nx[kvv_c] = new_x;
+        auto k_basis = simplex_basis(kvv2nx);
+        auto k_tmp_size = element_size(k_basis);
+        auto k_size_grad = get_size_gradient(kvv2nx, kvv_c);
+        auto k_size_diff = k_tmp_size - orig_sizes[k];
+        auto k_tmp_error = size_errors[k] + k_size_diff;
+        auto k_obj = square(k_tmp_error);
+        auto k_obj_grad = 2.0 * k_obj * k_size_grad;
+        auto kvv2m = gather_symms<mesh_dim + 1, metric_dim>(metrics, kvv2v);
+        auto km = maxdet_metric(kvv2m);
+        auto k_qual = metric_element_quality(kvv2nx, km);
+        new_obj += k_obj;
+        obj_grad += k_obj_grad;
+        new_qual = min2(new_qual, k_qual);
+      }
+      if (step && (new_obj >= old_obj || new_qual < min_qual_allowed)) {
+        /* we either took a step or backtracked, and the new position
+           is bad either because it increases the objective or the elements
+           are too distorted */
+        if (step == nsteps) {
+          /* out of time, fully back up to the last good position */
+          new_x = old_x;
+          new_qual = old_qual;
+          break;
+        } else {
+          /* still have time, backtrack */
+          new_x = (new_x + old_x) / 2.0;
         }
-        Vector<mesh_dim> nx;
-        for (Int i = 0; i < mesh_dim; ++i) {
-          nx[i] = tmp_sol_w[v * pack.ncomps + pack.coords_offset + i];
+      } else {
+        /* either no prior step exists, or the prior step both reduced
+           the objective (good) and created acceptable quality elements */
+        if (step == nsteps) {
+          /* out of time, but the last step was good. don't move. */
+          break;
+        } else {
+          /* actually take a new step */
+          old_x = new_x;
+          old_obj = new_obj;
+          new_x = (old_x - (obj_grad * step_size));
         }
-        Vector<metric_ncomps> metric_comps;
-        for (Int i = 0; i < metric_ncomps; ++i) {
-          metric_comps[i] = tmp_sol_w[v * pack.ncomps + pack.metric_offset + i];
-        }
-        auto lnm = vector2symm(metric_comps);
-        auto nm = delinearize_metric(lnm);
-        auto overshoots = false;
-        for (auto ve_l = v2e.a2ab[v]; ve_l < v2e.a2ab[v + 1]; ++ve_l) {
-          auto e_l = v2e.ab2b[ve_l];
-          auto ve_code_l = v2e.codes[ve_l];
-          auto evv_c_l = code_which_down(ve_code_l);
-          auto evv_o_l = 1 - evv_c_l;
-          auto ov_l = ev2v[e_l * 2 + evv_o_l];
-          auto om = get_symm<metric_dim>(metrics, ov_l);
-          auto ox = get_vector<mesh_dim>(coords, ov_l);
-          Few<Vector<mesh_dim>, 2> evv2nx;
-          Few<Metric, 2> evv2nm;
-          evv2nx[evv_c_l] = nx;
-          evv2nx[evv_o_l] = ox;
-          evv2nm[evv_c_l] = nm;
-          evv2nm[evv_o_l] = om;
-          auto nl = metric_edge_length(evv2nx, evv2nm);
-          if (nl > max_length) {
-            overshoots = true;
-            break;
-          }
-        }  // end loop over edges again for overshooting
-        if (overshoots) continue;
-        Real new_qual = 1.0;
-        // loop over elements to check new qualities
-        for (auto vk = v2k.a2ab[v]; vk < v2k.a2ab[v + 1]; ++vk) {
-          auto k = v2k.ab2b[vk];
-          auto vk_code = v2k.codes[vk];
-          auto kvv_c = code_which_down(vk_code);
-          auto kvv2v = gather_verts<mesh_dim + 1>(kv2v, k);
-          auto kvv2nx = gather_vectors<mesh_dim + 1, mesh_dim>(coords, kvv2v);
-          kvv2nx[kvv_c] = nx;
-          auto kvv2m = gather_symms<mesh_dim + 1, metric_dim>(metrics, kvv2v);
-          kvv2m[kvv_c] = nm;
-          auto km = maxdet_metric(kvv2m);
-          auto k_qual = metric_element_quality(kvv2nx, km);
-          new_qual = min2(new_qual, k_qual);
-          if (new_qual <= last_qual) break;
-        }  // end loop over elements for quality
-        if (new_qual <= last_qual) continue;
-        found_step = true;
-        last_qual = new_qual;
-        for (Int i = 0; i < pack.ncomps; ++i) {
-          new_sol_w[v * pack.ncomps + i] = tmp_sol_w[v * pack.ncomps + i];
-        }
-      }  // end loop over edges for possible steps
-      if (!found_step) break;
-    }  // end loop over motion steps
-    auto did_move = last_qual > old_qual;
-    qualities_w[cand] = last_qual;
-    did_move_w[cand] = did_move;
-    if (did_move) OMEGA_H_CHECK(last_qual >= 0.0);
+      }
+    }
+    new_quals_w[cand] = new_qual;
+    set_vector(new_coords_w, cand, new_x);
   };
   parallel_for(ncands, f);
-  auto qualities = Reals(qualities_w);
-  auto new_sol = Reals(new_sol_w);
-  return {Read<I8>(did_move_w), Reals(qualities_w), Reals(new_sol_w)};
+  auto new_quals = Reals(new_quals_w);
+  auto new_coords = Reals(new_coords_w);
+  return {new_quals, new_coords};
 }
 
 MotionChoices get_motion_choices(
