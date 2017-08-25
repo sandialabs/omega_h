98faf7057c7f33bc605bdf3af7d7dc773455fd40
Dan Ibanez <daibane@sandia.gov>
2017-08-25 08:29:50 -0600

    Augment Meshb I/O to support full topology

--- src/Omega_h_meshb.cpp
+++ src/Omega_h_meshb.cpp
@@ -69,52 +69,51 @@ static void read_meshb_version(Mesh* mesh, GmfFile file, int dim) {
       GmfGetLin(file, GmfVertices, &tmp[0], &tmp[1], &tmp[2], &ref);
     for (int j = 0; j < dim; ++j) coords[i * dim + j] = Real(tmp[j]);
   }
-  auto side_kwd = simplex_kwds[dim - 1];
-  LO nsides = LO(GmfStatKwd(file, side_kwd));
-  auto sides2verts = HostWrite<LO>(LO(nsides) * dim);
-  auto sides2class_id = HostWrite<I32>(LO(nsides));
-  safe_goto(file, side_kwd);
-  for (LO i = 0; i < nsides; ++i) {
-    GmfIndex class_id;
-    Few<GmfIndex, 3> tmp;
-    if (dim == 2)
-      GmfGetLin(file, side_kwd, &tmp[0], &tmp[1], &class_id);
-    else
-      GmfGetLin(file, side_kwd, &tmp[0], &tmp[1], &tmp[2], &class_id);
-    for (int j = 0; j < dim; ++j) {
-      sides2verts[i * dim + j] = LO(tmp[j] - 1);
+  HostWrite<LO> eqs2verts[4];
+  HostWrite<I32> eqs2class_id[4];
+  LO neqs[4];
+  for (Int ent_dim = 1; ent_dim <= dim; ++ent_dim) {
+    auto ent_kwd = simplex_kwds[ent_dim];
+    neqs[ent_dim] = LO(GmfStatKwd(file, ent_kwd));
+    if (ent_dim < dim && neqs[ent_dim] < 1) continue;
+    eqs2verts[ent_dim] = HostWrite<LO>(LO(neqs[ent_dim]) * (ent_dim + 1));
+    eqs2class_id[ent_dim] = HostWrite<I32>(LO(neqs[ent_dim]));
+    safe_goto(file, ent_kwd);
+    bool is_old_convention = true;
+    for (LO i = 0; i < neqs[ent_dim]; ++i) {
+      GmfIndex class_id;
+      Few<GmfIndex, 4> tmp;
+      if (ent_dim == 1) {
+        GmfGetLin(file, ent_kwd, &tmp[0], &tmp[1], &class_id);
+      } else if (ent_dim == 2) {
+        GmfGetLin(file, ent_kwd, &tmp[0], &tmp[1], &tmp[2], &class_id);
+      } else {
+        GmfGetLin(file, ent_kwd, &tmp[0], &tmp[1], &tmp[2], &tmp[3], &class_id);
+      }
+      for (Int j = 0; j < (ent_dim + 1); ++j) {
+        eqs2verts[ent_dim][i * (ent_dim + 1) + j] = LO(tmp[j]) - 1;
+      }
+      eqs2class_id[ent_dim][i] = I32(class_id);
+      if (LO(class_id) != i + 1) is_old_convention = false;
     }
-    sides2class_id[i] = LO(class_id);
-  }
-  auto elem_kwd = simplex_kwds[dim];
-  LO nelems = LO(GmfStatKwd(file, elem_kwd));
-  safe_goto(file, elem_kwd);
-  auto elems2verts = HostWrite<LO>(LO(nelems) * (dim + 1));
-  auto elem_class_ids = HostWrite<LO>(LO(nelems));
-  auto is_old_convention = true;
-  for (LO i = 0; i < nelems; ++i) {
-    GmfIndex ref;
-    Few<GmfIndex, 4> tmp;
-    if (dim == 2)
-      GmfGetLin(file, elem_kwd, &tmp[0], &tmp[1], &tmp[2], &ref);
-    else
-      GmfGetLin(file, elem_kwd, &tmp[0], &tmp[1], &tmp[2], &tmp[3], &ref);
-    if (ref != GmfIndex(i + 1)) is_old_convention = false;
-    elem_class_ids[i] = LO(ref);
-    for (int j = 0; j <= dim; ++j) {
-      elems2verts[i * (dim + 1) + j] = LO(tmp[j] - 1);
+    if (is_old_convention) {
+      eqs2class_id[ent_dim] = HostWrite<I32>();
     }
   }
   GmfCloseMesh(file);
-  build_from_elems2verts(mesh, dim, LOs(elems2verts.write()), LO(nverts));
+  build_from_elems2verts(mesh, dim, LOs(eqs2verts[dim].write()), LO(nverts));
   mesh->add_tag(VERT, "coordinates", dim, Reals(coords.write()));
-  if (is_old_convention) {
-    mesh->add_tag(dim, "class_id", 1, LOs(LO(nelems), 1));
+  if (eqs2class_id[dim].exists()) {
+    mesh->add_tag(dim, "class_id", 1, LOs(LO(neqs[dim]), 1));
   } else {
-    mesh->add_tag(dim, "class_id", 1, LOs(elem_class_ids.write()));
+    mesh->add_tag(dim, "class_id", 1, LOs(eqs2class_id[dim].write()));
   }
-  classify_equal_order(
-      mesh, dim - 1, sides2verts.write(), sides2class_id.write());
+  for (Int ent_dim = 1; ent_dim < dim; ++ent_dim) {
+    if (eqs2class_id[ent_dim].exists()) {
+      classify_equal_order(
+          mesh, ent_dim, eqs2verts[ent_dim].write(), eqs2class_id[ent_dim].write());
+    }
+  }
   finalize_classification(mesh);
 }
 
@@ -141,40 +140,31 @@ static void write_meshb_version(Mesh* mesh, GmfFile file, int dim) {
     else
       GmfSetLin(file, GmfVertices, tmp[0], tmp[1], tmp[2], ref);
   }
-  auto side_kwd = simplex_kwds[dim - 1];
-  auto sds2class_dim = mesh->get_array<I8>(dim - 1, "class_dim");
-  auto sds_are_sides = each_eq_to(sds2class_dim, I8(dim - 1));
-  auto sides2sds = collect_marked(sds_are_sides);
-  auto nsides = sides2sds.size();
-  auto sds2verts = mesh->ask_verts_of(dim - 1);
-  auto sds2class_id = mesh->get_array<LO>(dim - 1, "class_id");
-  auto sides2verts = HostRead<LO>(unmap(sides2sds, sds2verts, dim));
-  auto sides2class_id = HostRead<LO>(unmap(sides2sds, sds2class_id, 1));
-  GmfSetKwd(file, side_kwd, GmfLine(nsides));
-  for (LO i = 0; i < nsides; ++i) {
-    GmfIndex ref = sides2class_id[i];
-    Few<GmfIndex, 3> tmp;
-    for (int j = 0; j < dim; ++j) tmp[j] = sides2verts[i * dim + j] + 1;
-    if (dim == 2)
-      GmfSetLin(file, side_kwd, tmp[0], tmp[1], ref);
-    else
-      GmfSetLin(file, side_kwd, tmp[0], tmp[1], tmp[2], ref);
-  }
-  auto elem_kwd = simplex_kwds[dim];
-  auto nelems = mesh->nelems();
-  auto elems2verts = HostRead<LO>(mesh->ask_elem_verts());
-  auto elem_class_ids = HostRead<LO>(mesh->get_array<LO>(dim, "class_id"));
-  GmfSetKwd(file, elem_kwd, GmfLine(nelems));
-  for (LO i = 0; i < nelems; ++i) {
-    auto ref = GmfIndex(elem_class_ids[i]);
-    Few<GmfIndex, 4> tmp;
-    for (int j = 0; j < dim + 1; ++j) {
-      tmp[j] = GmfIndex(elems2verts[i * (dim + 1) + j] + 1);
+  for (Int ent_dim = 1; ent_dim <= dim; ++ent_dim) {
+    auto ents2class_dim = mesh->get_array<I8>(ent_dim, "class_dim");
+    auto ents2class_id = mesh->get_array<LO>(ent_dim, "class_id");
+    auto ents2verts = mesh->ask_verts_of(ent_dim);
+    auto ents_are_eqs = each_eq_to(ents2class_dim, I8(ent_dim));
+    auto eqs2ents = collect_marked(ents_are_eqs);
+    auto neqs = eqs2ents.size();
+    auto eqs2verts = HostRead<LO>(unmap(eqs2ents, ents2verts, ent_dim + 1));
+    auto eqs2class_id = HostRead<LO>(unmap(eqs2ents, ents2class_id, 1));
+    auto ent_kwd = simplex_kwds[ent_dim];
+    GmfSetKwd(file, ent_kwd, GmfLine(neqs));
+    for (LO i = 0; i < neqs; ++i) {
+      GmfIndex ref = eqs2class_id[i];
+      Few<GmfIndex, 4> tmp;
+      for (Int j = 0; j < (ent_dim + 1); ++j) {
+        tmp[j] = eqs2verts[i * (ent_dim + 1) + j] + 1;
+      }
+      if (ent_dim == 1) {
+        GmfSetLin(file, ent_kwd, tmp[0], tmp[1], ref);
+      } else if (ent_dim == 2) {
+        GmfSetLin(file, ent_kwd, tmp[0], tmp[1], tmp[2], ref);
+      } else {
+        GmfSetLin(file, ent_kwd, tmp[0], tmp[1], tmp[2], tmp[3], ref);
+      }
     }
-    if (dim == 2)
-      GmfSetLin(file, elem_kwd, tmp[0], tmp[1], tmp[2], ref);
-    else
-      GmfSetLin(file, elem_kwd, tmp[0], tmp[1], tmp[2], tmp[3], ref);
   }
   GmfCloseMesh(file);
 }
@@ -226,6 +216,8 @@ void write(Mesh* mesh, const char* filepath, int version) {
   Omega_h_fail("unknown libMeshb version %d when writing\n", version);
 }
 
+/* Symmetric metric tensor storage convention used by
+   INRIA:  https://hal.inria.fr/inria-00363007/document */
 OMEGA_H_INLINE static Matrix<1, 1> vector2symm_inria(Vector<1> v) {
   return matrix_1x1(v[0]);
 }
@@ -275,8 +267,6 @@ static Reals symms_inria2osh(Int dim, Reals symms) {
 }
 
 #if 0
-/* Symmetric metric tensor storage convention used by
-   INRIA:  https://hal.inria.fr/inria-00363007/document */
 OMEGA_H_INLINE static Vector<1> symm2vector_inria(Matrix<1, 1> symm) {
   return vector_1(symm[0][0]);
 }
