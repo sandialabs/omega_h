35e86f276751e1ba4d90c9dc755925f01afa81b1
Dan Ibanez <daibane@sandia.gov>
2017-09-25 18:52:31 -0600

    add an EGADS option to osh_fix

--- src/osh_fix.cpp
+++ src/osh_fix.cpp
@@ -5,6 +5,10 @@
 #include <Omega_h_cmdline.hpp>
 #include <Omega_h_metric.hpp>
 
+#ifdef OMEGA_H_USE_EGADS
+#include <Omega_h_egads.hpp>
+#endif
+
 #include <iostream>
 
 static void compute_metric(Omega_h::Mesh* mesh) {
@@ -19,6 +23,10 @@ int main(int argc, char** argv) {
   Omega_h::CmdLine cmdline;
   cmdline.add_arg<std::string>("mesh_in.osh");
   cmdline.add_arg<std::string>("mesh_out.osh");
+#ifdef OMEGA_H_USE_EGADS
+  auto& model_flag = cmdline.add_flag("--model", "optional EGADS model");
+  model_flag.add_arg<std::string>("model.step");
+#endif
   if (!cmdline.parse_final(lib.world(), &argc, argv)) return -1;
   auto path_in = cmdline.get<std::string>("mesh_in.osh");
   auto path_out = cmdline.get<std::string>("mesh_out.osh");
@@ -31,6 +39,14 @@ int main(int argc, char** argv) {
   std::cout << "computing minimum quality\n";
   auto minqual = mesh.min_quality();
   Omega_h::AdaptOpts opts(&mesh);
+#ifdef OMEGA_H_USE_EGADS
+  if (cmdline.parsed("--model")) {
+    auto model_path = cmdline.get<std::string>("--model", "model.step");
+    std::cout << "reading in " << model_path << '\n';
+    auto eg = Omega_h::egads_load(model_path);
+    opts.egads_model = eg;
+  }
+#endif
   while (minqual < opts.min_quality_desired && minqual > minqual_old) {
     std::cout << "current quality " << minqual << '\n';
     minqual_old = minqual;
@@ -46,4 +62,7 @@ int main(int argc, char** argv) {
   std::cout << "writing out " << path_out << '\n';
   mesh.remove_tag(Omega_h::VERT, "metric");
   Omega_h::binary::write(path_out, &mesh);
+#ifdef OMEGA_H_USE_EGADS
+  Omega_h::egads_free(opts.egads_model);
+#endif
 }
