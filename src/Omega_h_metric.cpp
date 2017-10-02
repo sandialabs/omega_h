72e9ac8efd0f866e2b67db792414145370d8a328
Dan Ibanez <dan.a.ibanez@gmail.com>
2017-10-02 12:28:54 +0200

    put back MOX metric

--- src/Omega_h_metric.cpp
+++ src/Omega_h_metric.cpp
@@ -644,4 +644,92 @@ Reals isos_from_lengths(Reals h) {
   return out;
 }
 
+/* Micheletti, S., and S. Perotto.
+ * "Anisotropic adaptation via a Zienkiewicz–Zhu error estimator
+ *  for 2D elliptic problems."
+ * Numerical Mathematics and Advanced Applications 2009.
+ * Springer Berlin Heidelberg, 2010. 645-653.
+ *
+ * Farrell, P. E., S. Micheletti, and S. Perotto.
+ * "An anisotropic Zienkiewicz–Zhu‐type error estimator for 3D applications."
+ * International journal for numerical methods in engineering
+ * 85.6 (2011): 671-692.
+ */
+
+template <Int dim>
+Reals get_aniso_zz_metric_dim(Mesh* mesh, Reals elem_gradients,
+    Real error_bound, Real max_size) {
+  OMEGA_H_CHECK(mesh->have_all_upward());
+  constexpr auto nverts_per_elem = dim + 1;
+  auto elem_verts2vert = mesh->ask_elem_verts();
+  auto verts2elems = mesh->ask_up(VERT, dim);
+  constexpr auto max_elems_per_patch =
+    AvgDegree<dim, 0, dim>::value * nverts_per_elem;
+  auto elems2volume = measure_elements_real(mesh);
+  auto nglobal_elems = get_sum(mesh->comm(), mesh->owned(dim));
+  auto out = Write<Real>(mesh->nelems() * symm_ncomps(dim));
+  auto f = OMEGA_H_LAMBDA(LO elem) {
+    Few<LO, max_elems_per_patch> patch_elems;
+    Int npatch_elems = 0;
+    for (auto ev = elem * nverts_per_elem;
+         ev < ((elem + 1) * nverts_per_elem); ++ev) {
+      auto vert = elem_verts2vert[ev];
+      for (auto ve = verts2elems.a2ab[vert];
+           ve < verts2elems.a2ab[vert + 1]; ++ve) {
+        auto patch_elem = verts2elems.ab2b[ve];
+        add_unique(patch_elems, npatch_elems, patch_elem);
+      }
+    }
+    Real patch_volume = 0;
+    auto grad_sum = zero_vector<dim>();
+    for (Int i = 0; i < npatch_elems; ++i) {
+      auto patch_elem = patch_elems[i];
+      auto gradient = get_vector<dim>(elem_gradients, patch_elem);
+      auto volume = elems2volume[patch_elem];
+      patch_volume += volume;
+      grad_sum = grad_sum + (gradient * volume);
+    }
+    auto grad_avg = grad_sum / patch_volume;
+    auto op_sum = zero_matrix<dim, dim>();
+    for (Int i = 0; i < npatch_elems; ++i) {
+      auto patch_elem = patch_elems[i];
+      auto gradient = get_vector<dim>(elem_gradients, patch_elem);
+      auto volume = elems2volume[patch_elem];
+      auto grad_err = grad_avg - gradient;
+      auto op = outer_product(grad_err, grad_err);
+      op_sum = op_sum + (op * volume);
+    }
+    auto op_avg = op_sum / patch_volume;
+    auto iso_volume = (dim == 3) ?
+      (1.0 / (6.0 * sqrt(2.0))) : (sqrt(3.0) / 4.0);
+    auto volume_factor = elems2volume[elem] / iso_volume;
+    auto pullback_volume = patch_volume / volume_factor;
+    auto a = square(error_bound) / (Real(dim) * nglobal_elems * pullback_volume);
+    auto op_decomp = decompose_eigen(op_avg);
+    auto g = op_decomp.l;
+    auto gv = op_decomp.q;
+    auto g_min = a / raise<dim>(max_size);
+    for (Int i = 0; i < dim; ++i) g[i] = max2(g[i], g_min);
+    Matrix<dim, dim> r;
+    for (Int i = 0; i < dim; ++i) r[i] = gv[dim - i - 1];
+    Vector<dim> h;
+    for (Int i = 0; i < dim; ++i) h[i] = root<dim>(a / g[dim - i - 1]);
+    auto m = compose_metric(r, h);
+    set_symm(out, elem, m);
+  };
+  parallel_for(mesh->nelems(), f);
+  return out;
+}
+
+Reals get_aniso_zz_metric(Mesh* mesh, Reals elem_gradients,
+    Real error_bound, Real max_size) {
+  if (mesh->dim() == 3) {
+    return get_aniso_zz_metric_dim<3>(mesh, elem_gradients, error_bound, max_size);
+  } else if (mesh->dim() == 2) {
+    return get_aniso_zz_metric_dim<2>(mesh, elem_gradients, error_bound, max_size);
+  } else {
+    OMEGA_H_NORETURN(Reals());
+  }
+}
+
 }  // end namespace Omega_h
