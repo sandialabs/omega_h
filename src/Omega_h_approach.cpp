014253c86cf7ec695ace81bace5e8b180c54f523
Dan Ibanez <daibane@sandia.gov>
2017-12-11 08:43:06 -0700

    Fix compile errors

--- src/Omega_h_approach.cpp
+++ src/Omega_h_approach.cpp
@@ -94,18 +94,18 @@ bool approach_metric(Mesh* mesh, AdaptOpts const& opts) {
       auto maxl = mesh->max_length();
       if (can_print(mesh)) {
         if (minq < opts.min_quality_allowed) {
-          std::cerr << "Metric approach has stalled with minimum quality
-            minq << " < " << opts.min_quality_allowed << "\n";
+          std::cerr << "Metric approach has stalled with minimum quality "
+            << minq << " < " << opts.min_quality_allowed << "\n";
           std::cerr << "Decreasing \"Min Quality Allowed\" may help, but otherwise the metric is likely not satisfiable\n";
         }
         if (maxl > opts.max_length_allowed) {
-          std::cerr << "Metric approach has stalled with maximum length
-            maxl << " < " << opts.max_length_allowed << "\n";
+          std::cerr << "Metric approach has stalled with maximum length "
+            << maxl << " < " << opts.max_length_allowed << "\n";
           std::cerr << "Increasing \"Max Length Allowed\" will probably fix this, otherwise the metric is likely not satisfiable\n";
         }
       }
       Omega_h_fail(
-          "Metric approach has stalled at step size = %f < %f.\n"
+          "Metric approach has stalled at step size = %f < %f.\n",
           t, min_t);
     }
     auto current = interpolate_between_metrics(mesh->nverts(), orig, target, t);
