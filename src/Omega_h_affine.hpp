861c369e817a2f2ff5f8ad806aea59f64656e188
Dan Ibanez <daibane@sandia.gov>
2017-12-20 10:40:50 -0700

    Starting to move Hilbert to Affine

---
+++ src/Omega_h_affine.hpp
@@ -0,0 +1,37 @@
+#ifndef OMEGA_H_AFFINE_HPP
+#define OMEGA_H_AFFINE_HPP
+
+#include <Omega_h_matrix.hpp>
+
+namespace Omega_h {
+
+template <Int dim>
+struct Affine {
+  Matrix<dim, dim> r;
+  Vector<dim> t;
+};
+
+template <Int dim>
+OMEGA_H_INLINE Vector<dim> operator*(Affine<dim> a, Vector<dim> v) {
+  return (a.r * v) + a.t;
+}
+
+template <Int dim>
+OMEGA_H_INLINE Affine<dim> invert(Affine<dim> a) {
+  Affine<dim> ai;
+  ai.r = invert(a.r);
+  ai.t = -(ai.r * a.t);
+  return ai;
+}
+
+template <Int dim>
+OMEGA_H_INLINE Affine<dim> simplex_affine(Few<Vector<dim>, dim + 1> p) {
+  Affine<dim> a;
+  a.r = simplex_basis<dim, dim>(p);
+  a.t = p[0];
+  return a;
+}
+
+}
+
+#endif
