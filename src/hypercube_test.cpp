8cc070c2b0608718d0bc125899bda7d134e82d81
Dan Ibanez <daibane@sandia.gov>
2018-02-07 13:55:54 -0700

    Implement and test binary I/O of HYPERCUBE
    Bumps binary file version number to 7

--- src/hypercube_test.cpp
+++ src/hypercube_test.cpp
@@ -3,6 +3,8 @@
 
 int main(int argc, char** argv) {
   auto lib = Omega_h::Library(&argc, &argv);
-  auto mesh = Omega_h::build_box(lib.world(), OMEGA_H_HYPERCUBE, 1.0, 1.0, 0.0, 8, 8, 0);
-  Omega_h::vtk::write_vtu("test.vtu", &mesh);
+  auto mesh = Omega_h::build_box(lib.world(), OMEGA_H_HYPERCUBE, 1.0, 1.0, 1.0, 8, 8, 3);
+  Omega_h::binary::write("hypercube.osh", &mesh);
+  mesh = Omega_h::binary::read("hypercube.osh", lib.world());
+  Omega_h::vtk::write_vtu("hypercube.vtu", &mesh);
 }
