28c730211ea6f77eaaf3e08b32c7c053e89d9c2b
Dan Ibanez <daibane@sandia.gov>
2018-02-14 09:00:24 -0700

    pybind11 single-value ctor for Reals

--- src/Omega_h_pybind11_array.cpp
+++ src/Omega_h_pybind11_array.cpp
@@ -5,7 +5,12 @@ namespace Omega_h {
 
 void pybind11_array(py::module& module) {
   py::class_<Read<Real>>(module, "Read_real");
-  py::class_<Reals, Read<Real>>(module, "Reals");
+  py::class_<Reals, Read<Real>>(module, "Reals")
+    .def(py::init<LO, Real, std::string const&>(),
+        py::arg("size"),
+        py::arg("value"),
+        py::arg("name") = "")
+    ;
 }
 
 }  // namespace Omega_h
