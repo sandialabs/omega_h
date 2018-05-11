276b712143abb24424a70af322138d867d7f4f95
Dan Ibanez <daibane@sandia.gov>
2018-05-11 11:48:21 -0600

    Use backward-cpp and libdwarf for awesome traces
    just set Omega_h_USE_dwarf=ON to get super-cool
    stack traces when --osh-signal is passed to your
    code.

--- src/Omega_h_library.cpp
+++ src/Omega_h_library.cpp
@@ -1,13 +1,20 @@
 #include "Omega_h_control.hpp"
+#include "Omega_h_config.h"
 
-#include <cxxabi.h>
-#include <execinfo.h>
 #include <csignal>
 #include <cstdarg>
 #include <cstdlib>
 #include <iostream>
 #include <sstream>
 #include <string>
+
+#ifdef OMEGA_H_USE_DWARF
+#define BACKWARD_HAS_DWARF 1
+#endif
+#include <backward/backward.hpp>
+namespace backward {
+  cfile_streambuf::int_type cfile_streambuf::underflow() { return traits_type::eof(); }
+}
 
 #include "Omega_h_cmdline.hpp"
 #include "Omega_h_library.hpp"
@@ -16,79 +23,11 @@ namespace Omega_h {
 
 char* max_memory_stacktrace = nullptr;
 
-// stacktrace.h (c) 2008, Timo Bingmann from http://idlebox.net/
-// published under the WTFPL v2.0
-
-/* Dan Ibanez: found this code for stacktrace printing,
-   cleaned it up for compilation as strict C++11,
-   fixed the parser for OS X */
-
-/** Print a demangled stack backtrace of the caller function to FILE* out. */
-
 void print_stacktrace(std::ostream& out, int max_frames) {
-  out << "stack trace:\n";
-  // storage array for stack trace address data
-  void** addrlist = new void*[max_frames];
-  // retrieve current stack addresses
-  int addrlen = backtrace(addrlist, max_frames);
-  if (addrlen == 0) {
-    out << "  <empty, possibly corrupt>\n";
-    return;
-  }
-  // resolve addresses into strings containing "filename(function+address)",
-  // this array must be free()-ed
-  char** symbollist = backtrace_symbols(addrlist, addrlen);
-  delete[] addrlist;
-  // iterate over the returned symbol lines. skip the first, it is the
-  // address of this function.
-  for (int i = 1; i < addrlen; i++) {
-#if defined(__APPLE__)
-    std::string line(symbollist[i]);
-    std::stringstream instream(line);
-    std::string num;
-    instream >> num;
-    std::string obj;
-    instream >> obj;
-    obj.resize(20, ' ');
-    std::string addr;
-    instream >> addr;
-    std::string symbol;
-    instream >> symbol;
-    std::string offset;
-    instream >> offset >> offset;
-    int status;
-    char* demangled = abi::__cxa_demangle(symbol.c_str(), NULL, NULL, &status);
-    if (status == 0) {
-      symbol = demangled;
-    }
-    free(demangled);
-    out << "  " << num << ' ' << obj << ' ' << addr << ' ' << symbol << " + "
-        << offset << '\n';
-#elif defined(__linux__)
-    std::string line(symbollist[i]);
-    auto open_paren = line.find_first_of('(');
-    auto start = open_paren + 1;
-    auto plus = line.find_first_of('+');
-    if (!(start < line.size() && start < plus && plus < line.size())) {
-      out << symbollist[i] << '\n';
-      continue;
-    }
-    auto symbol = line.substr(start, (plus - start));
-    int status;
-    char* demangled =
-        abi::__cxa_demangle(symbol.c_str(), nullptr, nullptr, &status);
-    if (status == 0) {
-      symbol = demangled;
-    }
-    free(demangled);
-    out << line.substr(0, start) << symbol << line.substr(plus, line.size())
-        << '\n';
-#else
-    out << symbollist[i] << '\n';
-    fprintf(out, "%s\n", symbollist[i]);
-#endif
-  }
-  free(symbollist);
+  ::backward::StackTrace st;
+  st.load_here(std::size_t(max_frames));
+  ::backward::Printer p;
+  p.print(st, out);
 }
 
 char const* Library::static_version() { return OMEGA_H_SEMVER; }
