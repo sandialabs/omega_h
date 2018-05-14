0eb39f1c484e812c16de6912275e19f2dc2561b6
Brian Granzow <bngranz@sandia.gov>
2018-05-14 13:29:07 -0600

    New debug case for 3D

--- src/amr_debug.cpp
+++ src/amr_debug.cpp
@@ -5,20 +5,28 @@
 
 using Omega_h::Bytes;
 
-static void test_2D_arrays(Omega_h::Library* lib) {
+static void test_3D_refine(Omega_h::Library* lib) {
   auto w = lib->world();
   auto f = OMEGA_H_HYPERCUBE;
-  auto m = Omega_h::build_box(w, f, 2.0, 1.0, 0.0, 2, 1, 0);
+  auto m = Omega_h::build_box(w, f, 1.0, 1.0, 1.0, 2, 1, 1);
   Omega_h::vtk::FullWriter writer("out", &m);
   writer.write();
   auto xfer_opts = Omega_h::TransferOpts();
-  Omega_h::amr_refine(&m, Omega_h::Bytes({1, 0}), xfer_opts);
+
+  std::cout << "refine 1: " << std::endl;
+  auto mark1 = Omega_h::Bytes({1, 0});
+  auto one_level_mark1 = Omega_h::enforce_one_level(&m, 2, mark1);
+  Omega_h::amr_refine(&m, one_level_mark1, xfer_opts);
   writer.write();
-  Omega_h::amr_refine(&m, Omega_h::Bytes({0,0,0,0,0,1}), xfer_opts);
+
+  std::cout << "refine 2: " << std::endl;
+  auto mark2 = Omega_h::Bytes({0, 0, 1, 0, 0, 0, 0, 0, 0, 0});
+  auto one_level_mark2 = Omega_h::enforce_one_level(&m, 2, mark2);
+  Omega_h::amr_refine(&m, one_level_mark2, xfer_opts);
   writer.write();
 }
 
 int main(int argc, char** argv) {
   auto lib = Omega_h::Library(&argc, &argv);
-  test_2D_arrays(&lib);
+  test_3D_refine(&lib);
 }
