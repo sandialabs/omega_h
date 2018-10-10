d723312495ff2da59de3a7747fa96966216d142d
Dan Ibanez <daibane@sandia.gov>
2018-10-10 15:10:13 -0600

    forgot to allocate HostWrite(size, name)

--- src/Omega_h_array.cpp
+++ src/Omega_h_array.cpp
@@ -225,6 +225,9 @@ HostWrite<T>::HostWrite(LO size_in, std::string const& name_in)
       mirror_(create_uninit_mirror_view(write_.view()))
 #endif
 {
+#if defined(OMEGA_H_USE_CUDA)
+  mirror_.reset(new T[std::size_t(write_.size())]);
+#endif
 }
 
 template <typename T>
