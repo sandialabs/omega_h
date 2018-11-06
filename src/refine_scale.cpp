ac4c549d5f21ee850978521223b929773f63d5ba
Dan Ibanez <daibane@sandia.gov>
2018-11-06 10:23:48 -0700

    start redesigning refine_scale

--- src/refine_scale.cpp
+++ src/refine_scale.cpp
@@ -1,47 +1,43 @@
-#include <Omega_h_adapt.hpp>
-#include <Omega_h_array_ops.hpp>
-#include <Omega_h_build.hpp>
-#include <Omega_h_metric.hpp>
+#include <Omega_h_file.hpp>
+#include <Omega_h_cmdline.hpp>
 #include <Omega_h_profile.hpp>
-
 #include <iostream>
 
 int main(int argc, char** argv) {
   auto lib = Omega_h::Library(&argc, &argv);
-  Omega_h::ScopedTimer scoped_timer("main");
-  auto world = lib.world();
-  if (world->rank() == 0)
-    std::cout << "building a box of " << (6 * 7 * 7 * 210) << " elements!\n";
-  auto mesh =
-      Omega_h::build_box(world, OMEGA_H_SIMPLEX, 1e-6, 1e-6, 1e-3, 7, 7, 210);
-  if (world->rank() == 0) std::cout << "done building the box, re-balancing!\n";
-  mesh.balance();
-  if (world->rank() == 0)
-    std::cout << "done rebalancing, rank 0 has " << mesh.nelems()
-              << " elements!\n";
-  if (world->rank() == 0) std::cout << "ghosting!\n";
-  mesh.set_parting(OMEGA_H_GHOSTED);
-  if (world->rank() == 0)
-    std::cout << "done ghosting, rank 0 has " << mesh.nelems()
-              << " elements!\n";
-  if (world->rank() == 0) std::cout << "computing implied metric!\n";
-  Omega_h::add_implied_metric_tag(&mesh);
-  auto ncurrent_elems = mesh.nglobal_ents(mesh.dim());
-  auto ntarget_elems = ncurrent_elems * 8;
-  if (world->rank() == 0)
-    std::cout << "done with implied metric, scaling it to " << ntarget_elems
-              << " elements!\n";
-  auto metrics = mesh.get_array<double>(0, "metric");
-  auto scalar = Omega_h::get_metric_scalar_for_nelems(
-      mesh.dim(), ncurrent_elems, ntarget_elems);
-  if (world->rank() == 0) std::cout << "scalar is " << scalar << "!\n";
-  metrics = multiply_each_by(metrics, scalar);
-  mesh.add_tag(Omega_h::VERT, "target_metric", Omega_h::symm_ncomps(mesh.dim()),
-      metrics);
-  auto opts = Omega_h::AdaptOpts(&mesh);
-  opts.verbosity = Omega_h::EXTRA_STATS;
-  if (world->rank() == 0) std::cout << "done scaling, adapting!\n";
-  while (Omega_h::approach_metric(&mesh, opts)) {
-    Omega_h::adapt(&mesh, opts);
+  Omega_h::CmdLine cmdline;
+  cmdline.add_arg<std::string>("input.osh");
+  cmdline.add_arg<double>("desired-num-elements");
+  cmdline.add_arg<std::string>("output.osh");
+  auto const world = lib.world();
+  if (!cmdline.parse_final(world, &argc, argv)) {
+    return -1;
   }
+  Omega_h::ScopedTimer scoped_timer("main");
+  auto const world_size = world->size();
+  auto const world_rank = world->rank();
+  auto const inpath = cmdline.get<std::string>("input.osh");
+  auto const outpath = cmdline.get<std::string>("output.osh");
+  Omega_h::Mesh mesh(&lib);
+  for (int shift = 0; (1 << shift) <= world_size; ++shift) {
+    int const group_size = (1 << shift);
+    int const group_in_world = world_rank / group_size;
+    int const group_rank = world_rank % group_size;
+    auto const group = world->split(group_in_world, group_rank);
+    OMEGA_H_CHECK(group->rank() == group_rank);
+    if (group_in_world == 0) {
+      if (shift == 0) {
+        std::cout << "reading mesh...\n";
+        mesh = Omega_h::binary::read(inpath, group, true);
+      } else {
+        mesh.set_comm(group);
+        mesh.balance();
+      }
+      auto const nelems = mesh.nglobal_ents(mesh.dim());
+      auto const nelems_local = mesh.nelems();
+      if (world_rank == 0) std::cout << "mesh has " << nelems << " total elements, " << nelems_local << " on rank 0\n";
+    }
+    world->barrier();
+  }
+  return 0;
 }
