84ddeebd558b95ef3768e85beb17fe3eed62de5a
Dan Ibanez <daibane@sandia.gov>
2018-11-16 20:16:56 -0700

    CUB scan is 2X faster than Thrust
    our memory pool may be helping here
    too...

--- src/Omega_h_scan.hpp
+++ src/Omega_h_scan.hpp
@@ -20,6 +20,8 @@
 
 #include <thrust/execution_policy.h>
 #include <thrust/transform_scan.h>
+#include <thrust/system/cuda/detail/cub/device/device_scan.cuh>
+#include <Omega_h_malloc.hpp>
 
 #elif defined(OMEGA_H_USE_OPENMP)
 
@@ -48,7 +50,20 @@ void parallel_scan(LO n, T f, char const* name = "") {
 template <typename InputIterator, typename OutputIterator>
 OutputIterator inclusive_scan(InputIterator first, InputIterator last,
     OutputIterator result) {
-  return thrust::inclusive_scan(thrust::device, first, last, result);
+  std::size_t temp_storage_bytes;
+  int const n = int(last - first);
+  auto err = thrust::cuda_cub::cub::DeviceScan::InclusiveSum(
+      nullptr, temp_storage_bytes,
+      first, result, (last - first));
+  OMEGA_H_CHECK(err == cudaSuccess);
+  void* d_temp_storage = maybe_pooled_device_malloc(temp_storage_bytes);
+  err = thrust::cuda_cub::cub::DeviceScan::InclusiveSum(
+      d_temp_storage, temp_storage_bytes,
+      first, result, n);
+  OMEGA_H_CHECK(err == cudaSuccess);
+  maybe_pooled_device_free(d_temp_storage, temp_storage_bytes);
+  return result + n;
+//return thrust::inclusive_scan(thrust::device, first, last, result);
 }
 
 template <typename InputIterator, typename OutputIterator, typename BinaryOp,
