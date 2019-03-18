fa3f2adfe0966a1ffa9964dee9641e52bd0b27af
Brian Granzow <bngranz@sandia.gov>
2019-03-18 10:34:38 -0600

    Fix modify logic for AMR + MPI

--- src/Omega_h_modify.cpp
+++ src/Omega_h_modify.cpp
@@ -88,7 +88,10 @@ static Remotes get_prod_owners_shared(Mesh* mesh, Few<LOs, 4> mods2mds,
     auto nmods = mods2prods[mod_dim].size() - 1;
     auto nmod_prods = prod_end - prod_begin;
     /* HACK: assuming the number of products per split entity is constant! */
-    auto nprods_per_mod = divide_no_remainder(nmod_prods, nmods);
+    Write<LO> nprods_per_mod_array(1);
+    if (nmods == 0) nprods_per_mod_array[0] = 0;
+    else  nprods_per_mod_array[0] = divide_no_remainder(nmod_prods, nmods);
+    auto nprods_per_mod = get_max(Omega_h::read(nprods_per_mod_array));
     auto md_ranks = mesh->ask_owners(mod_dim).ranks;
     auto mod_ranks = read(unmap(mods2mds[mod_dim], md_ranks, 1));
     auto mod_prod_idxs = unmap_range(prod_begin, prod_end, prods2new_ents, 1);
