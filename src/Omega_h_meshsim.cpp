a7ac138e3bdf33f49a44e0993cf45687e426ddfc
Cameron Smith <smithc11@rpi.edu>
2019-03-28 11:40:32 -0400

    builds ents

--- src/Omega_h_meshsim.cpp
+++ src/Omega_h_meshsim.cpp
@@ -16,17 +16,123 @@ namespace meshsim {
 
 namespace {
 
+int classId(pEntity e) {
+  pGEntity g = EN_whatIn(e);
+  assert(g);
+  return GEN_tag(g);
+}
+
 void read_internal(pParMesh sm, Mesh* mesh) {
   (void)mesh;
-  auto i = 0;
   pMesh m = PM_mesh(sm, 0);
+  Int max_dim;
+  if (M_numRegions(m)) {
+    max_dim = 3;
+  } else if (M_numFaces(m)) {
+    max_dim = 2;
+  } else if (M_numEdges(m)) {
+    max_dim = 1;
+  } else {
+    Omega_h_fail("There were no Elements of dimension higher than zero!\n");
+  }
+  //get the types of elements
+  Omega_h_Family family = OMEGA_H_SIMPLEX;
+  RIter regions = M_regionIter(m);
+  pRegion rgn;
+  LO i = 0;
+  while ((rgn = (pRegion) RIter_next(regions))) {
+    if(R_topoType(rgn) != Rtet)
+      Omega_h_fail("Non-simplex element found!\n");
+    ++i;
+  }
+  RIter_delete(regions);
+  std::vector<int> ent_nodes[4];
+  std::vector<int> ent_class_ids[4];
+  //write vertex coords into node_coords and
+  //  write vertex ids into ent_nodes
+  const int numVtx = M_numVertices(m);
+  ent_nodes[0].reserve(numVtx);
+  ent_class_ids[0].reserve(numVtx);
+  HostWrite<Real> host_coords(numVtx*max_dim);
+  VIter vertices = M_vertexIter(m);
+  pVertex vtx;
+  i = 0;
+  while ((vtx = (pVertex) VIter_next(vertices))) {
+    double xyz[3];
+    V_coord(vtx,xyz);
+    for(int j=0; j<max_dim; j++)
+      host_coords[i + max_dim + j] = xyz[j];
+    ent_nodes[0].push_back(EN_id(vtx));
+    ent_class_ids[0].push_back(classId(vtx));
+    ++i;
+  }
+  VIter_delete(vertices);
+  //get the ids of vertices bounding each edge
+  const int numEdges = M_numEdges(m);
+  ent_nodes[1].reserve(numEdges*2);
+  ent_class_ids[1].reserve(numEdges);
   EIter edges = M_edgeIter(m);
-  pEntity ent;
-  while ((ent = EIter_next(edges))) {
-    i++;
+  pEdge edge;
+  while ((edge = (pEdge) EIter_next(edges))) {
+    for(int j=0; j<2; j++) {
+      vtx = E_vertex(edge,j);
+      ent_nodes[1].push_back(EN_id(vtx));
+    }
+    ent_class_ids[1].push_back(classId(edge));
   }
   EIter_delete(edges);
-  fprintf(stderr, "edges %d\n", i);
+  //get the ids of vertices bounding each face
+  const int numFaces = M_numFaces(m);
+  ent_nodes[2].reserve(numFaces*3);
+  ent_class_ids[2].reserve(numFaces);
+  FIter faces = M_faceIter(m);
+  pFace face;
+  while ((face = (pFace) FIter_next(faces))) {
+    pPList verts = F_vertices(face,0);
+    assert(PList_size(verts) == 3);
+    void *iter = 0; // must initialize to 0
+    while((vtx = (pVertex)PList_next(verts, &iter)))
+      ent_nodes[2].push_back(EN_id(vtx));
+    PList_delete(verts);
+    ent_class_ids[2].push_back(classId(face));
+  }
+  EIter_delete(edges);
+  //get the ids of vertices bounding each region
+  const int numRegions = M_numRegions(m);
+  ent_nodes[3].reserve(numRegions*4);
+  ent_class_ids[3].reserve(numRegions);
+  regions = M_regionIter(m);
+  while ((rgn = (pRegion) RIter_next(regions))) {
+    pPList verts = R_vertices(rgn,0);
+    assert(PList_size(verts) == 4);
+    void *iter = 0; // must initialize to 0
+    while((vtx = (pVertex)PList_next(verts, &iter)))
+      ent_nodes[3].push_back(EN_id(vtx));
+    PList_delete(verts);
+    ent_class_ids[3].push_back(classId(rgn));
+  }
+  RIter_delete(regions);
+  //flatten the ent_nodes and ent_class_ids arrays
+  for (Int ent_dim = max_dim; ent_dim >= 0; --ent_dim) {
+    Int neev = element_degree(family, ent_dim, VERT);
+    LO ndim_ents = static_cast<LO>(ent_nodes[ent_dim].size()) / neev;
+    HostWrite<LO> host_ev2v(ndim_ents * neev);
+    HostWrite<LO> host_class_id(ndim_ents);
+    for (i = 0; i < ndim_ents; ++i) {
+      for (Int j = 0; j < neev; ++j) {
+        host_ev2v[i * neev + j] =
+            ent_nodes[ent_dim][static_cast<std::size_t>(i * neev + j)];
+      }
+      host_class_id[i] = ent_class_ids[ent_dim][static_cast<std::size_t>(i)];
+    }
+    auto eqv2v = Read<LO>(host_ev2v.write());
+    if (ent_dim == max_dim) {
+      build_from_elems_and_coords(
+          mesh, family, max_dim, eqv2v, host_coords.write());
+    }
+    classify_equal_order(mesh, ent_dim, eqv2v, host_class_id.write());
+  }
+  finalize_classification(mesh);
 }
 
 }  // end anonymous namespace
