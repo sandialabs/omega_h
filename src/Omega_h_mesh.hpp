a11f61d86de8778530d586d3f0b0b672ba856cd6
joshia5 <joshia5@rpi.edu>
2020-03-21 03:18:49 -0400

    new balance function

--- src/Omega_h_mesh.hpp
+++ src/Omega_h_mesh.hpp
@@ -140,6 +140,9 @@ class Mesh {
   void set_parting(Omega_h_Parting parting_in, Int nlayers, bool verbose);
   void set_parting(Omega_h_Parting parting_in, bool verbose = false);
   void balance(bool predictive = false);
+  //new balance function below
+  void balance(Reals masses);
+  //
   Graph ask_graph(Int from, Int to);
   template <typename T>
   Read<T> sync_array(Int ent_dim, Read<T> a, Int width);
