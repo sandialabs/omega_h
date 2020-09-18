771a4b8cab447c9476d11bceb55f43f0e0f53fa1
joshia5 <joshia5@rpi.edu>
2020-09-18 00:20:39 -0400

    get inputs; mesh comm to be fixed for parallel

--- src/meshsim2osh.cpp
+++ src/meshsim2osh.cpp
@@ -8,40 +8,36 @@
 
 int main(int argc, char** argv) {
   auto lib = Omega_h::Library(&argc, &argv);
-  auto comm = lib.world();
+  auto world = lib.world();
+
   Omega_h::CmdLine cmdline;
   cmdline.add_arg<std::string>("mesh-in");
   cmdline.add_arg<std::string>("model-in");
   cmdline.add_arg<std::string>("mesh-out");
   cmdline.add_arg<std::string>("model-out");
-  if (!cmdline.parse_final(comm, &argc, argv)) return -1;
+
+  cmdline.add_arg<int>("nparts_out");
+
+  auto nparts_total = world->size();
+  auto nparts_in = 1;
+  //todo: add checks on nparts out and total
+
+  if (!cmdline.parse_final(world, &argc, argv)) return -1;
   auto mesh_in = cmdline.get<std::string>("mesh-in");
   auto model_in = cmdline.get<std::string>("model-in");
   auto mesh_out = cmdline.get<std::string>("mesh-out");
   auto model_out = cmdline.get<std::string>("model-out");
-  auto mesh = Omega_h::meshsim::read(mesh_in, model_in, comm);
-  Omega_h::binary::write(mesh_out, &mesh);
-  std::cout << "wrote mesh " << mesh_out << "\n";
 
-  Omega_h::binary::write_model(model_out, &mesh);
-  auto old_model_ents_v = mesh.ask_model_ents(0);
-  auto old_model_matches_v = mesh.ask_model_matches(0);
-  auto old_model_ents_e = mesh.ask_model_ents(1);
-  auto old_model_matches_e = mesh.ask_model_matches(1);
-  auto old_model_ents_f = mesh.ask_model_ents(2);
-  auto old_model_matches_f = mesh.ask_model_matches(2);
-  Omega_h::binary::read_model(model_out, &mesh);
-  auto new_model_ents_v = mesh.ask_model_ents(0);
-  auto new_model_matches_v = mesh.ask_model_matches(0);
-  auto new_model_ents_e = mesh.ask_model_ents(1);
-  auto new_model_matches_e = mesh.ask_model_matches(1);
-  auto new_model_ents_f = mesh.ask_model_ents(2);
-  auto new_model_matches_f = mesh.ask_model_matches(2);
-  OMEGA_H_CHECK(old_model_ents_v == new_model_ents_v);
-  OMEGA_H_CHECK(old_model_matches_v == new_model_matches_v);
-  OMEGA_H_CHECK(old_model_ents_e == new_model_ents_e);
-  OMEGA_H_CHECK(old_model_matches_e == new_model_matches_e);
-  OMEGA_H_CHECK(old_model_ents_f == new_model_ents_f);
-  OMEGA_H_CHECK(old_model_matches_f == new_model_matches_f);
+  auto nparts_out = cmdline.get<int>("nparts_out");
+  if (!world->rank()) {
+  printf("nparts1out=%d\n", nparts_out);
+    auto mesh = Omega_h::meshsim::read(mesh_in, model_in, world);
+  printf("nparts2out=%d\n", nparts_out);
+    Omega_h::binary::write(mesh_out, &mesh);
+  printf("nparts3out=%d\n", nparts_out);
+    Omega_h::binary::write_model(model_out, &mesh);
+  printf("nparts4out=%d\n", nparts_out);
+  }
+
   return 0;
 }
