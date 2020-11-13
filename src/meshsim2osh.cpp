a4be05b0991a866fe45ad3c1706f768f655cacd6
joshia5 <joshia5@rpi.edu>
2020-11-12 19:23:20 -0500

    add edge case tests on nparts

--- src/meshsim2osh.cpp
+++ src/meshsim2osh.cpp
@@ -18,8 +18,7 @@ int main(int argc, char** argv) {
   cmdline.add_arg<int>("nparts_out");
 
   auto nparts_total = world->size();
-  auto nparts_in = 1;
-  //todo: add edge case checks on nparts out and total
+  auto nparts_in = 1;//input distributed mesh not implemented
 
   if (!cmdline.parse_final(world, &argc, argv)) return -1;
   auto mesh_in = cmdline.get<std::string>("mesh-in");
@@ -27,6 +26,27 @@ int main(int argc, char** argv) {
   auto mesh_out = cmdline.get<std::string>("mesh-out");
   auto nparts_out = cmdline.get<int>("nparts_out");
   auto mesh = Omega_h::Mesh(&lib);
+
+  if (nparts_out < 1) {
+    if (!world->rank()) {
+      std::cout << "error: invalid output part count " << nparts_out << '\n';
+    }
+    return -1;
+  }
+  if (nparts_out > nparts_total) {
+    if (!world->rank()) {
+      std::cout << "error: output part count " << nparts_out
+                << " greater than MPI job size " << nparts_total << '\n';
+    }
+    return -1;
+  }
+  if (nparts_in > nparts_total) {
+    if (!world->rank()) {
+      std::cout << "error: input part count " << nparts_in
+                << " greater than MPI job size " << nparts_total << '\n';
+    }
+    return -1;
+  }
 
   auto is_in = (world->rank() < nparts_in);
   auto comm_in = world->split(int(is_in), 0);
@@ -37,23 +57,6 @@ int main(int argc, char** argv) {
   if (is_in || is_out) mesh.set_comm(comm_out);
   if (is_out) {
     if (nparts_out != nparts_in) mesh.balance();
-
-    auto rank = world->rank();
-    mesh.add_tag<Omega_h::Real>(0, "gravity", 1);
-    Omega_h::Write<Omega_h::Real> gravityArray(mesh.nverts(), 0.0, "gravityArray");
-    auto leaf_ids = mesh.get_matches(0).leaf_idxs;
-    auto root_ids = mesh.get_matches(0).root_idxs;
-    auto root_rks = mesh.get_matches(0).root_ranks;
-    auto fill_tag = OMEGA_H_LAMBDA (Omega_h::LO i) {
-      auto leaf = leaf_ids[i];
-      auto root = root_ids[i];
-      auto root_rk = root_rks[i];
-      if ((root == leaf) && (root_rk == rank))
-        gravityArray[leaf] = 9.81;
-    };
-    Omega_h::parallel_for(leaf_ids.size(), fill_tag);
-    mesh.set_tag<Omega_h::Real>(0, "gravity", Omega_h::Reals(gravityArray));
-    mesh.sync_tag_periodic(0, "gravity");
 
     Omega_h::binary::write(mesh_out, &mesh);
   }
--- src/periodic_test.cpp
+++ src/periodic_test.cpp
@@ -28,6 +28,27 @@ int main(int argc, char** argv) {
   auto nparts_out = cmdline.get<int>("nparts_out");
   auto mesh = Omega_h::Mesh(&lib);
 
+  if (nparts_out < 1) {
+    if (!world->rank()) {
+      std::cout << "error: invalid output part count " << nparts_out << '\n';
+    }
+    return -1;
+  }
+  if (nparts_out > nparts_total) {
+    if (!world->rank()) {
+      std::cout << "error: output part count " << nparts_out
+                << " greater than MPI job size " << nparts_total << '\n';
+    }
+    return -1;
+  }
+  if (nparts_in > nparts_total) {
+    if (!world->rank()) {
+      std::cout << "error: input part count " << nparts_in
+                << " greater than MPI job size " << nparts_total << '\n';
+    }
+    return -1;
+  }
+
   auto is_in = (world->rank() < nparts_in);
   auto comm_in = world->split(int(is_in), 0);
   auto is_out = (world->rank() < nparts_out);
