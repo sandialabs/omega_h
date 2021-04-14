2fc3d3e3a1dab9ef5f32c3210551d4f9e9feefe1
joshia5 <joshia5@rpi.edu>
2021-04-14 14:52:55 -0400

    cleanup

--- src/boundary_field_test.cpp
+++ src/boundary_field_test.cpp
@@ -21,10 +21,6 @@ void test_2d(Library *lib) {
   OMEGA_H_CHECK(!mesh.has_tag(0, "field1"));
   OMEGA_H_CHECK(mesh.has_tag(0, "field1_boundary"));
 
-  //mesh.change_tagToMesh<Real>(0, 1, "field1_boundary");
-  //OMEGA_H_CHECK(mesh.has_tag(0, "field1"));
-  //OMEGA_H_CHECK(!mesh.has_tag(0, "field1_boundary"));
-
   mesh.add_tag<Real>(2, "field2", 2);
   Write<Real> vals2(mesh.nfaces()*2, 50);
   Read<Real> vals_r2(vals2);
@@ -33,10 +29,6 @@ void test_2d(Library *lib) {
   mesh.change_tagToBoundary<Real>(2, 2, "field2");
   OMEGA_H_CHECK(!mesh.has_tag(2, "field2"));
   OMEGA_H_CHECK(mesh.has_tag(2, "field2_boundary"));
-
-  //mesh.change_tagToMesh<Real>(2, 2, "field2_boundary");
-  //OMEGA_H_CHECK(mesh.has_tag(2, "field2"));
-  //OMEGA_H_CHECK(!mesh.has_tag(2, "field2_boundary"));
 
   mesh.add_tag<Real>(0, "meshfield", 3);
   Write<Real> vals3(mesh.nverts()*3, 500);
