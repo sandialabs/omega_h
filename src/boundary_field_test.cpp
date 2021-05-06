59741096b92b9a2173553b8e79333354c92b312a
joshia5 <joshia5@rpi.edu>
2021-05-06 15:38:40 -0400

    create distinct serial adapt test

--- src/boundary_field_test.cpp
+++ src/boundary_field_test.cpp
@@ -4,9 +4,68 @@
 #include "Omega_h_file.hpp"
 #include "Omega_h_mesh.hpp"
 #include "Omega_h_array_ops.hpp"
+#include <Omega_h_metric.hpp>
+#include <Omega_h_adapt.hpp>
+#include <Omega_h_timer.hpp>
+#include <Omega_h_for.hpp>
 
 using namespace Omega_h;
 
+template <Int dim>
+static void set_target_metric(Mesh* mesh) {
+  auto coords = mesh->coords();
+  auto target_metrics_w = Write<Real>(mesh->nverts() * symm_ncomps(dim));
+  auto f = OMEGA_H_LAMBDA(LO v) {
+    auto z = coords[v * dim + (dim - 1)];
+    auto h = Vector<dim>();
+    for (Int i = 0; i < dim - 1; ++i) h[i] = 0.1;
+    h[dim - 1] = 0.001 + 0.198 * std::abs(z - 0.5);
+    auto m = diagonal(metric_eigenvalues_from_lengths(h));
+    set_symm(target_metrics_w, v, m);
+  };
+  parallel_for(mesh->nverts(), f);
+  mesh->set_tag(VERT, "target_metric", Reals(target_metrics_w));
+}
+
+template <Int dim>
+void run_case(Mesh* mesh, char const* vtk_path) {
+  auto world = mesh->comm();
+  mesh->set_parting(OMEGA_H_GHOSTED);
+  auto implied_metrics = get_implied_metrics(mesh);
+  mesh->add_tag(0, "metric", symm_ncomps(dim), implied_metrics);
+  mesh->add_tag<Real>(VERT, "target_metric", symm_ncomps(dim));
+  set_target_metric<dim>(mesh);
+
+  OMEGA_H_CHECK(mesh->has_tag(0, "metric"));
+
+  mesh->set_parting(OMEGA_H_ELEM_BASED);
+
+  OMEGA_H_CHECK(mesh->has_tag(0, "metric"));
+
+  mesh->ask_lengths();
+  mesh->ask_qualities();
+  vtk::FullWriter writer;
+  if (vtk_path) {
+    writer = vtk::FullWriter(vtk_path, mesh);
+    writer.write();
+  }
+
+  auto opts = AdaptOpts(mesh);
+  opts.verbosity = EXTRA_STATS;
+  opts.length_histogram_max = 2.0;
+  opts.max_length_allowed = opts.max_length_desired * 2.0;
+  opts.xfer_opts.type_map["field1_boundary"] = OMEGA_H_LINEAR_INTERP;
+  //TODO: change this so that the type map should only be "field1"
+  Now t0 = now();
+  while (approach_metric(mesh, opts)) {
+    adapt(mesh, opts);
+    if (mesh->has_tag(VERT, "target_metric")) set_target_metric<dim>(mesh);
+    if (vtk_path) writer.write();
+  }
+  Now t1 = now();
+  std::cout << "total time: " << (t1 - t0) << " seconds\n";
+
+}
 void test_2d(Library *lib) {
 
   auto mesh = Mesh(lib);
@@ -27,22 +86,26 @@ void test_2d(Library *lib) {
   Write<Real> vals3(mesh.nverts()*3, 500);
   Read<Real> vals_r3(vals3);
 
-  printf("btest ok1\n");
   vtk::write_vtu ("./../../omega_h/meshes/plate_6elem.vtu",
                    &mesh);
-  printf("btest ok2\n");
   binary::write ("./../../omega_h/meshes/plate_6elem_bField.osh",
                    &mesh);
-  printf("btest ok3\n");
 
   auto new_mesh = Mesh(lib);
   binary::read ("./../../omega_h/meshes/plate_6elem_bField.osh",
                 lib->world(), &new_mesh);
-  printf("btest ok4\n");
   auto new_bField = new_mesh.get_boundaryField_array<Real>(0, "field1"); 
-  printf("btest ok5\n");
   OMEGA_H_CHECK(new_bField == vals_r);
   OMEGA_H_CHECK(new_bField.size() < nverts);
+
+  return;
+}
+
+void test_3d(Library *lib) {
+  auto mesh = Mesh(lib);
+  binary::read ("./../../omega_h/meshes/box_3d.osh",
+                lib->world(), &mesh);
+  run_case<3>(&mesh, "./../../omega_h/meshes/adapt/box3d.vtk");
 
   return;
 }
@@ -52,6 +115,7 @@ int main(int argc, char** argv) {
   auto lib = Library (&argc, &argv);
 
   test_2d(&lib);
+  test_3d(&lib);
 
   return 0;
 }
