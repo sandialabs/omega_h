8dcaeaa1f5ca74f5b179c3693bf0e3ff9749cf28
Cameron Smith <smithc11@rpi.edu>
2021-05-27 20:13:05 +0000

    initialize kokkos first

--- src/unit_math.cpp
+++ src/unit_math.cpp
@@ -1,9 +1,10 @@
+#include <Kokkos_Core.hpp>
 #include <Omega_h_library.hpp>
 
 using namespace Omega_h;
 
-
 int main(int argc, char** argv) {
+  Kokkos::initialize(argc, argv);
   auto lib = Library(&argc, &argv);
   return 0;
 }
