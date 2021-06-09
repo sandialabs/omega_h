af621fcf9e6ed848a127d644256d976f243cade6
joshia5 <joshia5@rpi.edu>
2021-06-08 21:38:16 -0400

    cleanup

--- src/meshsim2osh.cpp
+++ src/meshsim2osh.cpp
@@ -16,19 +16,10 @@ int main(int argc, char** argv) {
   auto mesh_in = cmdline.get<std::string>("mesh-in");
   auto model_in = cmdline.get<std::string>("model-in(geomSim)");
   auto mesh_out = cmdline.get<std::string>("mesh-out");
-  comm->barrier();
-  printf("ok0 meshsim2osh\n");
   auto mesh = Omega_h::meshsim::read(mesh_in, model_in, comm);
-  mesh.comm()->barrier();
-  printf("ok1 meshsim2osh\n");
   auto family = mesh.family();
   if ((family == OMEGA_H_SIMPLEX) || family == OMEGA_H_HYPERCUBE) {
-    printf("ok2 meshsim2osh\n");
-    std::cout << mesh_out.c_str() << "\n";
     Omega_h::binary::write(mesh_out, &mesh);
-    Omega_h::vtk::FullWriter writer(
-      "/lore/joshia5/Meshes/mixed_mesh/cube_with_cutTriCube_5k_Full", &mesh);
-    writer.write();
   }
 
   return 0;
