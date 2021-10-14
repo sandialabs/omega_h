96d30c559e42f6c346a9d8d477a85121a03da736
Cameron Smith <smithc11@rpi.edu>
2021-10-14 18:49:26 +0000

    nearly functional reduction test over bbox

--- src/reduce_test.cpp
+++ src/reduce_test.cpp
@@ -1,24 +1,84 @@
 #include "Omega_h_for.hpp"
 #include "Omega_h_library.hpp"
+#include "Omega_h_bbox.hpp"
+#include "Omega_h_bbox.hpp"
 
 using namespace Omega_h;
+
+template <Int dim>
+struct GetBBoxOp2 {
+  Reals coords;
+  GetBBoxOp2(Reals coords_in) : coords(coords_in) {}
+  OMEGA_H_INLINE BBox<dim> operator()(LO i) const {
+    return BBox<dim>(get_vector<dim>(coords, i));
+  }
+};
+
+namespace sample {  // namespace helps with name resolution in reduction identity 
+   template< int N >
+   struct bboxWrap {
+     BBox<N> box;
+  
+     KOKKOS_INLINE_FUNCTION   // Default constructor - Initialize to 0's
+     bboxWrap() {
+       const auto zero = zero_vector<N>();
+       box.min = zero;
+       box.max = zero;
+     }
+     KOKKOS_INLINE_FUNCTION   // Copy Constructor
+     bboxWrap(const bboxWrap & rhs) { 
+       box = rhs.box;
+     }
+     KOKKOS_INLINE_FUNCTION   // add operator
+     bboxWrap& operator += (const bboxWrap& src) {
+       box = unite(box,src.box);
+       return *this;
+     } 
+     KOKKOS_INLINE_FUNCTION   // volatile add operator 
+     void operator += (const volatile bboxWrap& src) volatile {
+       box = unite(box,src.box);
+     }
+   };
+   typedef bboxWrap<3> BB3;  // used to simplify code below
+}
+namespace Kokkos { //reduction identity must be defined in Kokkos namespace
+   template<>
+   struct reduction_identity< sample::BB3 > {
+      KOKKOS_FORCEINLINE_FUNCTION static sample::BB3 sum() {
+         return sample::BB3();
+      }
+   };
+}
 
 int main(int argc, char** argv) {
   auto lib = Library(&argc, &argv);
   auto world = lib.world();
-  const int size = 5;
-  Write<Real> out(size);
+  const int size = 6;
+  Reals init = {0,0,0,1,2,3};
+  Vector<3> min = {0,0,0};
+  Vector<3> max = {1,2,3};
+  BBox<3> gold(min,max);
 
-  auto transform = OMEGA_H_LAMBDA(LO i)->bool {
-    return true;
-  };
+  auto transform = GetBBoxOp2<3>(init);
 
-  bool res;
+  sample::BB3 tr;
   Kokkos::parallel_reduce(
     Kokkos::RangePolicy<>(0, 1), 
-    KOKKOS_LAMBDA(const int& i, bool& update) {
-      update = transform(i);
-  }, Kokkos::LAnd<bool>(res) );
-  assert(res==true);
+    KOKKOS_LAMBDA(const int& i, sample::BB3& update) {
+      auto box = transform(i);
+      update.box = unite(update.box,box);
+  }, Kokkos::Sum<sample::BB3>(tr) );
+
+  std::cout << "min ";
+  for(int i=0; i<3; i++)
+    std::cout << tr.box.min[i] << " ";
+  std::cout << "\n";
+  std::cout << "max ";
+  for(int i=0; i<3; i++)
+    std::cout << tr.box.max[i] << " ";
+  std::cout << "\n";
+
+  assert(are_close(tr.box,gold));
+
   return 0;
 }
