cfef29d102bfa1df0749596989f98cb2b58e7d0e
Cameron Smith <smithc11@rpi.edu>
2021-10-25 20:58:26 +0000

    almost

--- src/int128_reduce_test.cpp
+++ src/int128_reduce_test.cpp
@@ -1,17 +1,15 @@
 #include "Omega_h_for.hpp"
 #include "Omega_h_library.hpp"
-#include "Omega_h_bbox.hpp"
-#include "Omega_h_bbox.hpp"
+#include "Omega_h_int128.hpp"
 
-using namespace Omega_h;
 
 namespace sample {
 struct Int128Wrap {
-  Int128 i128;
+  Omega_h::Int128 i128;
 
   KOKKOS_INLINE_FUNCTION   // Default constructor - Initialize to 0
   Int128Wrap() {
-    i128 = Int128(0);
+    i128 = Omega_h::Int128(0);
   }
   KOKKOS_INLINE_FUNCTION   // Copy Constructor
   Int128Wrap(const Int128Wrap & rhs) {
@@ -24,33 +22,33 @@ struct Int128Wrap {
   }
   KOKKOS_INLINE_FUNCTION   // volatile add operator
   void operator += (const volatile Int128Wrap& src) volatile {
-    const auto foo = i128 + src.i128; //FIXME does not compile, complains about no matching ctor for 'i128'
-    i128 = foo;
+    i128 = i128 + src.i128;
   }
 };
 }
 
 namespace Kokkos { //reduction identity must be defined in Kokkos namespace
 template<>
-struct reduction_identity< Omega_h::Int128 > {
-   KOKKOS_FORCEINLINE_FUNCTION static Omega_h::Int128 sum() {
-      return Omega_h::Int128();
+struct reduction_identity< sample::Int128Wrap > {
+   KOKKOS_FORCEINLINE_FUNCTION static sample::Int128Wrap sum() {
+      return sample::Int128Wrap();
    }
 };
 }
 
 int main(int argc, char** argv) {
+  using namespace Omega_h;
   auto lib = Library(&argc, &argv);
   auto world = lib.world();
-  Int128 a = {0,1,3};
+  Reals a = {0,1,2};
 
   sample::Int128Wrap res;
   Kokkos::parallel_reduce(
     Kokkos::RangePolicy<>(0, a.size() ),
     KOKKOS_LAMBDA(int i, sample::Int128Wrap& update) {
-      update.i128 = transform(i);
+      update.i128 = Int128::from_double(a[i],1.0);
     }, Kokkos::Sum< sample::Int128Wrap >(res) );
-  return res.i128;
+   printf("result %zu %zu\n", res.i128.high, res.i128.low);
 
   return 0;
 }
