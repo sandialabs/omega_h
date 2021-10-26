fbeb18c7b9de9a5ed1e48625f4f1b92d5bf53328
Cameron Smith <smithc11@rpi.edu>
2021-10-26 21:20:30 +0000

    remove prints

--- src/arrayops_test.cpp
+++ src/arrayops_test.cpp
@@ -9,24 +9,20 @@ int main(int argc, char** argv) {
   {
     Reals a = {1,0,2};
     bool res = is_sorted(a);
-    printf("sorted: %s\n", res ? "yes" : "no");
     OMEGA_H_CHECK(res == false);
   }
   {
     Reals a = {1,0,1,2};
     LO res = find_last(a,1.0);
-    printf("index: %d\n", res);
     OMEGA_H_CHECK(res == 2);
   }
   {
     Reals a = {1,0.0,1,2};
     Reals b = {1,0.1,1,2};
     bool res = are_close_abs(a,b,1e-3);
-    printf("are_close: %s\n", res ? "yes" : "no");
     OMEGA_H_CHECK(res == false);
     Reals c = {1,0.000001,1,2};
     res = are_close_abs(a,c,1e-3);
-    printf("are_close: %s\n", res ? "yes" : "no");
     OMEGA_H_CHECK(res == true);
   }
   {
@@ -35,13 +31,11 @@ int main(int argc, char** argv) {
     Reals a = {1,0.0,1,2};
     Reals b = {1,0.1,1,2};
     bool res = are_close(a,b,tol,floor);
-    printf("are_close: %s\n", res ? "yes" : "no");
     OMEGA_H_CHECK(res == false);
     const auto small = 0.000001;
     Reals c = {1,small,1,2};
     res = are_close(a,c,tol,floor);
     const auto expected = are_close(0.1,small,tol,floor);
-    printf("are_close: %s\n", res ? "yes" : "no");
     OMEGA_H_CHECK(res == expected);
   }
   return 0;
