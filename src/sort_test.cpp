1ccdadd2f5f9b5570f4304611fe2956caf9aabca
Cameron Smith <smithc11@rpi.edu>
2021-11-01 15:28:38 +0000

    compiles
    thanks to masterleinad

--- src/sort_test.cpp
+++ src/sort_test.cpp
@@ -8,22 +8,28 @@ int main(int argc, char** argv) {
   auto lib = Library(&argc, &argv);
   auto world = lib.world();
   {
-    LOs a({0, 1});
-    int const n = a.size();
-    Write<LO> perm(n, 0, 1);
+    //Write<LO> perm(n, 0, 1);
 
     auto space = Kokkos::Experimental::SYCL();
-    const auto q = space.impl_internal_space_instance()->m_queue;
+    const auto q = *space.impl_internal_space_instance()->m_queue;
 
     using namespace oneapi::dpl::execution;
     using namespace sycl;
     auto policy = make_device_policy(q);
+
+    Write<LO> perm = {43, 0, 3};
+    LOs expected = {0, 3, 43};
+    oneapi::dpl::sort(
+        policy, perm.data(), perm.data() + perm.size(),
+        [](auto lhs, auto rhs) { return lhs < rhs; });
+
+    HostRead<LO> h_perm(read(perm));
+    for(int i=0; i<h_perm.size(); i++)
+      printf("%d ", h_perm[i]);
+    printf("\n");
+
+    //OMEGA_H_CHECK(read(perm) == expected); //FIXME, does not compile, type mismatch???
   }
-//    oneapi::dpl::sort(
-//        policy, perm.data(), perm.data() + n,
-//        [](auto lhs, auto rhs) { return std::get<0>(lhs) < std::get<0>(rhs); });
-//
-//    OMEGA_H_CHECK(permute == LOs({0, 1}));
 //  }
 //  {
 //    LOs a({0, 2, 0, 1});
