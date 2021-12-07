d58e9e41881ca5e8f3c247a18577a7d398f94b89
Cameron Smith <smithc11@rpi.edu>
2021-12-07 12:23:24 -0800

    sort_test: custom comparison

--- src/sort_test.cpp
+++ src/sort_test.cpp
@@ -4,13 +4,25 @@
 #include "Omega_h_array_ops.hpp"
 //#include "Omega_h_sort.hpp"
 
+template <typename T, Omega_h::Int N>
+struct CompareKeySets {
+  T const* keys_;
+  CompareKeySets(T const* keys) : keys_(keys) {}
+  OMEGA_H_INLINE bool operator()(const Omega_h::LO& a, const Omega_h::LO& b) const {
+    for (Omega_h::Int i = 0; i < N; ++i) {
+      T x = keys_[a * N + i];
+      T y = keys_[b * N + i];
+      if (x != y) return x < y;
+    }
+    return false;
+  }
+};
+
 int main(int argc, char** argv) {
   using namespace Omega_h;
   auto lib = Library(&argc, &argv);
   auto world = lib.world();
   {
-    //Write<LO> perm(n, 0, 1);
-
     auto space = Kokkos::Experimental::SYCL();
     const auto q = *space.impl_internal_space_instance()->m_queue;
 
@@ -18,20 +30,32 @@ int main(int argc, char** argv) {
     using namespace sycl;
     auto policy = make_device_policy(q);
 
+    {
     Write<LO> perm = {43, 0, 3};
     LOs expected = {0, 3, 43};
     oneapi::dpl::sort(
         policy, perm.data(), perm.data() + perm.size(),
         [](auto lhs, auto rhs) { return lhs < rhs; });
 
-    HostRead<LO> h_perm(read(perm));
-    for(int i=0; i<h_perm.size(); i++)
-      printf("%d ", h_perm[i]);
-    printf("\n");
+    Read<LO> r_expected(expected);
+    Read<LO> r_perm(perm);
+    OMEGA_H_CHECK(r_perm == r_expected);
+    }
+
+    {
+    Write<LO> perm = {0,1};
+    LOs keys = {0,2,0,1};
+    LO const* keyptr = keys.data();
+    auto cmp = CompareKeySets<LO,2>(keyptr);
+    LOs expected = {1, 0};
+    oneapi::dpl::sort(policy,
+        perm.data(), perm.data() + perm.size(), cmp);
 
     Read<LO> r_expected(expected);
     Read<LO> r_perm(perm);
     OMEGA_H_CHECK(r_perm == r_expected);
+    }
+
   }
 //  }
 //  {
