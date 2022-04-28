33bd9f0b768772165bae4679d61fbad7daf4b6b7
Jacob Merson <mersoj@rpi.edu>
2022-04-28 02:45:07 -0400

    fix form_barycentric add function for global->xi coords

--- src/shape_test.cpp
+++ src/shape_test.cpp
@@ -2,32 +2,21 @@
 #include <iostream>
 
 
-auto get_inverse_basis(const Omega_h::Matrix<2,3> & coords) { 
-  auto basis = Omega_h::simplex_basis<2,2>(coords);
-  Omega_h::Matrix<2,2> hand_basis {{coords(0,1)-coords(0,0),coords(1,1)-coords(1,0)},
-                                   {coords(0,2)-coords(0,0),coords(1,2)-coords(1,0)}};
-  OMEGA_H_CHECK(are_close(basis, hand_basis));
-  auto inverse_basis = Omega_h::invert(basis);
-  return inverse_basis;
-}
-
-
-void check_verts(const Omega_h::Matrix<2,3> & coords, const Omega_h::Matrix<2,2> & inverse_basis) {
+void check_verts(const Omega_h::Matrix<2,3> & coords) {
   for(int i=0; i<coords.size(); ++i ) {
-    auto lambda = inverse_basis*(coords[i] - coords[0]);
-    auto xi = Omega_h::form_barycentric(lambda);
+    auto xi = Omega_h::barycentric_from_global<2,2>(coords[i], coords);
     Omega_h::Vector<3> hand_xi{0,0,0};
     hand_xi[i] = 1;
+    printf("[%f,%f,%f] == [%f,%f,%f]\n",xi[0],xi[1],xi[2],hand_xi[0],hand_xi[1],hand_xi[2]);
     OMEGA_H_CHECK(Omega_h::are_close(xi,hand_xi));
   }
 }
 
-void check_point(const Omega_h::Matrix<2,3> & coords, const Omega_h::Matrix<2,2> & inverse_basis) {
+void check_point(const Omega_h::Matrix<2,3> & coords) {
     Omega_h::Vector<2> point{0.5,0.5};
-    auto lambda = inverse_basis*(point - coords[0]);
-    auto xi = Omega_h::form_barycentric(lambda);
-
+    auto xi = Omega_h::barycentric_from_global<2,2>(point, coords);
     Omega_h::Vector<3> hand_xi{0.25,0.25,0.5};
+    printf("[%f,%f,%f] == [%f,%f,%f]\n",xi[0],xi[1],xi[2],hand_xi[0],hand_xi[1],hand_xi[2]);
     OMEGA_H_CHECK(are_close(xi,hand_xi));
 
 }
@@ -35,9 +24,8 @@ void check_point(const Omega_h::Matrix<2,3> & coords, const Omega_h::Matrix<2,2>
 
 int main(int argc, char** argv) {
   Omega_h::Matrix<2,3> coords{{0.0},{1,0},{0.5,1}} ;
-  auto inverse_basis = get_inverse_basis(coords);
-  check_verts(coords, inverse_basis);
-  check_point(coords,inverse_basis);
+  check_verts(coords);
+  check_point(coords);
 
   return 0;
 }
