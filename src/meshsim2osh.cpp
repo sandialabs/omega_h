96b0d602e9f8689f986ecabf702e361276547ec6
Cameron Smith <smithc11@rpi.edu>
2022-06-23 14:48:45 -0400

    meshsim2osh: optional arg for numbering

--- src/meshsim2osh.cpp
+++ src/meshsim2osh.cpp
@@ -12,12 +12,18 @@ int main(int argc, char** argv) {
   cmdline.add_arg<std::string>("mesh-in");
   cmdline.add_arg<std::string>("model-in(geomSim)");
   cmdline.add_arg<std::string>("mesh-out");
-  cmdline.add_arg<std::string>("numbering-in");
+  auto& numberingFlag = cmdline.add_flag(
+      "-numbering", "Attach the vertex numbering from the specified Simmetrix .nex file");
+  numberingFlag.add_arg<std::string>("numbering-in");
+
   if (!cmdline.parse_final(comm, &argc, argv)) return -1;
   auto mesh_in = cmdline.get<std::string>("mesh-in");
   auto model_in = cmdline.get<std::string>("model-in(geomSim)");
   auto mesh_out = cmdline.get<std::string>("mesh-out");
-  auto numbering_in = cmdline.get<std::string>("numbering-in");
+  std::string numbering_in;
+  if (cmdline.parsed("-numbering-in")) {
+    numbering_in = cmdline.get<std::string>("numbering-in");
+  }
   auto mesh = Omega_h::meshsim::read(mesh_in, model_in, numbering_in, comm);
   auto family = mesh.family();
   if ((family == OMEGA_H_SIMPLEX) || family == OMEGA_H_HYPERCUBE) {
