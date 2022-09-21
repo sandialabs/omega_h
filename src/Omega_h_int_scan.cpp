942a084f21624c5ead5320a549ed477894953034
Cameron Smith <smithc11@rpi.edu>
2022-09-21 02:36:40 +0000

    kk inclusive_scan compiles

--- src/Omega_h_int_scan.cpp
+++ src/Omega_h_int_scan.cpp
@@ -12,10 +12,20 @@ LOs offset_scan(Read<T> a, std::string const& name) {
   OMEGA_H_TIME_FUNCTION;
   Write<LO> out(a.size() + 1, name);
   out.set(0, 0);
+#if defined(OMEGA_H_USE_KOKKOS) and !defined(OMEGA_H_USE_CUDA) and !defined(OMEGA_H_USE_OPENMP)
+  auto outSub = Kokkos::subview(out.view(),std::make_pair(1,a.size()+1));
+  Kokkos::Experimental::inclusive_scan(
+    "omegah_kk_offset_scan", ExecSpace(),
+    Kokkos::Experimental::cbegin(a.view()),
+    Kokkos::Experimental::cend(a.view()),
+    Kokkos::Experimental::begin(outSub)
+  );
+#else
   auto const first = CastIterator<LO, T>(a.begin());
   auto const last = CastIterator<LO, T>(a.end());
   auto const result = out.begin() + 1;
   inclusive_scan(first, last, result);
+#endif
   return out;
 }
 
