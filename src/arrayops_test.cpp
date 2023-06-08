0c353518793655f13e6f7f9bef14f9623e0aa262
Cameron Smith <smithc11@rpi.edu>
2023-06-08 13:36:55 -0400

    formatting

--- src/arrayops_test.cpp
+++ src/arrayops_test.cpp
@@ -58,7 +58,7 @@ int main(int argc, char** argv) {
     OMEGA_H_CHECK(res == 2);
   }
   {
-    const int n = 1000000;
+    const int n = 1'000'000;
     Write<LO> a(n);
     parallel_for(n, OMEGA_H_LAMBDA(int i) { a[i] = i; }, "setVals");
     a.set(42,n);
@@ -72,7 +72,7 @@ int main(int argc, char** argv) {
     OMEGA_H_CHECK(res == 0.1);
   }
   {
-    const int n = 100000;
+    const int n = 100'000;
     Write<Real> a(n);
     parallel_for(n, OMEGA_H_LAMBDA(int i) { a[i] = i+1; }, "setVals");
     a.set(42,0.1);
