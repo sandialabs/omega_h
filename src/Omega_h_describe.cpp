660b36aaf0cc5486d27c5754ad421c26a284a029
Angelyr <scardking@gmail.com>
2023-11-28 16:43:01 -0500

    printing more fields

--- src/Omega_h_describe.cpp
+++ src/Omega_h_describe.cpp
@@ -8,5 +8,18 @@ int main(int argc, char** argv)
 
     int dim = mesh.dim();
     for(int d=0; d < mesh.dim(); d++)
-        printf("(Dim, Num Mesh Entities): (%d, %d)\n", dim, mesh.nents(d));
+        printf("(Dim, Num Mesh Entities): (%d, %d)\n", d, mesh.nents(d));
+
+    printf("nelems: %d\n", mesh.nelems());
+    printf("nregions: %d\n", mesh.nregions());
+    printf("nfaces: %d\n", mesh.nfaces());
+    printf("nedges: %d\n", mesh.nedges());
+    printf("nverts: %d\n", mesh.nverts());
+
+    printf("npyrams: %d\n", mesh.npyrams());
+    printf("nwedges: %d\n", mesh.nwedges());
+    printf("nhexs: %d\n", mesh.nhexs());
+    printf("ntets: %d\n", mesh.ntets());
+    printf("nquads: %d\n", mesh.nquads());
+    printf("ntris: %d\n", mesh.ntris());
 }
