c8a511a031dc4d6acb303c6c33d62c78a29fd04e
Angelyr <scardking@gmail.com>
2023-11-28 18:34:13 -0500

    printing tag name

--- src/Omega_h_describe.cpp
+++ src/Omega_h_describe.cpp
@@ -6,9 +6,11 @@ int main(int argc, char** argv)
     auto lib = Omega_h::Library(&argc, &argv);
     Omega_h::Mesh mesh = Omega_h::read_mesh_file(argv[1], lib.world());
 
-    int dim = mesh.dim();
-    for(int d=0; d < mesh.dim(); d++)
-        printf("(Dim, Num Mesh Entities): (%d, %d)\n", d, mesh.nents(d));
+    for(int dim=0; dim < mesh.dim(); dim++)
+        printf("(Dim, Num Mesh Entities): (%d, %d)\n", dim, mesh.nents(dim));
+
+    for(int dim=0; dim < mesh.dim(); dim++)
+        printf("(Dim, Imbalance): (%d, %d)\n", dim, mesh.imbalance(dim));
 
     printf("nelems: %d\n", mesh.nelems());
     printf("nregions: %d\n", mesh.nregions());
@@ -22,4 +24,10 @@ int main(int argc, char** argv)
     printf("ntets: %d\n", mesh.ntets());
     printf("nquads: %d\n", mesh.nquads());
     printf("ntris: %d\n", mesh.ntris());
+
+    for(int dim=0; dim < mesh.dim(); dim++)
+    for (int tag=0; tag < mesh.ntags(dim); tag++) {
+        auto tagbase = mesh.get_tag(dim, tag);
+        printf("(Dim, Tag): (%d, %s)\n", dim, tagbase->name().c_str());
+    }
 }
